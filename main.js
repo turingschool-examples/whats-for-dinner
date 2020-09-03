0424abfd15f1b12290e7b37147969f42efbb18dc
JP Carey <jpcarey4@gmail.com>
2020-09-02 21:54:44 -0600

    reset after clear button is clicked

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var dessertSelect = document.querySelector(".dessert-select");
 var letsCookButton = document.querySelector(".lets-cook-button");
 var resultBox = document.querySelector(".result-box");
 var cookPotIcon = document.querySelector(".cook-pot-icon");
+var clearButton = document.querySelector(".clear-button");
+var mealSelect = document.querySelector(".meal-select");
 
 var sides = [
   "Miso Glazed Carrots",
@@ -54,6 +56,7 @@ var desserts = [
 ];
 
 letsCookButton.addEventListener("click", displayRandomDish);
+clearButton.addEventListener("click", restartQuery)
 
 function displayRandomDish() {
   if (sideSelect.checked) {
@@ -68,17 +71,42 @@ function displayRandomDish() {
   }
 }
 
-function displayDish(dish) {
-  cookPotIcon.classList.add("hidden");
-  var singleDishBlock =
-  `<div>
-    <h4>You should make:</h4>
-    <h1>${dish}</h1>
-    </div>`
-  resultBox.insertAdjacentHTML('afterbegin', singleDishBlock);
-}
-
 function getRandomIndex(array) {
   var randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
+}
+
+function displayDish(dish) {
+  var singleDishBlock =
+  `<div class="dish-block">
+    <h4>You should make:</h4>
+    <h1>${dish}!</h1>
+    </div>`
+  resultBox.insertAdjacentHTML('afterbegin', singleDishBlock);
+  allowSelectionReset();
+}
+
+function allowSelectionReset() {
+  cookPotIcon.classList.add("hidden");
+  clearButton.classList.remove("hidden");
+  letsCookButton.disabled = true;
+  sideSelect.disabled = true;
+  mainDishSelect.disabled = true;
+  dessertSelect.disabled = true;
+  mealSelect.disabled = true;
+}
+
+function restartQuery() {
+  cookPotIcon.classList.remove("hidden");
+  clearButton.classList.add("hidden");
+  letsCookButton.disabled = false;
+  sideSelect.disabled = false;
+  mainDishSelect.disabled = false;
+  dessertSelect.disabled = false;
+  mealSelect.disabled = false;
+  letsCookButton.checked = false;
+  sideSelect.checked = false;
+  mainDishSelect.checked = false;
+  dessertSelect.checked = false;
+  mealSelect.checked = false;
 }

f7dc5f06bed4867005678ec63fb5763d2288bbc0
Nikki <piknik@me.com>
2020-12-15 16:21:37 -0700

    Add function for choices delegation

--- main.js
+++ main.js
@@ -6,7 +6,21 @@ choiceSelector.addEventListener("click", function(e) {
 
   var choice = document.querySelector('input[name="choice"]:checked').value;
 
-  console.log(choice)
+  // hand that choice to a helper function
 })
 
+function getMeal(choice) {
+  if (choice === "side") {
+
+  } else if (choice === "main-dish") {
+
+  } else if (choice === "dessert") {
+
+  } else if (choice === "entire-meal") {
+
+  } else {
+    return alert("You must make a choice of the type of meal you want.")
+  }
+
+}
 

1301910f718ecda0c96af474b91f9d56cfe7369b
Steven Mancine <stevenrmancine@gmail.com>
2020-12-16 13:02:55 -0700

    Create h1 element in html complete function to suggest recipe

--- main.js
+++ main.js
@@ -2,23 +2,26 @@ var sideRadio = document.querySelector('#side');
 var mainDishRadio = document.querySelector('#mainDish');
 var dessertRadio = document.querySelector('#dessert');
 var letsCookButton = document.querySelector('.lets-cook');
-
+var mealToMake = document.querySelector('.suggested-dish');
+var cookingPotImg= document.querySelector('.cooking-pot')
 
 letsCookButton.addEventListener('click', suggestRecipe);
 
 function suggestRecipe() {
-  if (sideRadio.checked) {
-    pickRandomRecipe(sides)
-  } else if (mainDishRadio.checked) {
-    pickRandomRecipe(mains)
-  } else if(dessertRadio.checked){
-    pickRandomRecipe(desserts)
-  }
-
   event.preventDefault();
+  if (sideRadio.checked) {
+    cookingPotImg.style.display = 'none'
+    mealToMake.innerText = pickRandomRecipe(sides)
+  } else if (mainDishRadio.checked) {
+    cookingPotImg.style.display = 'none'
+    mealToMake.innerText = pickRandomRecipe(mains)
+  } else if(dessertRadio.checked){
+    cookingPotImg.style.display = 'none'
+    mealToMake.innerText = pickRandomRecipe(desserts)
+  }
 }
 
 function pickRandomRecipe(type) {
   var randomArrayIndex = Math.floor(Math.random()*type.length)
-  var suggestedMeal = type[randomArrayIndex]
+  return type[randomArrayIndex]
 }

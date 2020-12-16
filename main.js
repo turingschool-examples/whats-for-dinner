0b5f2e893f1c10d16503eb6c5e835ab57319a4ca
Nikki <piknik@me.com>
2020-12-16 05:41:45 -0700

    WIP: Working through choice selection logic

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 
 var choiceSelector = document.querySelector('input.cook-button');
+var resultSelector = document.querySelector('.result');
+var imgSelector = document.querySelector('img#cookpot');
 
 choiceSelector.addEventListener("click", function(e) {
   e.preventDefault();
+  console.log(e)
 
   var choice = document.querySelector('input[name="choice"]:checked').value;
-
-  // hand that choice to a helper function
-  getMeal()
+  console.log(choice);
+  //
+  // // hand that choice to a helper function
+  // getMeal(choice)
 })
 
 function getMeal(choice) {
   if (choice === "side") {
-
+    var randomSide = randomGenerator(sides)
+    imgSelector.classList.add('hidden')
+    resultSelector.innerHTML = `You should totally make: <h3>${randomSide}`
   } else if (choice === "main-dish") {
 
   } else if (choice === "dessert") {

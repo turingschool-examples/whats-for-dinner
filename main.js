34a04fa8f954b61218bbb4afc3d91b714e2fe97a
Aaron Foucheaux <afoucheaux@gmail.com>
2020-12-17 07:52:30 -0600

    Added let's cook functionality

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 // Create variables targetting the relevant DOM elements here.
+var cookPotImg = document.querySelector('#svg-cookpot');
+var choice = document.querySelector(".choice-buttons");
+var cook = document.querySelector('.cook');
+var shouldMake = document.querySelector("#you-make");
+var clearButton = document.querySelector("#clear");
+var foodStatment = document.querySelector("#change-sentence");
 
 
 
 // Add your event listeners here
-
+cook.addEventListener("click", generateMeal);
 
 
 
 // Create your event handlers and other functions here.
 function unhide(element) {
-  element.classList.remove('hidden')
+  element.classList.remove('hidden');
 }
 
 function hide(element) {
-  element.classList.add('hidden')
+  element.classList.add('hidden');
 }
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
+}
+
+function generateMeal() {
+  event.preventDefault();
+  hide(cookPotImg);
+  unhide(shouldMake);
+  unhide(clearButton);
+  unhide(foodStatment);
 }

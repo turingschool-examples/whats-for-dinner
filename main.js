365260399c655b0f15b02e599886b7f1cdb765a9
Steven Mancine <stevenrmancine@gmail.com>
2020-12-16 18:39:31 -0700

    Add styling to results box

--- main.js
+++ main.js
@@ -2,26 +2,34 @@ var sideRadio = document.querySelector('#side');
 var mainDishRadio = document.querySelector('#mainDish');
 var dessertRadio = document.querySelector('#dessert');
 var letsCookButton = document.querySelector('.lets-cook');
-var mealToMake = document.querySelector('.suggested-dish');
-var cookingPotImg= document.querySelector('.cooking-pot')
+var mealToMake = document.querySelector('#suggested-dish');
+var cookingPotImg= document.querySelector('.cooking-pot');
+// var radioButtons = document.getElementsByName("dish");
+var youShouldCook= document.querySelector('.what-to-cook');
+var suggestedDish= document.querySelector('#suggested-dish');
 
 letsCookButton.addEventListener('click', suggestRecipe);
 
 function suggestRecipe() {
   event.preventDefault();
   if (sideRadio.checked) {
-    cookingPotImg.style.display = 'none'
-    mealToMake.innerText = pickRandomRecipe(sides)
+    youShouldCook.style.display = 'block';
+    cookingPotImg.style.display = 'none';
+    mealToMake.innerText =  pickRandomRecipe(sides) + "!";
   } else if (mainDishRadio.checked) {
-    cookingPotImg.style.display = 'none'
-    mealToMake.innerText = pickRandomRecipe(mains)
+    youShouldCook.style.display = 'block';
+    cookingPotImg.style.display = 'none';
+    mealToMake.innerText = pickRandomRecipe(mains) + "!";
   } else if(dessertRadio.checked){
-    cookingPotImg.style.display = 'none'
-    mealToMake.innerText = pickRandomRecipe(desserts)
-  }
-}
+    youShouldCook.style.display = 'block';
+    cookingPotImg.style.display = 'none';
+    mealToMake.innerText = pickRandomRecipe(desserts) + "!";
+  };
+};
+
+
 
 function pickRandomRecipe(type) {
-  var randomArrayIndex = Math.floor(Math.random()*type.length)
-  return type[randomArrayIndex]
-}
+  var randomArrayIndex = Math.floor(Math.random()*type.length);
+  return type[randomArrayIndex];
+};

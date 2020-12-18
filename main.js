6f688253f16ae33e4984554ebb209ea227468324
Steven Mancine <stevenrmancine@gmail.com>
2020-12-17 17:04:03 -0700

    Set visibility of error message to hidden until prompted

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ var cookingPotImg= document.querySelector('.cooking-pot');
 // var radioButtons = document.getElementsByName("dish");
 var youShouldCook= document.querySelector('.what-to-cook');
 var suggestedDish= document.querySelector('#suggested-dish');
-var errorMessage = document.querySelector('.error-message')
+var errorMessage = document.querySelector('#error-message')
+var clearButton = document.querySelector('#clear-section')
 
 letsCookButton.addEventListener('click', suggestRecipe);
+clearButton.addEventListener('click', clearResults)
 
 function suggestRecipe() {
   event.preventDefault();
@@ -22,9 +24,13 @@ function suggestRecipe() {
   } else if(dessertRadio.checked){
     changeView();
     mealToMake.innerText = pickRandomRecipe(desserts) + "!";
-  };
+  } else {
+    errorMessage.style.visibility = "visible"
+  }
 };
-
+function clearResults() {
+  location.reload()
+}
 function showErrorMessage() {
  // errorMessage.style.display = 'flex'
 }

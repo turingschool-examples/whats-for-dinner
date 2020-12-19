36e50b1516a740b650ae837a2ba5226357bc5666
Steven Mancine <stevenrmancine@gmail.com>
2020-12-18 21:43:59 -0700

    Finish design for login page

--- main.js
+++ main.js
@@ -7,11 +7,26 @@ var cookingPotImg= document.querySelector('.cooking-pot');
 // var radioButtons = document.getElementsByName("dish");
 var youShouldCook= document.querySelector('.what-to-cook');
 var suggestedDish= document.querySelector('#suggested-dish');
-var errorMessage = document.querySelector('#error-message')
-var clearButton = document.querySelector('#clear-section')
+var errorMessage = document.querySelector('#error-message');
+var clearButton = document.querySelector('#clear-section');
+var submitButton = document.querySelector('#submit');
+var hiddenHomeView = document.querySelectorAll('.hidden');
+var homeView = document.querySelector('.home-view');
+var loginPage = document.querySelector(".login-page");
+
 
 letsCookButton.addEventListener('click', suggestRecipe);
-clearButton.addEventListener('click', clearResults)
+clearButton.addEventListener('click', clearResults);
+submitButton.addEventListener('click', login)
+
+
+function login() {
+  event.preventDefault();
+  console.log(hiddenHomeView)
+  hiddenHomeView[0].style.display = "flex"
+  homeView.style.display = "flex"
+  loginPage.style.display = 'none';
+}
 
 function suggestRecipe() {
   event.preventDefault();
@@ -29,9 +44,9 @@ function suggestRecipe() {
   }
 };
 function clearResults() {
-  location.reload()
 }
 function showErrorMessage() {
+  errorMessage.classList.toggle("hidden")
  // errorMessage.style.display = 'flex'
 }
 

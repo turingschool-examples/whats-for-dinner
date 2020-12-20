7fbc80927bccc448172443955ee75812e1fcbb70
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2020-12-19 22:26:24 -0700

    Create query selectors, modify HTML element IDs

---
+++ main.js
@@ -0,0 +1,5 @@
+var sideRadio = document.querySelector("#meal-side");
+var mainDishRadio = document.querySelector("#meal-main-dish");
+var dessertRadio = document.querySelector("#meal-dessert");
+
+var letsCookButton = document.querySelector("#lets-cook");

f53885e9426c2703272dc4d7b8315a6f90eb5aaa
Nichele Dunn <the7yearitch@hotmail.com>
2020-12-20 11:29:30 -0700

    Add variables for radio button selectors

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var sideRadio = document.querySelector('#Side');
+var mainRadio = document.querySelector('#Main-dish');
+var dessertRadio = document.querySelector('#Dessert');
+var mealRadio = document.querySelector('#Entire-meal');
+
 // Build a function that will randomly generate either a Side, Main Dish, or Dessert, depending upon which radio button is clicked
   // need arrays for [Sides], [Main Dish], [Dessert]
 
@@ -6,7 +11,9 @@ function generateRandomIndex(array) {
     var randomIndex = array[Math.floor(Math.random() * array.length)];
   }
   return randomIndex;
-};  
+};
+
+
 
 // when the [LET'S COOK] button is clicked, display randomly generated recipe name
 // Recipe name should appear below the phrase "You should make:" - styles medium size font, italic

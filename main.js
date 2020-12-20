0296fc24b9fb282542a57cae758b537b562247b7
Nichele Dunn <the7yearitch@hotmail.com>
2020-12-20 11:26:26 -0700

    Add random index generator

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 // Build a function that will randomly generate either a Side, Main Dish, or Dessert, depending upon which radio button is clicked
   // need arrays for [Sides], [Main Dish], [Dessert]
 
+function generateRandomIndex(array) {
+  for (var i = 0; i < array.length; i++) {
+    var randomIndex = array[Math.floor(Math.random() * array.length)];
+  }
+  return randomIndex;
+};  
+
 // when the [LET'S COOK] button is clicked, display randomly generated recipe name
 // Recipe name should appear below the phrase "You should make:" - styles medium size font, italic
 // Recipe name should appear in large font with an '!' at the end

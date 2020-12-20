21ee5e5d8562fb48f91648267541d42f58fdd629
Nichele Dunn <the7yearitch@hotmail.com>
2020-12-20 15:02:47 -0700

    Update functions - not functioning currently

--- main.js
+++ main.js
@@ -2,14 +2,15 @@
 var sideRadio = document.querySelector('#Side');
 var mainRadio = document.querySelector('#Main-dish');
 var dessertRadio = document.querySelector('#Dessert');
-var mealRadio = document.querySelector('#Entire-meal');
-var addRecButton = document.querySelector('.add-recipe');
+// var mealRadio = document.querySelector('#Entire-meal');
+// var addRecButton = document.querySelector('.add-recipe');
 var selectionButton = document.querySelector('.selector');
 var cookpot = document.querySelector('.cookpot');
+var makeThis = document.querySelector('.make-this')
 
 // ⤵ event listeners
-sideRadio.addEventListener('click .selector', showRandomSide);
-
+// sideRadio.addEventListener('click .selector', showRandomSide);
+selectionButton.addEventListener('click', showRandomSelection)
 
 // Build a function that will randomly generate either a Side, Main Dish, or Dessert, depending upon which radio button is clicked
   // need arrays for [Sides], [Main Dish], [Dessert]
@@ -18,21 +19,37 @@ function generateRandomIndex(array) {
   for (var i = 0; i < array.length; i++) {
     var randomIndex = array[Math.floor(Math.random() * array.length)];
   }
+  console.log(randomIndex);
   return randomIndex;
 };
 
-function showRandomSide() {
-  cookpot.classList.add('hidden');
-  recipe.innerText = generateRandomIndex(sides);
-};
+function showRandomSelection() {
+  if ('#Side'.checked) {
+    generateRandomIndex(sides)
+    console.log(sides[i])
+    return makeThis.innerText(sides[i])
+  } else if ('#Main-dish'.checked) {
+    generateRandomIndex(mainDishes)
+  } else if ('#Dessert'.checked) {
+    generateRandomIndex(desserts)
+  }
+//   console.log(randomIndex)
+//   return makeThis.innerText = randomIndex
+}
 
-function showRandomMain() {
-  cookpot.classList.add('hidden');
-};
-
-function showRandomDessert() {
-  cookpot.classList.add('hidden');
-};
+//
+// function showRandomSide() {
+//   cookpot.classList.add('hidden');
+//   recipe.innerText = generateRandomIndex(sides);
+// };
+//
+// function showRandomMain() {
+//   cookpot.classList.add('hidden');
+// };
+//
+// function showRandomDessert() {
+//   cookpot.classList.add('hidden');
+// };
 
 // function showRandomMeal() {
 //   cookpot.classList.add('hidden');

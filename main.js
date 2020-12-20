1006c38c30510b54048e820afb391ff3ac3f764e
Melanie Daoheuang <daoh0031@gmail.com>
2020-12-20 14:07:15 -0700

    Update arrays to keys in an object and add code to obtain radio button input

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-//variables to target side,main,dessert,entire meal and button 
-//function with conditional for which radio button is selected >> pull rando data from array >> function 
-//event listener to "let's cook" to view random dish from list of dishes in that category 
-//hide pot picture >> display rando dish pops up in box2
-
+//Radio Button HTML Elements 
 var side = document.querySelector('#side');
 var main = document.querySelector('#main-dish');
 var dessert = document.querySelector('#dessert');
-var meal = document.querySelector('entire-meal');
+var meal = document.querySelector('#entire-meal');
 var cookButton = document.querySelector('.button2');
+var radios = document.getElementsByName('dish');
 
+//Event Listeners
+cookButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  getRandomDish();
+});
+         
+//Retrieves random arry from index
+function getRandomNumber(array) {
+  return Math.floor(Math.random() * array.length);
+};
+
+//Return radio button input will access the array 
+function getRandomDish() {
+  var randomDish;
+  var randomNum;
+  var userInput;
+  var dishString = '';
+  for (var i = 0; i < radios.length; i++) {  
+    if (radios[i].checked) {
+      userInput = radios[i].id;
+    }  
+  };
+
+  randomNum = getRandomNumber(food[userInput])
+  randomDish = food[userInput][randomNum]
+  console.log(randomDish)
+};

00aa6561013785f3630fe5ffca35e8019a2eeb14
Melanie Daoheuang <daoh0031@gmail.com>
2020-12-20 19:09:41 -0700

    Update README and padding for entire meal

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Overview
+What's for dinner? The ultimate question. This is an app that helps users choose a recipe and put together meals. 
 
-What's for dinner? The ultimate question. You will be building an app that helps users choose a recipe and put together meals. 
+## Instructions 
+Based on what type of dish you are looking for, please select one button from the 4 radio button options. After a button is slected, click the "let's cook" button to view  random dish or an entire meal. The "let's cook" button can be clicked multiple times to view a new dish option.  
 
-This Solo Challenge gives students and instructors the opportunity to get a pulse on where you are with the foundational concepts of Module 1 curriculum. Students should use this as an opportunity to challenge themselves and work completely independently. Google can (and probably should!) be used, but any other code base should not be referenced. Instructors will be able to use your work, both completion of functionality and code quality, to determine where you stand and if you are behind for this point in the module, provide supports to intervene.
-
-The spec for this project can be found [here](https://frontend.turing.io/projects/module-1/dinner.html). 
+[GitHub Pages found here](https://daomeow.github.io/whats-for-dinner/.).
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-//Radio Button HTML Elements 
+//////////////////////////////////////
+// Global variables
+//////////////////////////////////////  
 var side = document.querySelector('#side');
 var main = document.querySelector('#main-dish');
 var dessert = document.querySelector('#dessert');
@@ -6,47 +8,59 @@ var meal = document.querySelector('#entire-meal');
 var cookButton = document.querySelector('.button2');
 var radios = document.getElementsByName('dish');
 
-//Event Listeners
+//////////////////////////////////////
+// Event Listener
+//////////////////////////////////////
 cookButton.addEventListener('click', function(event) {
   event.preventDefault();
   getRandomDish();
-});
-         
-//Retrieves random arry from index
+})
+
+//////////////////////////////////////
+// Functions
+//////////////////////////////////////
+
+// Retrieve random number from array 
 function getRandomNumber(array) {
   return Math.floor(Math.random() * array.length);
-};
+}
 
-//Return radio button input will access the array 
+// Return radio button input and access the appropriate array 
 function getRandomDish() {
   var randomDish;
   var randomNum;
-  var entireMeal;
   var userInput;
   var dishString = '';
+  var randomMain = '';
+  var randomSide = '';
+  var randomDessert = '';
+  var entireMeal;
+
+  // Loop through radio buttons to return user selected radio ID
   for (var i = 0; i < radios.length; i++) {  
     if (radios[i].checked) {
       userInput = radios[i].id;
     }  
-  };
+  }
 
+  // Display dish or entire meal based on user input 
   if (userInput === 'entire-meal') {
-    entireMeal = food['main'][(getRandomNumber(food['main']))] + 
-      " with a side of " +
-      food['side'][(getRandomNumber(food['side']))] + 
-      " and " +  
-      food['dessert'][(getRandomNumber(food['dessert']))] + " for dessert!"
-
+    randomMain = food.main[(getRandomNumber(food.main))];
+    randomSide = food.side[(getRandomNumber(food.side))];
+    randomDessert = food.dessert[(getRandomNumber(food.dessert))];  
+    
+    entireMeal = `${randomMain} with a side of ${randomSide} and ${randomDessert} for dessert!`;
     document.querySelector('h2').innerHTML = entireMeal;  
-
   } else {
     randomNum = getRandomNumber(food[userInput]);
     randomDish = food[userInput][randomNum];
     dishString = `${randomDish}!`;
     document.querySelector('h2').innerHTML = dishString;
   }
+  
+  // Hide cook-pot picture and display suggestion message 
   document.querySelector('.suggestion').style.display = 'block';
   document.querySelector('.img2').style.display = 'none'; 
-};
+}
 
 

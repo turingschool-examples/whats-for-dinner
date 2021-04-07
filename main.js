6029a216fedd4fd373d51696d8d2fdaebfdc3b36
StevenTheBearded <>
2021-04-07 08:35:55 -0400

    event bubbling

--- main.js
+++ main.js
@@ -6,24 +6,35 @@ var dessertRadio = document.querySelector('#dessert')
 var footer = document.querySelector('#foot')
 var crockpot = document.querySelector('div')
 
+var main = document.querySelector('main')
+
 var recipeType = document.querySelector('#recipeType')
 var recipeName = document.querySelector('#recipeName')
 
 var addRecipeButton = document.querySelector('#addRecipe')
 var letsCookButton = document.querySelector('#letsCook')
 var addNewButton = document.querySelector('#addNew')
+var viewFavoritesButton = document.querySelector('#viewFavorites')
+// var favoriteButton = document.querySelector('#favorite')
 
 //Event Listeners
 letsCookButton.addEventListener('click', createRecipe);
 addRecipeButton.addEventListener('click', displayForm);
 addNewButton.addEventListener('click', addRecipe)
+viewFavoritesButton.addEventListener('click', addFavorite, false);
 
 //Event handlers
+function addFavorite(){
+
+}
+
 function displayRecipe(recipe){
   event.preventDefault()
   crockpot.innerHTML = `<div>
   <p>You should make:</p>
   <h2>${recipe}</h2>
+  <button id="favorite">Favorite</button>
+  <button id="viewFavorites">View Favorites</button>
   </div>`
 }
 
@@ -36,6 +47,9 @@ function addRecipe(){
     meal.mains.push(recipeName.value)
   } else if (recipeType.value === 'Dessert'){
     meal.desserts.push(recipeName.value)
+  } else {
+    var type = recipeType.value
+    meal.type = [recipeName.value]
   }
   displayRecipe(recipeName.value)
 }

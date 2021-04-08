e86a0e9b57b904b2ef04b597ceee37d8f9d63fb3
Claire Fields <clairefields15@gmail.com>
2021-04-08 14:15:09 -0600

    Add event listeners and functions to toggle between home and fav recipe views

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-
 //////////// DOM ELEMENTS  ////////////
-var sideButton = document.querySelector('#side')
-var mainDishButton = document.querySelector('#main-dish')
-var dessertButton = document.querySelector('#dessert')
-//var entireMealButton = document.querySelector('#entire-meal')
-var letsCookButton = document.querySelector('#lets-cook')
-var cookpotView = document.querySelector('#cookpot-view')
-var youShouldMakeView = document.querySelector('#you-should-make')
-var formOutput = document.querySelector('#form-output')
-var clearButton = document.querySelector('#clear')
+var sideButton = document.querySelector('#side');
+var mainDishButton = document.querySelector('#main-dish');
+var dessertButton = document.querySelector('#dessert');
+//var entireMealButton = document.querySelector('#entire-meal');
+var letsCookButton = document.querySelector('#lets-cook');
+var cookpotView = document.querySelector('#cookpot-view');
+var youShouldMakeView = document.querySelector('#you-should-make');
+var formOutput = document.querySelector('#form-output');
+var clearButton = document.querySelector('#clear');
+var viewFavoritesButton = document.querySelector('.view-favs');
+var addToFavoritesButton = document.querySelector('.add-favorite');
+var homeButton = document.querySelector('.go-home');
+var leftBox = document.querySelector('.whats-cooking');
+var rightBox = document.querySelector('.right-box')
+var favRecipesBox = document.querySelector('.show-fav-recipes')
 
 //////////// VARIABLES ////////////
 
@@ -16,6 +21,9 @@ var clearButton = document.querySelector('#clear')
 //////////// EVENT LISTENERS ////////////
 letsCookButton.addEventListener('click', displayRandomFood)
 clearButton.addEventListener('click', clearForm)
+viewFavoritesButton.addEventListener('click', viewFavoritesPage)
+addToFavoritesButton.addEventListener('click', addToFavorites)
+homeButton.addEventListener('click', goHome)
 
 //////////// FUNCTIONS AND EVENT HANDLERS ////////////
 function randomFoods(foodType) {
@@ -44,4 +52,33 @@ function clearForm() {
   mainDishButton.checked = false
   dessertButton.checked = false
   sideButton.checked = false
+}
+
+function viewFavoritesPage() {
+  leftBox.classList.add('hidden');
+  rightBox.classList.add('hidden');
+  favRecipesBox.classList.remove('hidden');
+  homeButton.classList.remove('hidden')
+
+  //page should display all of their favorite recipes in the ul
+  //need to add li elements
+}
+
+function addToFavorites() {
+  //when addToFavoritesButton is clicked, the current recipe should be added to the favorites array
+
+
+}
+
+function goHome() {
+  leftBox.classList.remove('hidden');
+  rightBox.classList.remove('hidden');
+  favRecipesBox.classList.add('hidden');
+  homeButton.classList.add('hidden')
+}
+
+function removeFavorite() {
+  //Users should be able to remove a recipe from their list of favorites, by clicking a button.
+  //change the DOM and data model
+  //need to add this button to the html
 }

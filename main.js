c466cd7398f78172535bc9fb3a1d3a4389335088
Em Orendorff <em.orendorff@gmail.com>
2021-04-07 21:42:47 -0500

    Add variables to access html elements

---
+++ main.js
@@ -0,0 +1,6 @@
+//-------------QUERY SELECTORS----------------------//
+var sideSelector = document.querySelector("#side");
+var mainDishSelector = document.querySelector("#main-dish");
+var dessertSelector = document.querySelector("#dessert");
+var entireMealSelector = document.querySelector("entireMealSelector");
+var clearButton = document.querySelector("#clear-button");

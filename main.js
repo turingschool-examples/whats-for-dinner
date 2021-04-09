89adb1c54644bd3fc67890554e1b97b140f69214
Hayley Witherell <hayleywitherell@gmail.com>
2021-04-09 13:58:21 -0600

    Fix typos, import js file

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-///////// VARIABLES /////////
-
-var letsCookButton = ".letscook"
-var transparentBoxRight = ".transparent-box-right"
+///////// ARRAYS /////////
 
 var sidesArray = ["Miso Glazed Carrots", "Coleslaw", "Garden Salad", "Crispy Potatoes", "Sweet Potato Tots", "Coconut Rice", "Caeser Salad", "Shrimp Summer Rolls", "Garlic Butter Mushrooms", "Hush Puppies"]
 var mainArray = ["Spaghetti and Meatballs", "Pineapple Chicken", "Shakshuka", "Thai Yellow Curry", "Bibimbap", "Chicken Parmesean", "Butternut Squash SoupBBQ Chicken Burgers", "Ramen", "Empanadas", "Chicken Fried Rice", "Sheet Pan Fajitas", "Margarita Pizza"]
 var dessertArray = ["Apple Pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler", "Cheesecake", "Funfetti Cake", "Baklava", "Flan", "Macarons", "Macaroons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"]
 
-var recipe = ".recipe"
-var cookpot = ".cookpot"
-var hidden = "hidden"
+///////// QUERY SELECTORS /////////
+
+var letsCookButton = document.querySelector(".letscook")
+var transparentBoxRight = document.querySelector(".transparent-box-right")
+
+var recipe = document.querySelector(".recipe")
+var cookpot = document.querySelector(".cookpot")
+var hidden = document.querySelector("hidden")
 
 ///////// EVENT LISTENERS /////////
 
-letsCookButton.addEventListener("click", letsCook;
+letsCookButton.addEventListener("click", letsCook;)
 
 ///////// FUNCTIONS /////////
 

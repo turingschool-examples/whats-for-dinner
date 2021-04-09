78c9102835955d7e5b977952811dcfdd1bb06287
Shayan Golafshani <shayangolafsh@gmail.com>
2021-04-09 08:23:33 -0600

    Add pseudocode for showDish function

--- main.js
+++ main.js
@@ -49,10 +49,39 @@ var desserts = [
 ]
 
 
-//add Query selectors
+//Add Query-Selectors
 var sideRadial = document.querySelector('#js-side');
 var mainDishRadial = document.querySelector('#js-main-dish');
 var dessertRadial = document.querySelector('#js-dessert');
 var entireMealRadial = document.querySelector('#js-entire-meal');
 var cookButton = document.querySelector('#js-cook-btn');
-var addRecipeButton = document.querySelector('#js-add-recipe-btn');  
+var addRecipeButton = document.querySelector('#js-add-recipe-btn');
+
+//Add Event-Listeners
+cookButton.addEventListener('click', showDish)
+
+//I'm going to check what dish is passed into this function
+//then I'm going to return a random slot from the array matching the dish-type
+//then I'm going to remove the pot img
+//then I'm going to display the text!
+
+//var dishType = the value of the radial button selected
+
+function showDish(dishType){
+
+}
+
+
+
+
+
+
+
+
+function getRandomElement(array) {
+  return array[getRandomIndex(array)]
+};
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};

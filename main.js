edf923454b0818b0751af3aa38b64ad1070ba81b
Hayley Witherell <hayleywitherell@gmail.com>
2021-04-08 20:09:46 -0600

    Work on eventlisteners

--- main.js
+++ main.js
@@ -1,14 +1,52 @@
+//variables
+
 var letsCookButton = ".lets-cook"
+var transparentBoxRight = ".transparent-box-right"
 
-document.getElementById('prob2').addEventListener('change', msg1);
+var sidesArray = ["Miso Glazed Carrots", "Coleslaw", "Garden Salad", "Crispy Potatoes", "Sweet Potato Tots", "Coconut Rice", "Caeser Salad", "Shrimp Summer Rolls", "Garlic Butter Mushrooms", "Hush Puppies"]
+var mainArray = ["Spaghetti and Meatballs", "Pineapple Chicken", "Shakshuka", "Thai Yellow Curry", "Bibimbap", "Chicken Parmesean", "Butternut Squash SoupBBQ Chicken Burgers", "Ramen", "Empanadas", "Chicken Fried Rice", "Sheet Pan Fajitas", "Margarita Pizza"]
+var dessertArray = ["Apple Pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler", "Cheesecake", "Funfetti Cake", "Baklava", "Flan", "Macarons", "Macaroons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"]
 
+var recipe = ".recipe"
+var cookpot = ".cookpot"
+var hidden = "hidden"
 
+// event listeners
 
-if (document.querySelector('input[name="contract_duration"]')) {
-  document.querySelectorAll('input[name="contract_duration"]').forEach((elem) => {
-    elem.addEventListener("change", function(event) {
-      var item = event.target.value;
-      console.log(item);
-    });
-  });
+letsCookButton.addEventListener("click", letsCook);
+
+//functions
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random()*array.length);
+};
+
+function getRandomSide() {
+	var randomSideIndex = getRandomIndex(sidesArray);
+  return sidesArray[randomSideIndex];
+}
+
+function getRandomMain() {
+	var randomMainIndex = getRandomIndex(mainArray);
+  return mainArray[randomMainIndex];
+}
+
+function getRandomDessert() {
+	var randomDessertIndex = getRandomIndex(dessertArray);
+  return dessertArray[randomDessertIndex];
+}
+
+function letsCook() {
+  cookpot.classList.add("hidden");
+  recipe.classList.remove("hidden");
+  var circleSide = document.getElementById("circle-side");
+  var circleMain = document.getElementById("circle-main");
+  var circleDessert = document.getElementById("circle-dessert");
+  if (circleSide.checked === true) {
+    getRandomSide()
+  } else if (circleMain.checked === true) {
+    getRandomMain()
+  } else if (circleDessert.checked === true) {
+    getRandomDessert()
+  }
 }

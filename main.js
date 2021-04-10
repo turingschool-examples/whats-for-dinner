3df61daf240e2de2fd43f0eaf3f8ee08f419f01d
Hayley Witherell <hayleywitherell@gmail.com>
2021-04-10 11:28:54 -0600

    Complete Iteration1

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 ///////// ARRAYS /////////
 
-var sidesArray = ["Miso Glazed Carrots", "Coleslaw", "Garden Salad", "Crispy Potatoes", "Sweet Potato Tots", "Coconut Rice", "Caeser Salad", "Shrimp Summer Rolls", "Garlic Butter Mushrooms", "Hush Puppies"]
-var mainArray = ["Pineapple Chicken", "Shakshuka", "Thai Yellow Curry", "Bibimbap", "Chicken Parmesean", "Butternut Squash Soup", "BBQ Chicken Burgers", "Ramen", "Empanadas", "Chicken Fried Rice", "Sheet Pan Fajitas", "Margarita Pizza"]
-var dessertArray = ["Apple Pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler", "Cheesecake", "Funfetti Cake", "Baklava", "Flan", "Macarons", "Macaroons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"]
+var sidesArray = ["French Fries", "Mashed Potatoes", "Glazed Carrots", "Coleslaw", "Garden Salad", "Crispy Potatoes", "Sweet Potato Tots", "Coconut Rice", "Caeser Salad", "Egg Rolls", "Garlic Bread", "Hush Puppies"]
+var mainArray = ["Lasagna", "Sushi", "Beef Stew", "Tacos", "Pineapple Chicken", "Shakshuka", "Bibimbap", "Ramen", "Empanadas", "Chicken Fried Rice", "Margarita Pizza"]
+var dessertArray = ["Cookies", "Ice Cream", "Apple Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler", "Cheesecake", "Funfetti Cake", "Baklava", "Flan", "Macarons", "Macaroons", "Cupcakes", "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"]
 
 ///////// QUERY SELECTORS /////////
 

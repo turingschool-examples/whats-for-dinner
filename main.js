1adad37c3329fa5e5bca1dd290242b453edc1adb
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-04-09 19:56:12 -0600

    selectRandomDish functionality

--- main.js
+++ main.js
@@ -12,9 +12,55 @@ var dessertRadio = document.querySelector("#dessert");
 var entireMealRadio = document.querySelector("#entire-meal");
 var letsCookButton = document.querySelector("#lets-cook");
 
+var cookpotImage = document.querySelector("#cookpot");
+
+var cookpotCard = document.querySelector("#cookpot-card")
 
 //Event listeners
-
+letsCookButton.addEventListener("click", selectRandomDish)
 
 
 //Event handlers/functions
+function preventDefault() {
+  event.preventDefault()
+}
+
+function randomIndex(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+function hideCookpot() {
+  cookpotImage.classList.add("hidden");
+}
+
+function displayCookpotCard() {
+  cookpotCard.classList.remove("hidden");
+}
+
+function selectRandomDish() {
+  var sideDish;
+  var mainDish;
+  var dessertDish;
+  if (sideRadio.checked) {
+    preventDefault();
+    hideCookpot();
+    displayCookpotCard();
+    sideDish = randomIndex(sides);
+    cookpotCard.innerHTML = `<h4 class="">You should make:</h4>
+          <h1>${sideDish}</h1>`
+  } else if (mainRadio.checked) {
+    preventDefault();
+    hideCookpot();
+    displayCookpotCard();
+    mainDish = randomIndex(mains);
+    cookpotCard.innerHTML = `<h4 class="">You should make:</h4>
+          <h1>${mainDish}</h1>`
+  } else if (dessertRadio.checked) {
+    preventDefault();
+    hideCookpot();
+    displayCookpotCard();
+    dessertDish = randomIndex(desserts);
+    cookpotCard.innerHTML = `<h4 class="">You should make:</h4>
+          <h1>${dessertDish}</h1>`
+    }
+  } 

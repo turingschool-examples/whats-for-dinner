ee54b5f4cb1777e1d8c1ebb13fd66591e989474e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-04-10 14:53:39 -0600

    Added functionality so user can add recipe have it appear on the recipe container

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 // Variables
   // Buttons
-var cookButton = document.querySelector('.cook-btn')
+var cookBtn= document.querySelector('.cook-btn')
   // Hide/Show
 var potLogo = document.querySelector('.pot')
 var dish = document.querySelector('.dish')
@@ -10,22 +10,35 @@ var mainDish = document.querySelector('.main-dish')
 var dessert = document.querySelector('.dessert')
 var entireMeal = document.querySelector('.entire-meal')
 var suggestion = document.querySelector('h3')
-
-
+var clearBtn = document.querySelector('.clear-btn')
+var recipeBtn = document.querySelector('.recipe-btn')
+  // Add Recipe Form
+var createBtn = document.getElementById('add-btn')
+var recipeName = document.getElementById('recipe-name')
+var recipeType = document.getElementById('recipe-type')
 
 // Event Listeners
-cookButton.addEventListener('click', randomizeDish)
+cookBtn.addEventListener('click', randomizeDish)
+clearBtn.addEventListener('click', clearPage)
+createBtn.addEventListener('click', addRecipe)
 
 // Event Handerlers
 function randomizeDish(e) {
-e.preventDefault()
+if (dish.innerText !== 'Pick a dish!') {
+show(clearBtn)
+show(suggestion)
+}
 hide(potLogo)
-reveal(suggestion)
-reveal(dish)
-return side.checked ? dish.innerText = randomizeIndex(sides)
+show(dish)
+e.preventDefault()
+
+
+return (side.checked) ? dish.innerText = randomizeIndex(sides)
  : mainDish.checked ? dish.innerText = randomizeIndex(mains)
  : dessert.checked ? dish.innerText = randomizeIndex(desserts) 
- : dish.innerText = `${randomizeIndex(sides)} with a side of ${randomizeIndex(mains)} and ${randomizeIndex(desserts)} for dessert!`
+ : entireMeal.checked ? dish.innerText = `${randomizeIndex(sides)} with a side of ${randomizeIndex(mains)} and ${randomizeIndex(desserts)} for dessert!` 
+ : console.log('randomizeDish function error')
+ 
 }
 
 
@@ -33,10 +46,33 @@ function hide(e) {
   e.classList.add('hidden')
 }
 
-function reveal(e) {
+function show(e) {
   e.classList.remove('hidden')
 }
 
 function randomizeIndex(dishList) {
   return dishList[Math.floor(Math.random() * dishList.length)];
+}
+
+function clearPage() {
+
+if (dish.innerText !== 'Pick a dish!') {Î
+show(potLogo)
+hide(clearBtn)
+hide(suggestion)
+hide(dish)
+  }
+}
+
+function addRecipe(e) {
+  e.preventDefault()
+  // if(!recipeName.value === "") { 
+    hide(potLogo)
+    show(clearBtn)
+    show(suggestion)
+    show(dish)
+    dish.innerText = recipeName.value
+    e.preventDefault()
+  // }
+
 }

53b72e08177290ccac9b8b94e3574fd804a700c0
Em Orendorff <em.orendorff@gmail.com>
2021-04-10 14:07:20 -0500

    Change if statements in generateRandomDish function and refractor html/css

--- main.js
+++ main.js
@@ -2,45 +2,54 @@
 var sideSelector = document.querySelector("#side");
 var mainDishSelector = document.querySelector("#main-dish");
 var dessertSelector = document.querySelector("#dessert");
-var entireMealSelector = document.querySelector("entireMealSelector");
+var entireMealSelector = document.querySelector("#entire-meal");
 var userLetsCook = document.querySelector(".lets-cook");
 var clearButton = document.querySelector(".clear-button");
 var cookpot = document.querySelector(".cookpot");
 var shouldMake = document.querySelector(".you-should-make");
-var dishOption = document.querySelector("dish-option")
+var dishOptions = document.querySelector(".dish-options")
+
+
 
 
 //---------Event Listeners -------------------//
-// window.addEventListener('DOMContentLoaded', showDefaultView);
-userLetsCook.addEventListener('click', displayRandomDish);
+// window.addEventListener('DOMContentLoaded', displayRandomDish);
+userLetsCook.addEventListener('click', generateRandomDish);
 
 //-------Functions-------------------//
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 };
 
-function generateRandomDish() {
-  event.preventDefault();
-  var randomSide = side[getRandomIndex(side)];
-  var randomMainDish = mainDish[getRandomIndex(mainDish)];
-  var randomDessert = dessert[getRandomIndex(randomDessert)];
-
-  if(sideSelector.checked === true) {
-    dishOption.innerHTML = `${randomSide}!`
-    displayRandomDish()
-  }
-  if(mainDishSelector.checked === true) {
-    dishOption.innerHTML = `${randomMainDish}!`
-    displayRandomDish()
-  }
-  if(dessertSelector.checked === true) {
-    dishOption.innerHTML = `${randomDessert}!`
-    displayRandomDish()
-  }
-};
-
 function displayRandomDish() {
   clearButton.classList.remove("hidden");
   cookpot.classList.add("hidden");
-  shouldMake.classList.remove("hidden")
+  shouldMake.classList.remove("hidden");
+  dishOptions.classList.remove("hidden");
+};
+
+// // var randomSide = side[getRandomIndex(side)];
+// // var randomMainDish = mainDish[getRandomIndex(mainDish)];
+// var randomDessert = dessert[getRandomIndex(dessert)];
+
+
+function generateRandomDish() {
+  event.preventDefault();
+
+  if(sideSelector.checked) {
+    dishOptions.innerText = `${side[getRandomIndex(side)]}!`;
+    displayRandomDish()
+  }
+  if(mainDishSelector.checked) {
+    dishOptions.innerText = `${randomMainDish = mainDish[getRandomIndex(mainDish)]}!`;
+    displayRandomDish()
+  }
+  if(dessertSelector.checked) {
+    dishOptions.innerText = `${dessert[getRandomIndex(dessert)]}!`;
+    displayRandomDish()
+  }
+  if(entireMealSelector.checked) {
+    dishOptions.innerText = `${side[getRandomIndex(side)]}, ${randomMainDish = mainDish[getRandomIndex(mainDish)]}, and ${dessert[getRandomIndex(dessert)]}!`;
+    displayRandomDish()
+  }
 };

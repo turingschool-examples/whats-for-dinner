c9a5585c8b36ab4ef86db01839875385d0c1cbf2
Erica <>
2021-04-10 19:20:10 -0600

    Fix functionality on radio buttons and lets cook

--- main.js
+++ main.js
@@ -3,23 +3,25 @@ var food;
 
 //Buttons
 var letsCookBtn = document.querySelector('.lets-cook-btn');
-var clearBtn = document.querySelector('.clear-btn')
+var clearBtn = document.querySelector('.clear-btn');
 
 //Radio Buttons
 var sideDishValue = document.querySelector('#side');
 var mainDishValue = document.querySelector('#main');
 var dessertValue = document.querySelector('#dessert');
+var entireValue = document.querySelector('#entire-meal');
 
 //Sections
 var letsCookBox = document.querySelector('#lets-cook-form');
-var cookPotBox = document.querySelector('#cookpot');
+var cookPotBox = document.querySelector('.right-box');
 
 //Images
 var cookPotImg = document.querySelector('.cookpot-img');
 
 //Event Listeners
 letsCookBtn.addEventListener("click", letsCook);
-clearBtn.addEventListener("click", clearFoodBox);
+cookPotBox.addEventListener("click", clearFoodBox);
+letsCookBox.addEventListener("click", disableLetsCookBtn);
 
 //Functions
 function getFoodItem(array) {
@@ -27,35 +29,50 @@ function getFoodItem(array) {
 }
 
 function letsCook() {
-    if (sideDishValue.checked) {
-      food = sides[getFoodItem(sides)];
-      sideDishValue.checked = false;
-    } else if (mainDishValue.checked) {
-      food = mains[getFoodItem(mains)];
-      mainDishValue.checked = false;
-    } else if (dessertValue.checked) {
-      food = desserts[getFoodItem(desserts)];
-      dessertValue.checked = false;
-    }
-      cookPotImg.classList.add('hidden');
-      clearBtn.classList.remove('hidden');
-      displayFood(food);
+  if (sideDishValue.checked) {
+    food = sides[getFoodItem(sides)];
+    sideDishValue.checked = false;
+  } else if (mainDishValue.checked) {
+    food = mains[getFoodItem(mains)];
+    mainDishValue.checked = false;
+  } else if (dessertValue.checked) {
+    food = desserts[getFoodItem(desserts)];
+    dessertValue.checked = false;
+  } else {
+    food = getEntireMeal();
+    entireValue.checked = false;
+  }
+  displayFood(food);
 }
 
 function displayFood(food) {
-  // cookPotBox.insertAdjacentHTML = ("afterbegin", `
-  // <p><em>You should make:</em></p>
-  //   <br>
-  //   <br>
-  //   <h1 class="meal-suggestion">${food}</h1>`);
+  cookPotBox.innerHTML = `
+  <h2><em>You should make:</em></h2>
+  <h1 class="meal-suggestion">${food}</h1>
+  <button class="clear-btn" id="clear" type="button">CLEAR</button>`
+}
+
+function getEntireMeal() {
+  var side = sides[getFoodItem(sides)];
+  var main = mains[getFoodItem(mains)];
+  var dessert = desserts[getFoodItem(desserts)];
   cookPotBox.innerHTML = `
     <p><em>You should make:</em></p>
-    <br>
-    <br>
-    <h1 class="meal-suggestion">${food}</h1>`
+    <br><h1 class="whole-meal">${main} with a side of ${side} and ${dessert} for dessert.</h1>
+    <button class="clear-btn" id="clear" type="button">CLEAR</button>`;
+  letsCook();
 }
 
 function clearFoodBox() {
-  cookPotBox.innerHTML = '';
-  clearBtn.classList.add('hidden');
+  if (event.target.id === "clear") {
+    cookPotBox.innerHTML = '';
+  } cookPotBox.innerHTML = `<img class="cookpot-img" src="assets/cookpot.svg" alt="cookpot logo">`
+}
+
+function disableLetsCookBtn() {
+  if (sideDishValue.checked || mainDishValue.checked || dessertValue.checked || entireValue.checked) {
+    letsCookBtn.disabled = false;
+  } else {
+    letsCookBtn.disabled = true;
+  }
 }

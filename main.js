f7ad7a5db67b8f6ba6e2966de76ae6353d66bac9
Lourdes Mendoza <lourd.mendoza@gmail.com>
2021-04-10 19:06:37 -0600

    Refactor functions for SRP and DRY code, add function to deselect buttons

--- main.js
+++ main.js
@@ -2,12 +2,11 @@
 var sideBtn = document.querySelector('#side');
 var mainBtn = document.querySelector('#main-dish');
 var dessertBtn = document.querySelector('#dessert-button');
-var letsCookBtn = document.querySelector('.cook-button');
 var entireMealBtn = document.querySelector('#entire-meal');
+var letsCookBtn = document.querySelector('.cook-button');
 var cookingPot = document.querySelector('.cooking-pot');
 var foodDisplayHeading = document.querySelector('.food-display-heading');
 var foodText = document.querySelector('.food-display-text');
-var addRecipeBtn = document.querySelector('.add-recipe');
 var clearBtn = document.querySelector('.clear-button')
 
 
@@ -16,24 +15,38 @@ letsCookBtn.addEventListener('click', preventEvent);
 letsCookBtn.addEventListener('click', displayRandomFood);
 clearBtn.addEventListener('click', clearFood);
 
+//Functions
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
 
 function preventEvent(e) {
     e.preventDefault();
 }
 
-function clearFood(){
+
+function clearFood() {
   cookingPot.classList.remove('hidden');
   foodDisplayHeading.classList.add('hidden');
   foodText.classList.add('hidden');
   clearBtn.classList.add('hidden');
+  clearRadioButtons();
 }
+
+
+function clearRadioButtons() {
+  sideBtn.checked = false;
+  mainBtn.checked = false;
+  dessertBtn.checked = false;
+  entireMealBtn.checked = false;
+}
+
 
 function hideCookingPot() {
   cookingPot.classList.add('hidden');
 }
+
 
 function showRecipeSection() {
   foodDisplayHeading.classList.remove('hidden');
@@ -41,47 +54,24 @@ function showRecipeSection() {
   clearBtn.classList.remove('hidden');
 }
 
-function createSide() {
-  if(sideBtn.value) {
-    hideCookingPot();
-    showRecipeSection();
-    foodText.innerText = `${sides[getRandomIndex(sides)]}!`
-  }
-}
-
-function createMain() {
-  if(mainBtn.value) {
-    hideCookingPot();
-    showRecipeSection();
-    foodText.innerText = `${mains[getRandomIndex(mains)]}!`
-    }
-}
-
-function createDessert() {
-  if(dessertBtn.value) {
-    hideCookingPot();
-    showRecipeSection();
-    foodText.innerText = `${desserts[getRandomIndex(desserts)]}!`
-  }
-}
-
-function createMeal() {
-  if(entireMealBtn.value) {
-    hideCookingPot();
-    showRecipeSection();
-    foodText.innerText = `${mains[getRandomIndex(mains)]} with a side of ${sides[getRandomIndex(sides)]} and ${desserts[getRandomIndex(desserts)]} for dessert!`
-
-  }
-}
 
 function displayRandomFood() {
   if(sideBtn.checked) {
-    createSide();
+    hideCookingPot();
+    showRecipeSection();
+    foodText.innerText = `${sides[getRandomIndex(sides)]}!`
   } else if(mainBtn.checked) {
-    createMain();
+    hideCookingPot();
+    showRecipeSection();
+    foodText.innerText = `${mains[getRandomIndex(mains)]}!`
   } else if(dessertBtn.checked) {
-    createDessert();
+    hideCookingPot();
+    showRecipeSection();
+    foodText.innerText = `${desserts[getRandomIndex(desserts)]}!`
   } else if(entireMealBtn.checked) {
-    createMeal();
-  }
+    hideCookingPot();
+    showRecipeSection();
+    foodText.innerText = `${mains[getRandomIndex(mains)]} with a side of ${sides[getRandomIndex(sides)]} and ${desserts[getRandomIndex(desserts)]} for dessert!`
+  } else
+    alert("Please make a selection.");
 }

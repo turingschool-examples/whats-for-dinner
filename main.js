8094519797dd37f0dbc36b74aa2985d27343bb5c
janitastic <76501557+janitastic@users.noreply.github.com>
2021-04-10 20:11:03 -0700

    Modify CSS for Entire Meal

--- main.js
+++ main.js
@@ -10,8 +10,10 @@ var entireMealRadio = document.querySelector('#entire-meal');
 var letsCookBtn = document.querySelector('#cook');
 var clearBtn = document.querySelector('#clear');
 var cookpotImg = document.querySelector('#cookpot');
+// var shouldMake = document.querySelector('.meal-only');
 var shouldMake = document.querySelector('#make-title');
 var mealText = document.querySelector('#meal-text');
+var entireMealText = document.querySelector('#entire-meal-text');
 
 // EVENT LISTENERS
 // letsCookBtn.addEventListener("click", displayMeal();
@@ -21,6 +23,7 @@ clearBtn.addEventListener("click", potView);
 // EVENT HANDLERS
 
 function displayMeal() {
+  shouldMake.innerText = 'You should make:';
   if (sideRadio.checked) {
     getRandomSide();
   } else if (mainRadio.checked) {
@@ -42,10 +45,19 @@ function preventDefault() {
 // }
 
 // MEAL CONTAINER VIEW FUNCTIONS //
-function recipeView() {
+function mealView() {
   cookpotImg.classList.add("hidden");
   shouldMake.classList.remove("hidden");
   mealText.classList.remove("hidden");
+  entireMealText.classList.add("hidden");
+  clearBtn.classList.remove("hidden");
+}
+
+function entireMealView() {
+  cookpotImg.classList.add("hidden");
+  shouldMake.classList.remove("hidden");
+  mealText.classList.add("hidden");
+  entireMealText.classList.remove("hidden");
   clearBtn.classList.remove("hidden");
 }
 
@@ -65,7 +77,7 @@ function getRandomIndex(array) {
 function getRandomSide() {
   preventDefault();
   if(sideRadio.value) {
-    recipeView();
+    mealView();
     mealText.innerText = `${sides[getRandomIndex(sides)]}!`;
   }
 }
@@ -73,7 +85,7 @@ function getRandomSide() {
 function getRandomMain() {
   preventDefault();
   if(mainRadio.value) {
-    recipeView();
+    mealView();
     mealText.innerText = `${mains[getRandomIndex(mains)]}!`;
   }
 }
@@ -81,7 +93,7 @@ function getRandomMain() {
 function getRandomDessert() {
   preventDefault();
   if(dessertRadio.value) {
-    recipeView();
+    mealView();
     mealText.innerText = `${desserts[getRandomIndex(desserts)]}!`;
   }
 }
@@ -89,7 +101,7 @@ function getRandomDessert() {
 function getRandomMeal() {
   preventDefault();
   if(entireMealRadio.value) {
-    recipeView();
-    mealText.innerText = `${mains[getRandomIndex(mains)]}, with a side of ${sides[getRandomIndex(sides)]}, and ${desserts[getRandomIndex(desserts)]} for dessert!`;
+    entireMealView();
+    entireMealText.innerText = `${mains[getRandomIndex(mains)]}, with a side of ${sides[getRandomIndex(sides)]}, and ${desserts[getRandomIndex(desserts)]} for dessert!`;
   }
 }

214b8ea5ee24210bed657d4ac15dc8c43ca148bd
Oscar Cortez <oacortez98@gmail.com>
2021-07-19 20:53:41 -0600

    Add file of data.js and created a array called sides

---
+++ data.js
@@ -0,0 +1,12 @@
+var sides = [
+"Miso Glazed Carrots",
+"Coleslaw",
+"Garden Salad",
+"Crispy Potatoes",
+"Sweet Potato Tots",
+"Coconut Rice",
+"Caeser Salad",
+"Shrimp Summer Rolls",
+"Garlic Butter Mushrooms",
+"Hush Puppies"
+];

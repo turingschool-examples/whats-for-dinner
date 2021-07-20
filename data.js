c226753e14b585d9da71f11c62c053f67c08122e
Oscar Cortez <oacortez98@gmail.com>
2021-07-19 21:00:44 -0600

    Add a desserts array

--- data.js
+++ data.js
@@ -27,3 +27,25 @@ var mains = [
 "Sheet Pan Fajitas",
 "Margarita Pizza"
 ];
+
+
+var desserts = [
+"Apple Pie",
+"Lemon Meringue Pie",
+"Black Forest Cake",
+"Banana Bread",
+"Peach Cobbler",
+"Cheesecake",
+"Funfetti Cake",
+"Baklava",
+"Flan",
+"Macarons",
+"Macaroons",
+"Chocolate Cupcakes",
+"Pavlova",
+"Pumpkin Pie",
+"Key Lime Pie",
+"Tart Tatin",
+"Croissants",
+"Eclairs"
+];

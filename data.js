98a80c8f030e613448f907d6b6948cac5b1a2989
Oscar Cortez <oacortez98@gmail.com>
2021-07-19 20:56:25 -0600

    Add a mains array

--- data.js
+++ data.js
@@ -10,3 +10,20 @@ var sides = [
 "Garlic Butter Mushrooms",
 "Hush Puppies"
 ];
+
+
+var mains = [
+"Spaghetti and Meatballs",
+"Pineapple Chicken",
+"Shakshuka",
+"Thai Yellow Curry",
+"Bibimbap",
+"Chicken Parmesean",
+"Butternut Squash Soup",
+"BBQ Chicken Burgers",
+"Ramen",
+"Empanadas",
+"Chicken Fried Rice",
+"Sheet Pan Fajitas",
+"Margarita Pizza"
+];

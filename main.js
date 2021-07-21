058b6f5cb2c103c2bc6366475f7f434b61b57233
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-07-21 09:39:01 -0600

    Add functionality to the Entire Meal option

--- main.js
+++ main.js
@@ -35,13 +35,6 @@ var desserts = [
     'Golfeados',
     'Strawberries and Cream'
 ];
-class EntireMeal {
-    constructor(mainDish, side, dessert) {
-        this.mainDish = mainDish;
-        this.side = side;
-        this.dessert = dessert;
-    };
-};
 
 var letsCookButton = document.querySelector('#lets-cook');
 var potImg = document.querySelector('#pot-img');
@@ -54,23 +47,26 @@ letsCookButton.addEventListener('click', letsCook);
 
 function letsCook(event) {
     event.preventDefault();
-    var getRandomSide = sides[getRandomNumber(sides)];
-    var getRandomDish = mainDishes[getRandomNumber(mainDishes)];
-    var getRandomDessert = desserts[getRandomNumber(desserts)];
+    var randomSide = sides[getRandomNumber(sides)];
+    var randomDish = mainDishes[getRandomNumber(mainDishes)];
+    var randomDessert = desserts[getRandomNumber(desserts)];
     var userInput;
     for (var i = 0; i < selectedMeals.length; i++) {
         if (selectedMeals[i].checked) {
             userInput = selectedMeals[i].id;
             if (userInput === 'side') {
                 potSection.innerHTML = `<p class="result-p"> You should make:</p>
-                <p class="meal-selection">${getRandomSide}!</p>`;
+                <p class="meal-selection">${randomSide}!</p>`;
             } else if (userInput === 'main-dish') {
                 potSection.innerHTML = `<p class="result-p"> You should make:</p>
-                <p class="meal-selection">${getRandomDish}!</p>`;
+                <p class="meal-selection">${randomDish}!</p>`;
             } else if (userInput === 'dessert') {
                 potSection.innerHTML = `<p class="result-p"> You should make:</p>
-                <p class="meal-selection">${getRandomDessert}!</p>`;
-            } else if ()        
+                <p class="meal-selection">${randomDessert}!</p>`;
+            } else if (userInput === 'entire-meal') {
+                potSection.innerHTML = `<p class="result-p"> You should make:</p>
+                <p class="meal-selection">${randomDish} with a side of ${randomSide} and ${randomDessert} for dessert!</p>`;
+            };       
         };
     };
 };

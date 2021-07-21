5a3ac8a293617a92be8af3c5706d4f34792ea11e
Markus Rossio <argitavis@gmail.com>
2021-07-20 22:33:48 -0500

    Add functionality for main dish and dessert options displaying random ideas for corrosponding buttons fixes bug that allowed users to click lets cook button multiple times after being given a recipe

--- assets/recipies.js
+++ assets/recipies.js
@@ -9,4 +9,40 @@ var side = [
 'Shrimp Summer Rolls',
 'Hush Puppies',
 'Garlic Butter Mushrooms',
+];
+
+var mainDish = [
+    'Spaghetti and Meatballs',
+    'Pineapple Chicken',
+    'Shakshuka',
+    'Thai Yellow Curry',
+    'Bibimbap',
+    'Chicken Parmesean',
+    'Butternut Squash Soup',
+    'BBQ Chicken Burgers',
+    'Ramen',
+    'Empanadas',
+    'Chicken Fried Rice',
+    'Sheet Pan Fajitas',
+    'Margarita Pizza',
+];
+
+var dessert = [
+    'Apple Pie',
+    'Lemon Meringue Pie',
+    'Black Forest Cake',
+    'Banana Bread',
+    'Peach Cobbler',
+    'Cheesecake',
+    'Funfetti Cake',
+    'Baklava',
+    'Flan',
+    'Macarons',
+    'Chocolate Cupcakes',
+    'Pavlova',
+    'Pumpkin Pie',
+    'Key Lime Pie',
+    'Tart Tatin',
+    'Croissants',
+    'Eclairs',
 ]
--- main.js
+++ main.js
@@ -2,9 +2,14 @@ var mealOptionForm = document.getElementById('meal-option-form');
 var radioButtons = document.getElementsByName('options');
 var cookPotImg = document.getElementById('cook-pot');
 var recipeText = document.getElementById('recipe-text');
-var shouldMakeText = document.getElementById('should-make-text')
+var shouldMakeText = document.getElementById('should-make-text');
+var letsCookBtn = document.getElementById('lets-cook-btn');
 
 mealOptionForm.addEventListener('submit', checkCheckedButton);
+
+function disableElement(element){
+    element.disabled = true;
+}
 
 
 function getRandomIndex(array) {
@@ -23,11 +28,17 @@ function checkCheckedButton(event){
                 toggleHidden(cookPotImg);
                 toggleHidden(shouldMakeText);
                 generateRecipeText(side);
-                console.log('checked: ', radioButtons[i].id);
+                disableElement(letsCookBtn);
             } else if(radioButtons[i].id === 'main-dish'){
-                console.log('checked: ', radioButtons[i].id)
+                toggleHidden(cookPotImg);
+                toggleHidden(shouldMakeText);
+                generateRecipeText(mainDish);
+                disableElement(letsCookBtn);
             } else if(radioButtons[i].id === 'dessert'){
-                console.log('checked: ', radioButtons[i].id)
+                toggleHidden(cookPotImg);
+                toggleHidden(shouldMakeText);
+                generateRecipeText(dessert);
+                disableElement(letsCookBtn);
             } else if(radioButtons[i].id === 'entire-meal'){
                 console.log('checked: ', radioButtons[i].id)
             }

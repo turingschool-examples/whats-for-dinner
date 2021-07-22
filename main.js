9659e06da7f451b4e2aee3653d77f11ed37745bb
Oscar Cortez <oacortez98@gmail.com>
2021-07-21 19:07:17 -0600

    Add comments

---
+++ main.js
@@ -0,0 +1,8 @@
+// var cookingPot.querySelector()
+// we are going to have 2 helper functions
+// 1 function is going to be "hide" and the other is going to be "show"
+// google - javascrip - "classList.add" and "classList.remove"
+// google - css = display: none
+// google - event.default
+
+}

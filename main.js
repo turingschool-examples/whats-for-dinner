9d4f6ff8c473aba1014f0ba7fdacd112e9b84f09
Jessica Organ <jorgan612@gmail.com>
2021-07-23 06:33:25 -0600

    Add pseudocode

---
+++ main.js
@@ -0,0 +1,6 @@
+//Pseudocode:
+//NOTE: Will need an event.preventDefault() method to prevent the
+// form from trying to send the user selection somewhere
+// for radio button:
+  //How do you make it to where only one of the buttons can be selected?
+  //Will there be a toggle option to reselect a new choice option?

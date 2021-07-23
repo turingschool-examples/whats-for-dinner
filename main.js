d075b6a808b4f6f6271a2c44950c1022ddf6abca
Oscar Cortez <oacortez98@gmail.com>
2021-07-22 18:42:43 -0600

    Add new blueprint comments

--- main.js
+++ main.js
@@ -5,8 +5,24 @@
 // google - css = display: none
 // google - event.default
 
-var sideDishRadioBtn = document.querySelector('#sideDishRadioBtn');
 
-if(sideDishRadioBtn.checked) {
-  // statment of sidedish array will come here 
-}
+// var sideDishInput = document.querySelector('#sideDishRadioBtn');
+// var mainDishInput =
+
+
+
+// if(sideDishRadioBtn.checked) {
+//   // statment of sidedish array will come here
+// }
+// var letsCookBtn = document.querySelector('#letsCookBtn');
+//
+// letsCookBtn.addEventListener('click', test);
+//
+// var test1 = document.querySelector('#test1')
+//
+// function test() {
+//   event.preventDefault();
+//   sideDishRadioBtn.value;
+//   console.log(sideDishRadioBtn.value);
+//   console.log("It Works!", test1.value);
+// }

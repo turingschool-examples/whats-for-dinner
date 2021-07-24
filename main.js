74993121ab95581a803759da9f7a3e36cac82875
Susanna Carey <susannaopal@gmail.com>
2021-07-24 07:47:22 -0400

    Map needed js and create object

---
+++ main.js
@@ -0,0 +1,22 @@
+//querySelectors
+
+
+
+//buttons
+
+
+
+//eventListeners
+
+
+//object for meal? or individual foods? do i even need this?
+// maybe to push into full meal array for if i get to the CYOA? A full meal array?
+//would it update to output a full array?
+
+var foodSuggestion {
+  sides: sides,
+  mains: mains,
+  desserts: desserts
+}
+
+//functions

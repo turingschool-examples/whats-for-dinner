1ab6e5729bc9f7637a7c931e046f8eb84fde8293
Susanna Carey <susannaopal@gmail.com>
2021-07-24 08:20:44 -0400

    Add meal class in addition to object

--- main.js
+++ main.js
@@ -14,12 +14,16 @@ letsCookBtn.addEventListener('click', letsCook)
 
 //object for meal? or individual foods? do i even need this?
 // maybe to push into full meal array for if i get to the CYOA? A full meal array?
-//would it update to output a full array? //do i need a class instead or at all?
+//would it update to output a full array?
+//do i need a class instead or at all?
+//i think i do because of there are several arrays running but can they just pass through the object?
 
 class Foods {
   constructor () {
     this.sides = this.sides;
-
+    this.mains = this.mains;
+    this.desserts = this.desserts;
+    this.entireMea = this.entireMeal;
   }
 }
 

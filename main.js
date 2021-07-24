1fe3a70eb8f7d1f76b56339479ff9cfc567b2c14
Logan Vincent <ldvsimp@gmail.com>
2021-07-24 11:57:19 -0600

    Add event listeners for dish buttons

---
+++ main.js
@@ -0,0 +1,29 @@
+var side = document.querySelector('#side');
+var mainDish = document.querySelector('#main-dish');
+var dessert = document.querySelector('#dessert');
+var entireMeal = document.querySelector('#entire-meal');
+
+
+
+side.addEventListener('click', showSideDish);
+mainDish.addEventListener('click', showMainDish);
+dessert.addEventListener('click', showDessert);
+entireMeal.addEventListener('click', showEntireMeal);
+
+
+
+function showSideDish() {
+
+};
+
+function showMainDish() {
+
+};
+
+function showDessert() {
+
+};
+
+function showEntireMeal() {
+
+};

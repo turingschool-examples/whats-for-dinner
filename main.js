af89992d4ebcf73fc360038472d0051c0ff33f1f
Susanna Carey <susannaopal@gmail.com>
2021-07-25 14:48:12 -0400

    Work on clear button functionality

--- main.js
+++ main.js
@@ -50,7 +50,6 @@ var desserts = [
 'Eclairs'
 ]
 
-var entireMeal = [];
 
 
 //querySelectors
@@ -65,93 +64,16 @@ var cookPot = document.querySelector('.cook-pot-img');
 var youMake = document.querySelector('.you-make');
 
 
+var youMake = document.querySelector('.you-make')
+
+var radioEntireBtn = document.querySelector('.entire-meal-option');
 
 
 var foodRecommendation = document.querySelector('.food-recommendation')
 
-//eventListeners
-//buttons
-//does the below button even need functionality? in the cyoa it  does
-// addRecipeBtn.addEventListener('click', addRecipe);
 
 letsCookBtn.addEventListener('click', letsCook);
-// radioSidesBtn.addEventListener('click', letsCook);
-// radioMainsBtn.addEventListener('click', letsCook);
-// radioDessertsBtn.addEventListener('click', letsCook);
-// radioEntireBtn.addEventListener('click', letsCook);
-//
-
-
-
-
-//object for meal? or individual foods? do i even need this?
-// maybe to push into full meal array for if i get to the CYOA? A full meal array?
-//would it update to output a full array?
-//do i need a class instead or at all?
-//i think i do because of there are several arrays running but can they just pass through the object?
-
-// class Foods {
-//   constructor (sides, mains, desserts, entireMeal) {
-//     this.sides = sides;
-//     this.mains = mains;
-//     this.desserts = desserts;
-//     //not sure if entireMeal can go into the constructor?
-//     //this is possible going to be its own object instead?
-//     //leave out of constructor ?
-//     //should it have its own object instance instead? how to do?
-//     this.entireMeal = entireMeal;
-//   }
-// };
-
-//do i need new vars for each?
-//object literal for all sans entireMeal
-//need to assign these to the class above?
-// var foodSuggestionSides
-//
-// var foodSuggestionsMains
-//
-// var foodSuggestionsDesserts
-//
-// var entireMeal = {
-//   sides: sides,
-//   mains: mains,
-//   desserts: desserts,
-//
-// }
-
-//functions
-
-//this function will grant access to one of the 3 arrays
-//eventually will give option also for whole meal selection
-//technically will need 3 functions w/in this for
-//sides - mains - desserts
-//if i get to cyoa can entire meal go in here also? i think so
-//will need to create a for loop i think to run through function
-//do i need to have a random math function like last weekend's project?
-
-// class Foods {
-//   constructor (sides, mains, desserts, entireMeal) {
-//     this.sides = sides;
-//     this.mains = mains;
-//     this.desserts = desserts;
-//     //not sure if entireMeal can go into the constructor?
-//     //this is possible going to be its own object instead?
-//     //leave out of constructor ?
-//     //should it have its own object instance instead? how to do?
-//     this.entireMeal = entireMeal;
-//   }
-// };
-
-// function selectRadioSides ()
-//
-// function selectRadioMains()
-//
-// function selectRadioDesserts()
-
-
-
-//do i need a random math function like we used last weekend?
-//can i use the same? does it make sense?
+// clearBnt.addEventListener('click', pushClearBtn);
 
 function checkRadioButton () {
   if(radioSidesBtn.checked) {
@@ -164,7 +86,7 @@ function checkRadioButton () {
     return desserts[getRandomIndex(desserts)];
 }
   if(radioEntireBtn.checked) {
-    return `${mains[getRandomIndex(mains)]} with a side of ${sides[getRandomIndex(sides)]} and ${desserts[getRandomIndex(desserts)]} for dessert!`
+    return `${mains[getRandomIndex(mains)]} with a side of ${sides[getRandomIndex(sides)]} and ${desserts[getRandomIndex(desserts)]} for dessert`
   }
 }
 
@@ -179,17 +101,29 @@ function checkRadioButton () {
          youMake.classList.remove('hidden');
        }
 
+function pushClearBtn () {
+  event.preventDefault()
+  clearBtn.classList.add('hidden');
+  foodRecommendation.classList.add('hidden');
+  cookPot.classList.remove('hidden');
+  youMake.classList.add('hidden');
+}
+
+
+
+
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 };
 
 // var youMake = document.querySelector('.you-make');
+var clearBtn= document.querySelector('.clear-btn')
 
-
-// clearBtn.classList.remove('hidden');
-var youMake = document.querySelector('.you-make')
-
-var radioEntireBtn = document.querySelector('.entire-meal-option');
+//
+// var youMake = document.querySelector('.you-make')
+//
+// var radioEntireBtn = document.querySelector('.entire-meal-option');
 
 //would this work like last project or does it need to be one function per food/meal type?
 

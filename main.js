e042424b9654045b03a151debf0b55737d7cb6a3
Logan Vincent <ldvsimp@gmail.com>
2021-07-25 19:38:26 -0600

    Add fcn to retrieve dish data from a rrays

--- main.js
+++ main.js
@@ -1,16 +1,84 @@
-var side = document.querySelector('#side');
-var mainDish = document.querySelector('#main-dish');
-var dessert = document.querySelector('#dessert');
-var entireMeal = document.querySelector('#entire-meal');
+var sideBtn = document.querySelector('#side');
+var mainDishBtn = document.querySelector('#main-dish');
+var dessertBtn = document.querySelector('#dessert');
+var entireMealBtn = document.querySelector('#entire-meal');
+var letsCookBtn = document.querySelector('#lets-cook');
+var recipeBox = document.querySelector('.recipe-box');
+var recipeText = document.querySelector('.recipe-text');
+
+var sideMenu = ['Miso Glazed Carrots',
+'Coleslaw',
+'Garden Salad',
+'Crispy Potatoes',
+'Sweet Potato Tots',
+'Coconut Rice',
+'Caeser Salad',
+'Shrimp Summer Rolls',
+'Garlic Butter Mushrooms',
+'Hush Puppies'];
+
+var mainMenu = ['Spaghetti and Meatballs',
+'Pineapple Chicken',
+'Shakshuka',
+'Thai Yellow Curry',
+'Bibimbap',
+'Chicken Parmesean',
+'Butternut Squash Soup',
+'BBQ Chicken Burgers',
+'Ramen',
+'Empanadas',
+'Chicken Fried Rice',
+'Sheet Pan Fajitas',
+'Margarita Pizza'];
+
+var dessertMenu = ['Apple Pie',
+'Lemon Meringue Pie',
+'Black Forest Cake',
+'Banana Bread',
+'Peach Cobbler',
+'Cheesecake',
+'Funfetti Cake',
+'Baklava',
+'Flan',
+'Macarons',
+'Macaroons',
+'Chocolate Cupcakes',
+'Pavlova',
+'Pumpkin Pie',
+'Key Lime Pie',
+'Tart Tatin',
+'Croissants',
+'Eclairs'];
+
+var entireMealMenu = [];
+var currentDish = [];
+
+// sideBtn.addEventListener('click', showSideDish);
+// mainDishBtn.addEventListener('click', showMainDish);
+// dessertBtn.addEventListener('click', showDessert);
+// entireMealBtn.addEventListener('click', showEntireMeal);
+letsCookBtn.addEventListener('click', displayDish);
 
 
+function getRandomIndex(array){
+  return Math.floor(Math.random() * array.length);
+};
 
-side.addEventListener('click', showSideDish);
-mainDish.addEventListener('click', showMainDish);
-dessert.addEventListener('click', showDessert);
-entireMeal.addEventListener('click', showEntireMeal);
+function getCurrentDish() {
+  console.log(sideBtn);
+  if (sideBtn) {
+    currentDish = sideMenu[getRandomIndex(sideMenu)];
+  } else if (mainDishBtn) {
+    currentDish = mainMenu[getRandomIndex(mainMenu)]
+  } else if (dessertBtn) {
+    currentDish = dessertMenu[getRandomIndex(dessertMenu)];
+  }
+}
 
-
+function displayDish() {
+  getCurrentDish();
+  recipeBox.innerHTML = `<p class='recipe-text'>${currentDish}</p>`;
+}
 
 function showSideDish() {
 

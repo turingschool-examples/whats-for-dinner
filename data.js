68bd6bdce0de308311935bfc12a504bdaebe2040
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-10-25 22:29:48 -0700

    random food funcitonality working

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-var sides = ["Miso Glazed Carrots", "Coleslaw","Garden Salad","Crispy Potatoes","Sweet Potato Tots","Coconut Rice","Caeser Salad","Shrimp Summer Rolls","Garlic Butter Mushrooms","Hush,Puppies"];
+var sides = ["Miso Glazed Carrots", "Coleslaw","Garden Salad","Crispy Potatoes","Sweet Potato Tots","Coconut Rice","Caeser Salad","Shrimp Summer Rolls","Garlic Butter Mushrooms","Hush Puppies"];
 
 var mains = ["Spaghetti and Meatballs","Pineapple Chicken","Shakshuka","Thai Yellow Curry","Bibimbap","Chicken Parmesean","Butternut Squash Soup","BBQ Chicken Burgers","Ramen","Empanadas","Chicken Fried Rice","Sheet Pan Fajitas","Margarita Pizza"];
 
--- main.js
+++ main.js
@@ -5,8 +5,7 @@ var addRecipeButton = document.querySelector(".add-recipe-button");
 // Meal Form Variables
 var submitMealButton = document.querySelector(".meal-option-button");
 var mealFormValue = document.querySelector(".meal-form");
-var formValue = mealFormValue['food-option'].value;
-var validationText = document.querySelector(".validation-text");
+
 
 // Meal View Variables
 var mealText = document.querySelector(".meal-text");
@@ -14,27 +13,55 @@ var clearTextButton = document.querySelector(".clear-button");
 var cookpotImage = document.querySelector(".cookpot-image");
 
 // Footer Variables
+var footer = document.querySelector(".footer");
 var recipeTypeInput = document.querySelector(".recipe-type");
-var recipeNameInput = document.querySelector(".recipe-name");
+var recipeNameInput = document.querySelector("#recipe-name");
 var addNewRecipeButton = document.querySelector(".add-new-recipe-button");
 
 //Event Listeners
-submitMealButton.addEventListener("click",generateRandomFoods);
+submitMealButton.addEventListener("click", generateRandomFoods);
+addNewRecipeButton.addEventListener("click", addRecipeButtonClicked);
+addRecipeButton.addEventListener("click", addNewRecipeButtonClicked)
 
 
 
 function generateRandomFoods() {
     event.preventDefault();
+    var formValue = document.querySelector('input[name="food-option"]:checked').value;
     var side = sides[getRandomIndex(sides)];
     var mainDish = mains[getRandomIndex(mains)];
     var dessert = desserts[getRandomIndex(desserts)];
     var entireMeal = `${mainDish} with a side of ${side} and ${dessert} for dessert!`;
-    console.log(formValue)
-    if(formValue === undefined){
-        validationText.classList.remove("hidden")
+    if(formValue === "side"){
+        mealText.innerHTML = side
+    } else if(formValue === "main-dish"){
+        mealText.innerHTML = mainDish
+    } else if(formValue === "dessert"){
+        mealText.innerHTML = dessert
+    } else if(formValue === "entire-meal"){
+        mealText.innerHTML = entireMeal
     };
-    
 }
+
+function addNewRecipeButtonClicked(){
+    event.preventDefault();
+    footer.classList.remove('hidden')
+
+
+}
+
+function addRecipeButtonClicked(){
+    event.preventDefault();
+    var newRecipeType = document.getElementById("recipe-type");
+    var newRecipeTypeValue = newRecipeType.options[newRecipeType.selectedIndex].value;
+    var recipeInputValue = recipeNameInput.value;
+    // push values to array
+    // `${newRecipeTypeValue}`.push(recipeNameInput.value)
+}
+
+// var e = document.getElementById("elementId");
+// var value = e.options[e.selectedIndex].value;
+// var text = e.options[e.selectedIndex].text;
 
 
 

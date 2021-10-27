7a824286803b5892eb55f5517ec4da1419e08bb3
Gabby Recny <gjtrecny@gmail.com>
2021-10-27 12:59:58 -0500

    Add suggestion text

--- main.js
+++ main.js
@@ -1,53 +1,3 @@
-var sides = [
-  "Miso Glazed Carrots",
-  "Coleslaw",
-  "Garden Salad",
-  "Crispy Potatoes",
-  "Sweet Potato Tots",
-  "Coconut Rice",
-  "Caeser Salad",
-  "Shrimp Summer Rolls",
-  "Garlic Butter Mushrooms",
-  "Hush Puppies",
-]
-
-var mains = [
-  "Spaghetti and Meatballs",
-  "Pineapple Chicken",
-  "Shakshuka",
-  "Thai Yellow Curry",
-  "Bibimbap",
-  "Chicken Parmesean",
-  "Butternut Squash Soup",
-  "BBQ Chicken Burgers",
-  "Ramen",
-  "Empanadas",
-  "Chicken Fried Rice",
-  "Sheet Pan Fajitas",
-  "Margarita Pizza",
-  ]
-
-var desserts = [
-  "Apple Pie",
-  "Lemon Meringue Pie",
-  "Black Forest Cake",
-  "Banana Bread",
-  "Peach Cobbler",
-  "Cheesecake",
-  "Funfetti Cake",
-  "Baklava",
-  "Flan",
-  "Macarons",
-  "Macaroons",
-  "Chocolate Cupcakes",
-  "Pavlova",
-  "Pumpkin Pie",
-  "Key Lime Pie",
-  "Tart Tatin",
-  "Croissants",
-  "Eclairs",
-  ]
-
 var sideSelection = document.querySelector('#side');
 var mainSelection = document.querySelector('#mainDish');
 var dessertSelection = document.querySelector('#dessert');
@@ -71,13 +21,24 @@ function randomMeal(mealOptions) {
 }
 
 function randomSide() {
-  recipeDisplayBox.innerHTML = `${sides[randomMeal(sides)]}!`;
+  suggestionMessage()
+  recipeDisplayBox.innerHTML += `${sides[randomMeal(sides)]}!`;
+  recipeDisplayBox.classList.add('meal-text');
 }
 
 function randomMain() {
-  recipeDisplayBox.innerHTML = `${mains[randomMeal(mains)]}!`;
+  suggestionMessage()
+  recipeDisplayBox.innerHTML += `${mains[randomMeal(mains)]}!`;
+  recipeDisplayBox.classList.add('meal-text');
 }
 
 function randomDessert() {
-  recipeDisplayBox.innerHTML = `${desserts[randomMeal(desserts)]}!`;
+  suggestionMessage()
+  recipeDisplayBox.innerHTML += `${desserts[randomMeal(desserts)]}!`;
+  recipeDisplayBox.classList.add('meal-text');
+}
+
+function suggestionMessage() {
+  recipeDisplayBox.innerHTML = "You should make:"
+  // recipeDisplayBox.classList.add('suggestion-text')
 }

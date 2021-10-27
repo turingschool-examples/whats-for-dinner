66d21c68c983f91aa1c424109cdeaf7c1507fed0
Ryan McBride <mr.ryan444@gmail.com>
2021-10-27 15:51:41 -0600

    Add query selectors

---
+++ main.js
@@ -0,0 +1,6 @@
+// Query Selectors
+var sideButton = document.querySelector('.side-button');
+var mainDishButton = document.querySelector('.main-dish-button');
+var dessertButton = document.querySelector('.dessert-button');
+var entireMealButton = document.querySelector('.entire-meal-button');
+var letsCookButton = document.querySelector('.lets-cook-btn');

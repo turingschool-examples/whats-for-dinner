19032ed33d2196374938a0fcbcd6932e6c00de58
Stevie Ambroise <s.ambroise@protonmail.com>
2021-10-28 17:07:19 -0600

    Add working buttons

---
+++ data.js
@@ -0,0 +1,27 @@
+var side = [
+"Miso Glazed Carrots",
+"Coleslaw",
+"Garden Salad",
+"Crispy Potatoes",
+"Sweet Potato Tots",
+"Coconut Rice",
+"Caeser Salad",
+"Shrimp Summer Rolls"
+];
+
+
+
+
+var dessert = [
+"Apple Pie",
+"Lemon Meringue Pie",
+"Black Forest Cake",
+"Banana Bread",
+"Peach Cobbler",
+"Cheesecake",
+"Funfetti Cake",
+"Baklava"
+"Flan",
+"Macarons",
+"Macaroons"
+];

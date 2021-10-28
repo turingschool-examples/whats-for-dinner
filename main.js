39c59f595d1099a79ff69dc370579aa4507c1654
Phil Lewis <phillewis1985@gmail.com>
2021-10-28 15:41:50 -0600

    Make form buttons functional

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
-var addRecipeButton = document.getElementByID('add-recipe-button');
-var selectionForm = document.getElementByID('selection-form');
-var userChoice = document.getElementsByName('radio-button');
-var firstSelection = document.getElementByID('first-item');
-var secondSelection = document.getElementByID('second-item');
-var thirdSelection = document.getElementByID('third-item');
-var fourthSelection = document.getElementByID('fourth-item');
-var submitButton = document..getElementByID('submit-button');
-var cookpot = document.getElementByID('cookpot');
-var outputPrompt = document.getElementByID('you-should-make');
-var selectionOutput = document.getElementByID('selection-output');
-var clearButton = document.getElementByID('clear-selection-button');
-
 var sides = ['Mac n Cheese', 'Coleslaw', 'Pinto Beans', 'Baked Beans', 'Green Beans', 'Fries', 'Fried Pickles', 'Fried Okra', 'Potato Salad', 'Small Salad'];
 
 var mainDishes = ['Ribs', 'Sliced Brisket', 'Chopped Brisket', 'Balogna', 'Pulled Pork', 'Hot Links', 'Chicken'];
 
 var desserts = ['Brownie', 'Cookie', 'Apple Pie', 'Peach Cobbler', 'Ice Cream', 'Banana Pudding', 'Carrot Cake'];
 
-//Event listeners:
-addRecipeButton.addEventListener('click', addRecipe());
-submitButton.addEventListener('click', selectBasicFoodType());
-clearButton.addEventListener('click', clearResults());
+var addRecipeButton = document.querySelector('#add-recipe-button');
+var selectionForm = document.querySelector('#selection-form');
+var firstSelection = document.querySelector('#first-item');
+var secondSelection = document.querySelector('#second-item');
+var thirdSelection = document.querySelector('#third-item');
+var fourthSelection = document.querySelector('#fourth-item');
+var submitButton = document.getElementById('submit-button');
+var cookpot = document.querySelector('#cookpot');
+var outputPrompt = document.querySelector('#you-should-make');
+var selectionOutput = document.querySelector('#selection-output');
+var clearButton = document.querySelector('#clear-selection-button');
+
+submitButton.addEventListener('click', selectBasicFoodType);
+clearButton.addEventListener('click', clearResults);
 
 function show(element) {
   element.classList.remove('hidden');
@@ -28,37 +25,34 @@ function show(element) {
 
 function hide(element) {
   element.classList.add('hidden');
-}
+};
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-}
+};
+
+function findUserChoice() {
+  if (firstSelection.checked) {
+    selectionOutput.innerText = sides[getRandomIndex(sides)] + '!';
+  } else if (secondSelection.checked) {
+    selectionOutput.innerText = mainDishes[getRandomIndex(mainDishes)] + '!';
+  } else if (thirdSelection.checked) {
+    selectionOutput.innerText = desserts[getRandomIndex(desserts)] + '!';
+  } else if (fourthSelection.checked) {
+    selectionOutput.innerText = `${sides[getRandomIndex(sides)]}, ${mainDishes[getRandomIndex(mainDishes)]}, and ${desserts[getRandomIndex(desserts)]}!`;
+  }
+};
 
 function selectBasicFoodType() {
-  var choice = findUserChoice();
-  selectionOutput.innerText = choice[getRandomIndex(choice)] + '!';
+  findUserChoice();
   hide(cookpot);
   show(outputPrompt);
   show(selectionOutput);
   show(clearButton);
 };
 
-function findUserChoice() {
-  var usersChoice = '';
-  if (firstSelection.checked) {
-    usersChoice = 'sides';
-  } else if (secondSelection.checked) {
-    usersChoice = 'mainDishes';
-  } else if (thirdChoice.checked) {
-    usersChoice = 'desserts';
-  } else if (fourthChoice.checked) {
-    usersChoice = 'entireMeal';
-  }
-  return userChoice;
-};
-
 function clearResults() {
-  hide(outputPromt);
+  hide(outputPrompt);
   hide(selectionOutput);
   hide(clearButton);
   show(cookpot);

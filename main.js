a234e442fdb6b4f0e222152af47319c0ecf6a19c
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-10-28 15:01:11 -0700

    Declare variables and assign them to input/button elements in form

---
+++ main.js
@@ -0,0 +1,6 @@
+var side = document.querySelector("#side");
+var mainDish = document.querySelector("#main-dish");
+var dessert = document.querySelector("#dessert");
+var entireMeal = document.querySelector("#entire-meal");
+
+var buttonLetsCook = document.querySelector("lets-cook");

eda2286d6acb73ef706295645991cbd3286fca1c
Emily Corcoran <cathleencorcoran@gmail.com>
2021-10-27 22:00:49 -0400

    Display recipe function working

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var letsCookButton = document.querySelector('.lets-cook-button');
 var potImage = document.querySelector('.pot-image')
 var foodText = document.querySelector('.food-text')
 var sideRadioButton = document.querySelector('.side-button')
+var mainDishRadioButton = document.querySelector('.main-dish-button')
+var dessertRadioButton = document.querySelector('.dessert-button')
 
 function getRandomIndex(array) {
   var randomIndex = Math.floor(Math.random() * array.length);
@@ -10,13 +12,20 @@ function getRandomIndex(array) {
 
 function displayRecipe() {
   if (sideRadioButton.checked === true) {
-    var randomSide = getRandomIndex(sides)
-    foodText.innerText = randomSide
+    var randomFoodItem = getRandomIndex(sides)
+    foodText.innerText = randomFoodItem
+  } else if (mainDishRadioButton.checked === true) {
+    randomFoodItem = getRandomIndex(mainDishes)
+    foodText.innerText = randomFoodItem
+  } else if (dessertRadioButton.checked === true) {
+    randomFoodItem = getRandomIndex(desserts)
+    foodText.innerText = randomFoodItem
   }
   potImage.classList.add('hidden')
   foodText.classList.remove('hidden')
-
 };
+
+
 
 
 letsCookButton.addEventListener('click', displayRecipe)

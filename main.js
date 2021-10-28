a471ec416ad520db0ace9910185f7ec79b399c76
Emily Corcoran <cathleencorcoran@gmail.com>
2021-10-27 22:38:37 -0400

    Add elseif statement for entire meal radio button

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var foodText = document.querySelector('.food-text')
 var sideRadioButton = document.querySelector('.side-button')
 var mainDishRadioButton = document.querySelector('.main-dish-button')
 var dessertRadioButton = document.querySelector('.dessert-button')
+var entireMealRadioButton = document.querySelector('.entire-meal-button')
+
 
 function getRandomIndex(array) {
   var randomIndex = Math.floor(Math.random() * array.length);
@@ -12,14 +14,16 @@ function getRandomIndex(array) {
 
 function displayRecipe() {
   if (sideRadioButton.checked === true) {
-    var randomFoodItem = getRandomIndex(sides)
-    foodText.innerText = randomFoodItem
+    // var randomFoodItem = getRandomIndex(sides)
+    foodText.innerText = getRandomIndex(sides)
   } else if (mainDishRadioButton.checked === true) {
     randomFoodItem = getRandomIndex(mainDishes)
     foodText.innerText = randomFoodItem
   } else if (dessertRadioButton.checked === true) {
     randomFoodItem = getRandomIndex(desserts)
     foodText.innerText = randomFoodItem
+  } else if (entireMealRadioButton.checked === true) {
+    foodText.innerText = `You Should Make: ${getRandomIndex(mainDishes)} with a side of ${getRandomIndex(sides)} and ${getRandomIndex(desserts)} for dessert!`
   }
   potImage.classList.add('hidden')
   foodText.classList.remove('hidden')

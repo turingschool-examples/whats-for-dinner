2da5ef25371fc5505c12c36f68080d2394d25290
Grace Gardner <graceirisgardner@gmail.com>
2021-10-29 10:03:33 -0600

    Create hideCookPot function

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
 var selectedRecipe ='';
 
-var recipePosition = document.quarySelector(".recipe-position");
+var recipePosition = document.querySelector(".recipe-position");
 var sideSelector = document.getElementById("side");
 var mainDishSelector = document.getElementById("main-dish");
 var dessertSelector = document.getElementById("dessert");
 var entireMealSelector = document.getElementById("entire-meal");
+var cookPot = document.querySelector(".cook-pot");
 
-var letsCookButton = document.quarySelector(".lets-cook");
-var clearButton = document.quarySelector(".clear");
-var addRecipeButton = document.quarySelector(".add-a-recipe");
+var letsCookButton = document.querySelector(".lets-cook");
+var clearButton = document.querySelector(".clear");
+var addRecipeButton = document.querySelector(".add-a-recipe");
 
-clearButton.addEventListener();
-addRecipeButton.addEventListener();
-letsCookButton.addEventListener("submit", displayRecipe);
+// clearButton.addEventListener();
+// addRecipeButton.addEventListener();
+letsCookButton.addEventListener('click', displayRecipe);
 
 function displayRecipe() {
     selectedRecipe.innerText +=
- `You should make ${findRandomRecipe()}`
+ (`You should make ${findRandomRecipe()}`)
 }
 
 function findRandomRecipe() {
   if (sideSelector.checked){
-    return sides[getRandom(sides)];
+    return `${sides[getRandom(sides)]}`;
   } else if (mainDishSelector.checked) {
-    return mains[getRandom(sides)];
+    return `${mains[getRandom(sides)]}`;
   } else if (dessertSelector.checked){
-    return desserts[getRandom(sides)];
+    return `${desserts[getRandom(sides)]}`;
   } else if (entireMealSelector.checked){
     return `${sides[getRandom(sides)]} ${mains[getRandom(sides)]} ${desserts[getRandom(sides)]}`
   }
-  }
+}
+
+function hideCookPot(){
+  cookPot.classList.add('hidden');
 }
 
 

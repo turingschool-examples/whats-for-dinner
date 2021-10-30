4e45d9b18a5cc9036fc55b57d231dc3dbc03f2ed
Johnathan Moore <johnathan.moore@thehoofed.com>
2021-10-30 16:32:15 -0500

    add functionality to all buttons

--- main.js
+++ main.js
@@ -1,13 +1,58 @@
-var randomSide = document.querySelector('#side');
-var randomMainDish = document.querySelector('#main-dish');
-var randomDessert = document.querySelector('#dessert');
-var entireMeal = document.querySelector('#entire-meal');
+var sideArray = ['tater tots', 'brussel sprouts', 'caesar salad', 'garlic bread', 'hummus', 'macaroni and cheese', 'egg drop soup', 'fried rice'];
+var mainArray = ['chicken fried steak', 'chicken nuggets', 'spaghetti', 'chicken pesto pasta', 'pizza', 'tacos', 'bologna sandwich'];
+var dessertArray = ['carrot cake', 'pudding', 'yogurt', 'ice cream', 'fruit cocktail', 'pie'];
 
-function getRandomItem(array) {
-    return Math.floor(Math.random() * array.length)
+var entireMeal = document.querySelector('#entire-meal');
+var side = document.querySelector('#side');
+var mainDish = document.querySelector('#main-dish');
+var dessert = document.querySelector('#dessert');
+var foodItem = document.querySelector('.foodItem');
+var suggestion = document.querySelector('h3');
+var letsCookButton = document.querySelector('.letsCookButton');
+var boxRightHidden = document.querySelector('.box-right-hidden');
+var boxRight = document.querySelector('.box-right');
+var boxLeft = document.querySelector('.box-left');
+var stockPotImage = document.querySelector('.stockPotImg');
+var clearButton = document.querySelector('.clear-button');
+
+letsCookButton.addEventListener('click', letsCook);
+clearButton.addEventListener('click', clearOption);
+suggestion.classList.add('hidden');
+clearButton.classList.add('hidden');
+foodItem.classList.add('hidden');
+
+function randomFood(array) {
+    return array[Math.floor(Math.random() * array.length)];
 };
 
-randomSide.addEventListener('click', getRandomSide);
-randomMainDish.addEventListener('click', getRandomMainDish);
-randomDessert.addEventListener('click', getRandomDessert);
-entireMeal.addEventListener('click', getEntireMeal);
+function letsCook() {
+    stockPotImage.classList.add('hidden');
+    suggestion.classList.remove('hidden');
+    clearButton.classList.remove('hidden');
+    foodItem.classList.remove('hidden');
+
+    if (side.checked) { 
+    foodItem.innerHTML = `${randomFood(sideArray)}!`;
+    } else if (mainDish.checked) {
+    foodItem.innerHTML = `${randomFood(mainArray)}!`
+    } else if (dessert.checked) {
+    foodItem.innerHTML = `${randomFood(dessertArray)}!`
+    } else if (entireMeal.checked) {
+    foodItem.innerHTML = `A main course of ${randomFood(mainArray)} with a side of ${randomFood(sideArray)}, and ${randomFood(dessertArray)} for dessert!`
+    } else {
+        foodItem.innerHTML = 'Please make a valid selection'
+    }
+    
+};
+
+function clearOption() {
+    stockPotImage.classList.remove('hidden');
+    suggestion.classList.add('hidden');
+    foodItem.classList.add('hidden');
+    clearButton.classList.add('hidden');
+
+};
+
+
+
+  

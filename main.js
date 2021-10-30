4575e685bd297f2d4031b6096f5c29102504c4cc
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-30 11:13:27 -0600

    Add hidden view of random dish to html

--- main.js
+++ main.js
@@ -5,3 +5,17 @@
 //make it possible to only select one radio button at a time
 //create an event listener for each of the radio buttons when Let's cook button is selected
 //create css classlist for checked and unchecked radio buttons.
+
+
+/* <button class="add-recipe-button">ADD A RECIPE</button> */
+var letsCookButton = document.querySelector('.lets-cook-button')
+var radioButtons = document.querySelectorAll('input[name="options"]')
+var cookPotImage = document.querySelector('.cook-pot-img')
+var viewDishGenerated = document.querySelector('.dish-generated')
+
+letsCookButton.addEventListener('click', renderRandomDish)
+
+function renderRandomDish() {
+cookPotImage.classList.add('hidden')
+viewDishGenerated.classList.remove('hidden')
+}

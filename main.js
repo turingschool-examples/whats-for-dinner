9adaf34f3b896b7e300cbd7043f17cff8a0875e4
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-30 13:50:26 -0600

    Add random generator

--- main.js
+++ main.js
@@ -5,6 +5,37 @@
 //make it possible to only select one radio button at a time
 //create an event listener for each of the radio buttons when Let's cook button is selected
 //create css classlist for checked and unchecked radio buttons.
+var side = [
+  "mashed potatoes",
+  "mac & cheese",
+  "veggies",
+  "white rice",
+  "brown rice",
+  "baked brusell sprouts",
+  "green beans",
+  "corn",
+  "baked potato",
+  "ceasar salad",
+  "fruit salad"
+]
+
+var mainDish = [
+  "turky and gravy",
+  "steak",
+  "fried chicken",
+  "grilled chicken",
+  "lemon baked salmon",
+  "grilled shrimp",
+  "bacon wrapped scallop"
+]
+
+var dessert = [
+  "lemon cake",
+  "chantilly cake",
+  "tiramisu cake",
+  "mochi donut",
+  "macaroons"
+]
 
 
 /* <button class="add-recipe-button">ADD A RECIPE</button> */
@@ -12,10 +43,20 @@ var letsCookButton = document.querySelector('.lets-cook-button')
 var radioButtons = document.querySelectorAll('input[name="options"]')
 var cookPotImage = document.querySelector('.cook-pot-img')
 var viewDishGenerated = document.querySelector('.dish-generated')
+var itemGenerated = document.querySelector('.item-generated')
 
 letsCookButton.addEventListener('click', renderRandomDish)
 
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * (array.length))
+}
+
 function renderRandomDish() {
-cookPotImage.classList.add('hidden')
-viewDishGenerated.classList.remove('hidden')
+  randomSide = side[getRandomIndex(side)]
+  randomMainDish = mainDish[getRandomIndex(mainDish)]
+  randomDessert = dessert[getRandomIndex(dessert)]
+  cookPotImage.classList.add('hidden')
+  viewDishGenerated.classList.remove('hidden')
+  itemGenerated.innerText = randomSide.toUpperCase(randomSide)+"!"
+  console.log(randomSide)
 }

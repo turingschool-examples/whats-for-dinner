d75f69a9407ddca77bb0886cece589bdb4dcd513
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-30 09:56:31 -0600

    Add data.js variables and changes to html text

---
+++ data.js
@@ -0,0 +1,31 @@
+var side = [
+  "mashed potatoes",
+  "mac & cheese",
+  "veggies",
+  "white rice",
+  "brown rice",
+  "baked brusell sprouts",
+  "green beans",
+  "corn",
+  "baked potato",
+  "ceasar salad",
+  "fruit salad"
+]
+
+var mainDish = [
+  "turky and gravy",
+  "steak",
+  "fried chicken",
+  "grilled chicken",
+  "lemon baked salmon",
+  "grilled shrimp",
+  "bacon wrapped scallop"
+]
+
+var dessert = [
+  "lemon cake",
+  "chantilly cake",
+  "tiramisu cake",
+  "mochi donut",
+  "macaroons"
+]
---
+++ main.js
@@ -0,0 +1,7 @@
+//create variables for side, main, and desesert dishes
+//add classlist for .hidden .add
+//make pot image hidden when let's cook button is selected
+//functions for when each of the radio button is selected
+//make it possible to only select one radio button at a time
+//create an event listener for each of the radio buttons when Let's cook button is selected
+//create css classlist for checked and unchecked radio buttons.

3217c3b98bf4722a1470353290979198a91ba53c
Kevin King <kevinturingking@gmail.com>
2021-10-31 21:57:55 -0600

    Add Function to buttons & frame two

--- main.js
+++ main.js
@@ -1,19 +1,75 @@
+var sideDishes = ['Mashed Potatoes', 'Green Beans','Pasta', 'oranges', 'candy']
+// mainDish array
+// dessert array
+// entire meal array
+
 var cookButton = document.querySelector('#cook-button')
-// var sideButton = document.querySelector()
-// var dishButton = document.querySelector()
-// var dessertButton = document.querySelector()
+var sideButton = document.querySelector('#side')
+var dishButton = document.querySelector('#main-dish')
+var dessertButton = document.querySelector('#dessert')
 var crockPot = document.querySelector('.cookPot')
-var results = document.querySelector('.results')
+var mainDish = document.querySelector('#entire-meal')
 
+var renderHere = document.querySelector('.renderHere')
 
+var inputArray = [
+  sideButton,
+  dishButton,
+  dessertButton,
+  mainDish
+]
 
-cookButton.addEventListener('click', showRecipe)
+window.onload = function() {
+  cookButton.disabled = true
+}
+
+sideButton.addEventListener('click', checkEnableCookButton)
+dishButton.addEventListener('click', checkEnableCookButton)
+dessertButton.addEventListener('click', checkEnableCookButton)
+mainDish.addEventListener('click', checkEnableCookButton)
+cookButton.addEventListener('click', hideLogo)
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 };
 
-function showRecipe() {
-  console.log('it works')
+function hideLogo() {
   crockPot.classList.add('hidden')
+  renderDish()
+}
+
+function checkEnableCookButton(event) {
+  function checkInputValue(checkboxInput) {
+    if (checkboxInput.checked === true) {
+      cookButton.disabled = false
+    }
+  }
+  inputArray.forEach(checkInputValue)
+
+  if (sideButton.checked === false &&
+    dishButton.checked === false &&
+    dessertButton.checked === false &&
+    mainDish.checked === false) {
+      cookButton.disabled = true
+  }
+
+  disableOthercheckboxes(event)
+}
+
+function disableOthercheckboxes(event) {
+  inputArray.forEach(function (input) {
+    if(input !== event.target) {
+      input.checked = false
+    }
+  })
+}
+
+function renderDish() {
+  if (sideButton.checked === true) {
+    var sideIndex = getRandomIndex(sideDishes)
+    renderHere.innerText = sideDishes[sideIndex]
+  }
+  // if (mainDish)
+  // if (dessert)
+  // if (entire-meal)
 }

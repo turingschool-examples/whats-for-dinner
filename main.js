1fd9460a64967c3455647c9b63d6be93c30255bf
Nicholas Ao <aominhlong@yahoo.com>
2022-02-17 21:47:18 -0700

    Add random food function

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
 var cookBtn = document.querySelector('.lets-cook-button');
 var potImg = document.querySelector('.dinner-box');
 var makeText = document.querySelector('.dinner-box2');
+var foodSuggestion = document.querySelector('.size');
+var side = [
+  'Potato Salad',
+  'Mashed Potatoes',
+  'Broccoli Salad',
+  'Southern Coleslaw',
+  'Roasted Potatoes',
+];
+
+var main = [
+  'Chicken Parmesan',
+  'Bone-in Ribeye Steak',
+  'Oven-baked Salmon',
+  'Air Fried Pork Belly',
+  'Grilled Chicken',
+];
+
+var dessert = [
+  'Fudge Caramel Brownies',
+  'Chocolate Cake',
+  'Apple Pie',
+  'Ice Cream',
+  'Original Cheese Cake',
+];
 
 cookBtn.addEventListener('click', function() {
   hide(potImg)
   show(makeText)
+  random(main)
 });
 
-function showFood() {
-
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
 }
+
+function random(dish) {   //replace 'main' with meal dish
+  foodSuggestion.innerText = dish[getRandomIndex(dish)]
+}
+
 
 function hide(element) {
   element.classList.add('hide')

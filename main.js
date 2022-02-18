da6cc7049033280684952ae20d43b8dfa7dc4ead
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-02-18 17:03:59 -0600

    Add radio selection function

--- main.js
+++ main.js
@@ -21,8 +21,38 @@ dishText.style.visibility = 'hidden';
 clearButton.style.visibility = 'hidden';
 cookPot.style.visibility = 'visible';
 
+//FUNCTIONS
 
-//TOGGLE INSTEAD HERE???
+///return the value of the index, which in this case is a string
+
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+
+function getRecipe(){
+  var grabRadio = document.querySelector('input[name="meal-options"]:checked');
+  if(!grabRadio){
+    return
+   }
+    if(grabRadio.value === "sideChoices") {
+      return getRandomElement(sideChoices)
+   }
+//take food from an array, but happen when side, main,dessert is chosen
+    else if (grabRadio.value === "mainChoices"){
+      return getRandomElement(mainChoices)
+    }
+//if (side) do getrandomelement(sideChoices)
+//return get random function, putting in values of radio button
+    else if (grabRadio.value === "dessertChoices"){
+      return getRandomElement(dessertChoices)
+  }
+};
+
+//else if main do do getrandom(mainchoices)
+//else if dessert getrandom(dessertChoices)
+//
 
 function viewSide(){
   cookTitle.style.visibility = 'visible';

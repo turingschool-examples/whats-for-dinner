9681afef369dcf05333baacd50d8b8b5d3176cf2
Nicholas Ao <aominhlong@yahoo.com>
2022-02-17 19:55:23 -0700

    Add Black Forest Cake After Click

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var cookBtn = document.querySelector('.lets-cook-button');
-var potImg = document.querySelector('.pot');
-var makeText = document.querySelector('You-Should-Make');
+var potImg = document.querySelector('.dinner-box');
+var makeText = document.querySelector('.dinner-box2');
 
 cookBtn.addEventListener('click', function() {
   hide(potImg)
@@ -13,4 +13,8 @@ function showFood() {
 
 function hide(element) {
   element.classList.add('hide')
+}
+
+function show(element) {
+  element.classList.remove('hide')
 }

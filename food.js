8109807d6509ada474e993af5f65664b9e154069
Nicholas Ao <aominhlong@yahoo.com>
2022-02-18 16:14:45 -0700

    Add food list

---
+++ food.js
@@ -0,0 +1,23 @@
+var side = [
+  'Potato Salad',
+  'Mashed Potatoes',
+  'Broccoli Salad',
+  'Southern Coleslaw',
+  'Roasted Potatoes',
+];
+
+var main = [
+  'Chicken Parmesan',
+  'Bone-in Ribeye Steak',
+  'Oven-baked Salmon',
+  'Air Fried Pork Belly',
+  'Grilled Chicken',
+];
+
+var dessert = [
+  'Fudge Caramel Brownies',
+  'Chocolate Cake',
+  'Apple Pie',
+  'Ice Cream',
+  'Original Cheese Cake',
+];

2ccd2197ae7ccc49ab3507a34e020c2336b84c82
Neil Linton <lintonbneil@gmail.com>
2022-02-18 19:44:59 -0800

    Add more variables in js file

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-// Radio Buttons
+// recipe Button
+var recipeButton = document.querySelctor("#recipe-button")
+// Radio Buttons (section1)
+var entireRadio = document.querySelector("entire-meal")
 var sideRadio = document.querySelector("#side")
 var mainRadio = document.querySelctor("#main-dish")
 var dessertRadio = document.querySelector("#dessert")
-var entireRadio = document.querySelector("entire-meal")
-// recipe Button
-var recipeButton = document.querySelctor("#recipe-button")
-// let's cook Button
+// let's cook Button (section1)
 var letsCookButton = document.querySelector("#lets-cook-button")
-//
+// clear button (section 2)
+var clearButton = document.querySelector("#clear")
+// hidden values (section 2)
+var shouldMakeText = document.querySelctor(".should-make-text-hidden")
+var randomMealText = document.querySelctor(".random-meal-text-hidden")
+
+// event listeners
+
+
+
+
+
+
+
+
+// functions

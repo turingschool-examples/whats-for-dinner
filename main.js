dab3480c8dff80a58f52178c9da6e77e5a334359
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-02-19 08:13:51 -0600

    Finish formatting and button functionality

--- main.js
+++ main.js
@@ -9,9 +9,12 @@ var clearButton = document.querySelector('.clear-button')
 
 
 
-//**EVENT LISTENERS**//
+//**EVENT LISTENER**//
 cookButton.addEventListener('click', displayMeal);
 
+
+
+//**FUNCTIONS**//
 
 function displayMeal(){
   event.preventDefault();
@@ -21,30 +24,6 @@ function displayMeal(){
   dishText.classList.remove('hidden');
   clearButton.classList.remove('hidden');
 };
-
-//entire meal function that if entire meal is selected, interpolate the random elements
-
-// function show(element) {
-//   element.classList.remove('hidden');
-// };
-//
-// function hide(element) {
-//   element.classList.add('hidden');
-// };
-
-// var cookTitle = document.getElementById('dish-header');
-// var dishText = document.getElementById('dish-text');
-// var clearButton = document.getElementById('clear-button');
-// var cookPot = document.getElementById('cookpot');
-
-// cookTitle.style.visibility = 'hidden';
-// dishText.style.visibility = 'hidden';
-// clearButton.style.visibility = 'hidden';
-// cookPot.style.visibility = 'visible';
-
-//FUNCTIONS
-
-///return the value of the index, which in this case is a string
 
 
 function getRandomElement(array) {
@@ -59,65 +38,20 @@ function getRecipe(){
     if(grabRadio.value === "sideChoices") {
       return `${getRandomElement(sideChoices)}!`
    }
-//take food from an array, but happen when side, main,dessert is chosen
     else if (grabRadio.value === "mainChoices"){
       return `${getRandomElement(mainChoices)}!`
     }
-//if (side) do getrandomelement(sideChoices)
-//return get random function, putting in values of radio button
     else if (grabRadio.value === "dessertChoices"){
       return `${getRandomElement(dessertChoices)}!`
   }
     else if (grabRadio.value === "entireMeal")
-      return `${getRandomElement(mainChoices)} with a side of ${getRandomElement(sideChoices)} with a side of ${getRandomElement(dessertChoices)} for dessert!`
+      return `${getRandomElement(mainChoices)} with a side of ${getRandomElement(sideChoices)} and ${getRandomElement(dessertChoices)} for dessert!`
 };
 
-//else if main do do getrandom(mainchoices)
-//else if dessert getrandom(dessertChoices)
-//
-
-// function viewSide(){
-//   cookTitle.style.visibility = 'visible';
-//   dishText.style.visibility = 'visible';
-//   clearButton.style.visibility = 'visible';
-//   cookPot.style.visibility = 'hidden';
-// }
-//
-// function viewMain(){
-//   cookTitle.style.visibility = 'visible';
-//   dishText.style.visibility = 'visible';
-//   clearButton.style.visibility = 'visible';
-//   cookPot.style.visibility = 'hidden';
-// }
-//
-// function viewDessert(){
-//   cookTitle.style.visibility = 'visible';
-//   dishText.style.visibility = 'visible';
-//   clearButton.style.visibility = 'visible';
-//   cookPot.style.visibility = 'hidden';
-// }
-//
-// function viewEntireMeal(){
-//   cookTitle.style.visibility = 'visible';
-//   dishText.style.visibility = 'visible';
-//   clearButton.style.visibility = 'visible';
-//   cookPot.style.visibility = 'hidden';
-// }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+//**MEAL CHOICES**//
 
 var sideChoices = ["Miso Glazed Carrots", "Coleslaw", "Garden Salad", "Crispy Potatoes", "Sweet Potato Tots", "Coconut Rice", "Caesar Salad", "Shrimp Summer Rolls", "Garlic Butter Mushrooms", "Cauli Rice", "Creamed Spinach", "Roasted Broccoli", "Braised Collards", "Hush Puppies"]
 

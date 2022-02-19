989a9ff45e3c820dfcfe45e70d7ca88c7776a419
Angele Williams <angele.williams5@gmail.com>
2022-02-19 15:14:32 -0700

    Complete show random dish function

--- main.js
+++ main.js
@@ -1,29 +1,82 @@
 //global variables
-var letsCookB = document.querySelector('.lets-cook');
-var addRecipeB = document.querySelector('.add-recipe');
-//var clearB = document.querySelector('.clear');
+var letsCookB = document.querySelector(".lets-cook");
+var addRecipeB = document.querySelector(".add-recipe");
+var form = document.querySelector("form");
+// var log = document.querySelector("#log");
+// var radioButtons = document.querySelectorAll("input[name="dish"]");
 
+var sideRadio = document.querySelector("#side-dish");
+var mainRadio = document.querySelector("#main-dish");
+var dessertRadio = document.querySelector("#dessert-dish");
+var mealRadio =  document.querySelector("#entire-meal");
+
+var formOutput = document.querySelector(".form-output");
+var formResult = document.querySelector(".form-result");
+//var clearB = document.querySelector('.clear');
+var cookpotIcon = document.querySelector(".cookpot-icon");
 
 
 
 //event listeners
-letsCookB.addEventListener('click', displayRecipe)
-console.log(mains);
-
-
+// letsCookB.addEventListener('click', displayRecipe)
 // functions and event handlers
 
+form.addEventListener("submit", displayDish);
 
-function displayRecipe() {
+//
+function displayDish() {
+  event.preventDefault();
+  viewElement(formResult);
+  hideElement(cookpotIcon);
+  console.log("form has been submitted")
+    if (sideRadio.checked ) {
+       displayRandomSide()
 
+     }
+    else if (mainRadio.checked){
+        displayRandomMain()
+    }
+    else if (dessertRadio.checked) {
+        displayRandomDessert()
+    }
 };
 
-function randomizeRecipe() {
+//
+// var selectedDish;
+// for (const radioButton of radioButtons) {
+//   if (radioButton.checked) {
+//     selectedDish = radioButton.value;
+//   }
+// }
+  // // show the output:
+  // output.innerText = selectedSize ? `You selected ${selectedSize}` : `You haven't selected any size`;
 
 
-};
+function displayRandomSide(){
+  var dish = sides[getRandomIndex(sides)];
+    formOutput.innerText = `${dish}!`
+    console.log(dish);
+}
 
+function displayRandomMain(){
+  var dish = mains[getRandomIndex(mains)];
+    formOutput.innerText = `${dish}!`
+    console.log(dish);
+}
 
+function displayRandomDessert() {
+  var dish = desserts[getRandomIndex(desserts)];
+    formOutput.innerText = `${dish}!`
+    console.log(dish);
+}
+
+function viewElement(classToEdit) {
+  classToEdit.classList.remove("hidden");
+}
+
+function hideElement(classToEdit) {
+  classToEdit.classList.add("hidden");
+}
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }

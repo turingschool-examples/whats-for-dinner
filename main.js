238c1d9f7e82492e58b2ef14eb2abeeed009f1c0
Neil Linton <lintonbneil@gmail.com>
2022-02-19 12:39:28 -0800

    Add Object to store variable data, made eventlistener w/radio button inside

--- main.js
+++ main.js
@@ -6,23 +6,51 @@ var sideRadio = document.querySelector("#side")
 var mainRadio = document.querySelctor("#main-dish")
 var dessertRadio = document.querySelector("#dessert")
 // let's cook Button (section1)
-var letsCookButton = document.querySelector("#lets-cook-button")
+var letsCookButton = document.querySelector("#lets-cook-button"
 // clear button (section 2)
 var clearButton = document.querySelector("#clear")
 // hidden values (section 2)
 var shouldMake = document.querySelctor(".should-make-text-hidden")
-var randomMeal = document.querySelctor(".random-meal-text-hidden")
+var results = document.querySelctor(".random-meal-text-hidden")
 var cookPotImg = document.querySelctor(".cookPotImg")
+//store for items in array
+var randomMeal = {
+  sides: sides,
+  mains: mains,
+  desserts: desserts,
+  entireMeal: [sides, mains, desserts]
+}
+
+//radio button checking
 
 
 
 // event listeners
+letsCookButton.addEventLister('click', () => {
+  var selected = document.querySelector('input[type="radio"]:checked')
+
+});
+
+clearButton.addEventListener('click',)
 
 
 
 
-
+// random number generator
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
 
 
 
 // functions
+
+
+function hidden() {
+
+
+}
+
+function notHidden() {
+
+}

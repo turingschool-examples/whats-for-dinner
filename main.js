362e43ea34595ea545bfdeed32492cfff6abbe56
Neil Linton <lintonbneil@gmail.com>
2022-02-18 19:02:52 -0800

    Add javascript variables for radio buttons, and html for section 2 box with hidden values

---
+++ main.js
@@ -0,0 +1,10 @@
+// Radio Buttons
+var sideRadio = document.querySelector("#side")
+var mainRadio = document.querySelctor("#main-dish")
+var dessertRadio = document.querySelector("#dessert")
+var entireRadio = document.querySelector("entire-meal")
+// recipe Button
+var recipeButton = document.querySelctor("#recipe-button")
+// let's cook Button
+var letsCookButton = document.querySelector("#lets-cook-button")
+// 

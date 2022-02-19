506c4e007797f037c00fd3703c48547bef15814c
Nikhila Balmoori <nbalmoori@gmail.com>
2022-02-18 22:12:47 -0700

    Update radio button formatting

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+var radios = document.forms
+
 var letsCookButton = document.querySelector('#lets-cook');
 
 var cookpotView = document.querySelector('.cookpot-view');
-
 var dishViewHidden = document.querySelector('.dish-view.hidden');
+var dishForm = document.querySelector('form')
 
 
 letsCookButton.addEventListener('click', populateDish);

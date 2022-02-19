6e026478009ccc68aa321cebcc13c3c6254f03f2
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-02-18 18:27:20 -0600

    Add toggle functionality

--- main.js
+++ main.js
@@ -1,25 +1,46 @@
 
 //QUERY SELECTORS//
 
-var cookButton = document.querySelector('cook-click')
+var cookButton = document.querySelector('.cook-click');
+var dishTitle = document.querySelector('.dish-title');
+var dishText = document.querySelector('.dish-text');
+var potImage = document.querySelector('.cookpot');
+var clearButton = document.querySelector('.clear-button')
 
 
 
 //**EVENT LISTENERS**//
-cookButton.addEventListener('click', )
+cookButton.addEventListener('click', displayMeal);
 
 
+function displayMeal(){
+  event.preventDefault();
+  dishText.innerText = getRecipe();
+  potImage.classList.add('hidden');
+  dishTitle.classList.remove('hidden');
+  dishText.classList.remove('hidden');
+  clearButton.classList.remove('hidden');
+};
 
+//entire meal function that if entire meal is selected, interpolate the random elements
 
-var cookTitle = document.getElementById('dish-header');
-var dishText = document.getElementById('dish-text');
-var clearButton = document.getElementById('clear-button');
-var cookPot = document.getElementById('cookpot');
+// function show(element) {
+//   element.classList.remove('hidden');
+// };
+//
+// function hide(element) {
+//   element.classList.add('hidden');
+// };
 
-cookTitle.style.visibility = 'hidden';
-dishText.style.visibility = 'hidden';
-clearButton.style.visibility = 'hidden';
-cookPot.style.visibility = 'visible';
+// var cookTitle = document.getElementById('dish-header');
+// var dishText = document.getElementById('dish-text');
+// var clearButton = document.getElementById('clear-button');
+// var cookPot = document.getElementById('cookpot');
+
+// cookTitle.style.visibility = 'hidden';
+// dishText.style.visibility = 'hidden';
+// clearButton.style.visibility = 'hidden';
+// cookPot.style.visibility = 'visible';
 
 //FUNCTIONS
 
@@ -34,53 +55,55 @@ function getRandomElement(array) {
 function getRecipe(){
   var grabRadio = document.querySelector('input[name="meal-options"]:checked');
   if(!grabRadio){
-    return
-   }
+    // return
+  }
     if(grabRadio.value === "sideChoices") {
-      return getRandomElement(sideChoices)
+      return `${getRandomElement(sideChoices)}!`
    }
 //take food from an array, but happen when side, main,dessert is chosen
     else if (grabRadio.value === "mainChoices"){
-      return getRandomElement(mainChoices)
+      return `${getRandomElement(mainChoices)}`
     }
 //if (side) do getrandomelement(sideChoices)
 //return get random function, putting in values of radio button
     else if (grabRadio.value === "dessertChoices"){
-      return getRandomElement(dessertChoices)
+      return `${getRandomElement(dessertChoices)}!`
   }
+    else if (grabRadio.value === "entireMeal")
+      return `${getRandomElement(mainChoices)} with a side of ${getRandomElement(sideChoices)} with a side of ${getRandomElement(dessertChoices)} for dessert!`
 };
 
 //else if main do do getrandom(mainchoices)
 //else if dessert getrandom(dessertChoices)
 //
 
-function viewSide(){
-  cookTitle.style.visibility = 'visible';
-  dishText.style.visibility = 'visible';
-  clearButton.style.visibility = 'visible';
-  cookPot.style.visibility = 'hidden';
-}
-
-function viewMain(){
-  cookTitle.style.visibility = 'visible';
-  dishText.style.visibility = 'visible';
-  clearButton.style.visibility = 'visible';
-  cookPot.style.visibility = 'hidden';
-}
-
-function viewDessert(){
-  cookTitle.style.visibility = 'visible';
-  dishText.style.visibility = 'visible';
-  clearButton.style.visibility = 'visible';
-  cookPot.style.visibility = 'hidden';
-}
-
-function viewEntireMeal(){
-  cookTitle.style.visibility = 'visible';
-  dishText.style.visibility = 'visible';
-  clearButton.style.visibility = 'visible';
-  cookPot.style.visibility = 'hidden';
-}
+// function viewSide(){
+//   cookTitle.style.visibility = 'visible';
+//   dishText.style.visibility = 'visible';
+//   clearButton.style.visibility = 'visible';
+//   cookPot.style.visibility = 'hidden';
+// }
+//
+// function viewMain(){
+//   cookTitle.style.visibility = 'visible';
+//   dishText.style.visibility = 'visible';
+//   clearButton.style.visibility = 'visible';
+//   cookPot.style.visibility = 'hidden';
+// }
+//
+// function viewDessert(){
+//   cookTitle.style.visibility = 'visible';
+//   dishText.style.visibility = 'visible';
+//   clearButton.style.visibility = 'visible';
+//   cookPot.style.visibility = 'hidden';
+// }
+//
+// function viewEntireMeal(){
+//   cookTitle.style.visibility = 'visible';
+//   dishText.style.visibility = 'visible';
+//   clearButton.style.visibility = 'visible';
+//   cookPot.style.visibility = 'hidden';
+// }
 
 
 

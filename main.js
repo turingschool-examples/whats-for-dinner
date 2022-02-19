cb82b0e2453f4fd404cdf911c494c61b5ae8f282
Neil Linton <lintonbneil@gmail.com>
2022-02-19 15:56:33 -0800

    Add meal Object

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 // recipe Button
-var recipeButton = document.querySelctor("#recipe-button")
+var recipeButton = document.querySelector("#recipe-button")
 // Radio Buttons (section1)
 var entireRadio = document.querySelector("entire-meal")
 var sideRadio = document.querySelector("#side")
-var mainRadio = document.querySelctor("#main-dish")
+var mainRadio = document.querySelector("#main-dish")
 var dessertRadio = document.querySelector("#dessert")
 // let's cook Button (section1)
-var letsCookButton = document.querySelector("#lets-cook-button"
+var letsCookButton = document.querySelector("#lets-cook-button")
 // clear button (section 2)
 var clearButton = document.querySelector("#clear")
 // hidden values (section 2)
-var shouldMake = document.querySelctor(".should-make-text-hidden")
-var results = document.querySelctor(".random-meal-text-hidden")
-var cookPotImg = document.querySelctor(".cookPotImg")
+var shouldMake = document.querySelector(".should-make-text-hidden")
+var results = document.querySelector(".random-meal-text-hidden")
+var cookPotImg = document.querySelector(".cookPotImg")
 //store for items in array
-var randomMeal = {
+var meal = {
   sides: sides,
   mains: mains,
   desserts: desserts,
@@ -24,26 +24,24 @@ var randomMeal = {
 //radio button checking
 
 
-
 // event listeners
-letsCookButton.addEventLister('click', () => {
+letsCookButton.addEventListener('click', findRandomSelection)
+
+function findRandomSelection() {
   var selected = document.querySelector('input[type="radio"]:checked')
+  var randomIndex = getRandomIndex(meal[selected.value])
+  results.innerHTML = meal[selected.value][randomIndex]
+}
 
-});
-
-clearButton.addEventListener('click',)
-
+// clearButton.addEventListener('click',)
+// recipeButton.addEventListener('click',)
 
 
 
 // random number generator
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-}
-
-
-
-// functions
+};
 
 
 function hidden() {

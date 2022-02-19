f8120ca59d82c33012b28d51936fdadc9024ca13
Nicholas Ao <aominhlong@yahoo.com>
2022-02-18 19:38:22 -0700

    Add nothing selected functionality

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
 var cookBtn = document.querySelector('.lets-cook-button');
 var potImg = document.querySelector('.dinner-box');
 var makeText = document.querySelector('.dinner-box2');
-var foodSuggestion = document.querySelector('.size');
+var foodSuggestion = document.querySelector('.foodInput');
 var allFood = document.querySelectorAll('.dish');
+var specificSide = document.getElementById('side-dish')
+var specificMain = document.getElementById('main-dish')
+var specificDessert = document.getElementById('dessert-dish')
+var specificEntire = document.getElementById('entire-meal')
 
 cookBtn.addEventListener('click', function() {
-  hide(potImg)
-  show(makeText)
   specificFood()
-}, {once: false}); //can only click once
+}, {once: false});
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
-// function random(dish) {
-//   foodSuggestion.innerText = dish[getRandomIndex(dish)]
-//   console.log(allFood);
-// }
+function reveal() {
+  hide(potImg)
+  show(makeText)
+  specificFood()
+}
+
+function nothingSelected() {
+  show(potImg)
+  hide(makeText)
+}
 
 function hide(element) {
   element.classList.add('hide')
@@ -28,17 +36,19 @@ function show(element) {
 }
 
 function specificFood() {
-  var specificSide = document.getElementById('side-dish') // access id when user clicks
-  var specificMain = document.getElementById('main-dish')
-  var specificDessert = document.getElementById('dessert-dish')
-  var specificEntire = document.getElementById('entire-meal')
     if (specificSide.checked === true) {
       foodSuggestion.innerText = side[getRandomIndex(side)]
+      reveal()
     } else if (specificMain.checked === true) {
       foodSuggestion.innerText = main[getRandomIndex(main)]
+      reveal()
     } else if (specificDessert.checked === true) {
       foodSuggestion.innerText = dessert[getRandomIndex(dessert)]
+      reveal()
     } else if (specificEntire.checked === true) {
       foodSuggestion.innerHTML = `${main[getRandomIndex(main)]} with a side of ${side[getRandomIndex(side)]} and ${dessert[getRandomIndex(dessert)]}!`
+      reveal()
+    } else {
+      nothingSelected()
     }
   }

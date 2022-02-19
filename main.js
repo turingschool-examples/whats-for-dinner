46aa355ed1536593ed14321303cb76b267ce473c
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-02-18 19:27:36 -0700

    Save foodlists to HTML for accessibility

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 // When a user selects a dish option and then clicks the “Let’s Cook!” button, the user sees a random dish from the list of possible dishes for that category
 
-//need to pull in JS to HTML via <script> DID IT id work??????
-
 //Query Selectors:
-
 var sideButton = document.querySelector(".side");
 var mainDishButton = document.querySelector(".main-dish");
 var dessertsButton = document.querySelector(".dessert");
@@ -11,30 +8,40 @@ var dessertsButton = document.querySelector(".dessert");
 var letsCookButton = document.querySelector(".button2");
 //var addARecipeButton = document.querySelector(".button1");
 var cookPotImage = document.querySelector(".cookpot");
-var dinnerSuggestions = document.querySelector("dinner-suggestions").hidden;
+var dinnerSuggestions = document.querySelector("dinner-suggestions");
 
 //Event listeners:
-letsCookButton.addEventListener("click", displayRandomFood());
+letsCookButton.addEventListener("click", displaySideDish);
 
 //Functions:
-//want to get all 3 functions into one mega function with if statements...somehow...
+//want to get all 3 functions into one mega-function with if statements...somehow...
+
+//toggle event here?
+//instead of adding a class and removing it
+//cookPotImage.classList.toggle(" ");
+
+function displaySideDish() {
+  if (sideButton.checked) {
+    dinnerSuggestions.classList.remove("hidden");
+    dinnerSuggestions.innerText = `${sides[getRandomIndex(sides)]}`;
+  }
+}
 
 function displayRandomFood() {
   event.preventDefault();
   cookPotImage.classList.add("hidden");
   dinnerSuggestions.classList.remove("hidden");
 }
+displayRandomFood();
 
-function displaySideDish() {
-  if (sideButton.checked) {
-    dinnerSuggestions.innerText = `${sides[getRandomIndex(sides)]}!`;
-    displayRandomFood();
-  }
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
 }
 
-function getRandomIndex(foodLists) {
-  return Math.floor(Math.random() * foodLists.length);
-}
+// function displayMeal() {
+//   cookpot.classList.add("hidden");
+// }
+
 //if side button clicked,
 //then display random selection in box 2
 //and hide pot

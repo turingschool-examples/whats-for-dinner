cc55a7bc6f2c6ee9deef5bb067bf22c690a812f1
Nikhila Balmoori <nbalmoori@gmail.com>
2022-02-20 13:08:45 -0700

    Clean up code to remove comments and unnecessary spacing

--- main.js
+++ main.js
@@ -1,26 +1,17 @@
-
-
 var letsCookButton = document.querySelector('#lets-cook');
-
 var cookpotView = document.querySelector('.cookpot-view');
 var dishViewHidden = document.querySelector('.dish-view.hidden');
-
 var displayedDish = document.querySelector('.dish-result');
 
 letsCookButton.addEventListener('click', populateDish);
-
-
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 };
 
-
 function populateDish() {
-
   cookpotView.classList.add('hidden');
   dishViewHidden.classList.remove('hidden');
-
   if (document.getElementById('side').checked) {
     displayedDish.innerText = `${sides[getRandomIndex(sides)]}!`
   } else if (document.getElementById('main').checked) {

465f9629a52ec9f8f6c5fe780f496969217995ea
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-02-19 18:50:50 -0700

    Complete side-main-dessert functionality

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-// When a user selects a dish option and then clicks the “Let’s Cook!” button, the user sees a random dish from the list of possible dishes for that category
-
 //Query Selectors:
 var sideButton = document.querySelector(".side");
 var mainDishButton = document.querySelector(".main-dish");
 var dessertsButton = document.querySelector(".dessert");
 var entireMealButton = document.querySelector(".entire-meal");
 var letsCookButton = document.querySelector(".button2");
-//var addARecipeButton = document.querySelector(".button1");
 var cookPotImage = document.querySelector(".cookpot");
 var dinnerSuggestions = document.querySelector(".suggestions");
 var youShouldMake = document.querySelector(".h3");
 
+//Event Listeners:
+
 letsCookButton.addEventListener("click", showRandomFood);
-//window.onload = showBeforeSelection;
-letsCookButton.addEventListener("click", viewOrHideElement);
-/*------------------------FUNCTIONS-------------------------*/
+
+//Functions:
 
 function showRandomFood() {
   event.preventDefault();
@@ -25,31 +23,18 @@ function showRandomFood() {
   } else if (dessertsButton.checked) {
     dinnerSuggestions.innerHTML = `${desserts[getRandomIndex(desserts)]}!`;
   }
-  showBox2();
+
+  hide(cookPotImage);
+  show(youShouldMake);
+  show(dinnerSuggestions);
 }
 
-function displaySelection() {
-  cookPotImage.classList.add("hidden");
-  youShouldMake.classList.remove("hidden");
-  dinnerSuggestions.classList.remove("hidden");
-  //clearButton.classList.remove('hidden');
-}
-// function showBeforeSelection() {
-//   hideElement(youShouldMake);
-//   hideElement(dinnerSuggestions);
-//   //viewElement(clearButton);
-//   viewElement(cookPotImage);
-// }
-function showBox2() {
-  viewElement(youShouldMake);
-  viewElement(dinnerSuggestions);
-  //viewElement(clearButton);
-  hideElement(cookPotImage);
+function hide(element) {
+  element.classList.add("hidden");
 }
 
-function viewOrHideElement(varToView) {
-  varToView.classList.remove("hidden");
-  varToView.classList.add("hidden");
+function show(element) {
+  element.classList.remove("hidden");
 }
 
 function getRandomIndex(array) {

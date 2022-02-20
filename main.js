2803bc8a08b0ce96c4803c333f8d276bc53edb40
Nikhila Balmoori <nbalmoori@gmail.com>
2022-02-20 13:05:50 -0700

    Fix styling for entire meal result display

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ function getRandomIndex(array) {
 
 function populateDish() {
 
-  cookpotView.className = 'cookpot-view hidden';
-  dishViewHidden.className = 'dish-view';
+  cookpotView.classList.add('hidden');
+  dishViewHidden.classList.remove('hidden');
 
   if (document.getElementById('side').checked) {
     displayedDish.innerText = `${sides[getRandomIndex(sides)]}!`
@@ -29,5 +29,6 @@ function populateDish() {
     displayedDish.innerText = `${desserts[getRandomIndex(desserts)]}!`
   } else if (document.getElementById('entire').checked) {
     displayedDish.innerText = `${mains[getRandomIndex(mains)]} with a side of ${sides[getRandomIndex(sides)]} and ${desserts[getRandomIndex(desserts)]} for dessert!`;
+    displayedDish.classList.add('entire-meal')
   }
 };

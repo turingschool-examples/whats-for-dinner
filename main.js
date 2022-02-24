90952cf09b247295bbc50bcccb3ebe7a142c8ab4
Nikhila Balmoori <nbalmoori@gmail.com>
2022-02-24 09:19:38 -0700

    refactor alert if no option is selected to keep cookpot view

--- main.js
+++ main.js
@@ -13,20 +13,26 @@ function getRandomIndex(array) {
 };
 
 function populateDish() {
-  cookpotView.classList.add('hidden');
-  dishViewHidden.classList.remove('hidden');
   if (document.getElementById('side').checked) {
     displayedDish.innerText = `${sides[getRandomIndex(sides)]}!`
     displayedDish.classList.remove('entire-meal')
+    cookpotView.classList.add('hidden');
+    dishViewHidden.classList.remove('hidden');
   } else if (document.getElementById('main').checked) {
     displayedDish.innerText = `${mains[getRandomIndex(mains)]}!`
     displayedDish.classList.remove('entire-meal')
+    cookpotView.classList.add('hidden');
+    dishViewHidden.classList.remove('hidden');
   } else if (document.getElementById('dessert').checked) {
     displayedDish.innerText = `${desserts[getRandomIndex(desserts)]}!`
     displayedDish.classList.remove('entire-meal')
+    cookpotView.classList.add('hidden');
+    dishViewHidden.classList.remove('hidden');
   } else if (document.getElementById('entire').checked) {
     displayedDish.innerText = `${mains[getRandomIndex(mains)]} with a side of ${sides[getRandomIndex(sides)]} and ${desserts[getRandomIndex(desserts)]} for dessert!`;
     displayedDish.classList.add('entire-meal')
+    cookpotView.classList.add('hidden');
+    dishViewHidden.classList.remove('hidden');
   } else {
     alert("Pick an option!")
   }

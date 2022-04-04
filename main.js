9b2b65edbb3d392e35e8de4aa645c112fb6bc872
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-04-04 17:45:23 -0600

    Add function to form to return value if checked

--- main.js
+++ main.js
@@ -11,3 +11,18 @@ let mainrecipes = ["Spaghetti and Meatballs",
 "Chicken Fried Rice",
 "Sheet Pan Fajitas",
 "Margarita Pizza"]
+
+
+let userInputForm = document.querySelectorAll(".radioButtons");
+let submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click", formHandler);
+
+function formHandler(){
+  event.preventDefault();
+  for (let i = 0; i < 4; i++) {
+    if (userInputForm[i].checked) {
+      console.log(userInputForm[i].value)
+    }
+  }
+}

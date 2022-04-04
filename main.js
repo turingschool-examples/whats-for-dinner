47e7956a83aa795ea89ccb67e9d214c62e276e09
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-04-04 17:55:59 -0600

    Add function to return random recipe based on selection

--- main.js
+++ main.js
@@ -17,12 +17,34 @@ let userInputForm = document.querySelectorAll(".radioButtons");
 let submitButton = document.getElementById("submit");
 
 submitButton.addEventListener("click", formHandler);
+submitButton.addEventListener("click", returnRecipe);
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function getRandomElement(array) {
+  let randomNum = (getRandomIndex(array));
+  let randomItem = array[randomNum]
+  return randomItem
+}
 
 function formHandler(){
   event.preventDefault();
   for (let i = 0; i < 4; i++) {
     if (userInputForm[i].checked) {
-      console.log(userInputForm[i].value)
+      let selection = userInputForm[i].value;
+      console.log(selection);
+      returnRecipe(selection);
     }
+  }
+}
+
+
+function returnRecipe(selection) {
+  if (selection == "main") {
+    let recipe = getRandomElement(mainrecipes);
+    console.log(recipe)
+    return recipe
   }
 }

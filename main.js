38cf0a1df4f56909d3ad434f56e98f137a658b4d
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-04-04 17:03:51 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let mainrecipes = ["Spaghetti and Meatballs",
+"Pineapple Chicken",
+"Shakshuka",
+"Thai Yellow Curry",
+"Bibimbap",
+"Chicken Parmesean",
+"Butternut Squash Soup",
+"BBQ Chicken Burgers",
+"Ramen",
+"Empanadas",
+"Chicken Fried Rice",
+"Sheet Pan Fajitas",
+"Margarita Pizza"]

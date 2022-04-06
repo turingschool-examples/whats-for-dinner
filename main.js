ee2948a616448268d708567458c9d882b1bf90d4
Blue Jessen <canealisblue@gmail.com>
2022-04-06 14:23:38 -0700

    Add basic variable and function skeletons

--- main.js
+++ main.js
@@ -1,4 +1,55 @@
 //querySelectors
+var recipePickForm = document.querySelector('input');
+var letsCookButton = document.querySelector('.lets-cook-button');
+var addARecipeButton = document.querySelector('.add-a-recipe');
 
+//event listeners
+recipePickForm.addEventListener('click',takeInInput(event));
+letCookButton.addEventListener('click', randomRecipe);
+addARecipeButton.addEventListener('click', addARecipe);
+
+
+//global variables
+var desserts = [
+  "Banana Bread",
+  "Ciambella",
+  "French Silk Pie",
+  "Cream Horns",
+  "Rice Pudding",
+  "Strawberry Champagne Cake"
+];
+
+var sides = [
+"Miso Glazed Carrots",
+"Coleslaw",
+"Garden Salad",
+"Crispy Potatoes",
+"Sweet Potato Tots",
+"Coconut Rice",
+"Caeser Salad",
+"Shrimp Summer Rolls",
+"Garlic Butter Mushrooms",
+"Hush Puppies"
+];
+
+var mainDish = [
+  "Ramen",
+  "Nachos",
+  "Burritos",
+  "Tofu Potato Curry",
+  "Lemon Seared Salmon",
+];
 
 //functions
+// Build out own random function
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};
+
+function randomRecipe(){
+
+}
+
+function addARecipe(){
+
+}

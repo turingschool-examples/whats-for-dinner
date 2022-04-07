94149fdbffc5d7848beec4eda798e9f59f0b6bb7
Amanda Sands <amandajean555@gmail.com>
2022-04-06 20:35:12 -0600

    Add query selectors

---
+++ main.js
@@ -0,0 +1,10 @@
+//Query Selector radio buttons
+
+var sideButton = document.querySelector("#side");
+var mainButton = document.querySelector("#mainDish");
+var dessertButton = document.querySelector("#dessert");
+var entireMealButton = document.querySelector("#entireMeal");
+
+//Query selector other buttons
+
+var cookButton = document.querySelector("#letsCook"); 

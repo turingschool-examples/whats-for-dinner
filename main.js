bee345823698fde4fd89c95fd77f2b301aee4d78
Amanda Sands <amandajean555@gmail.com>
2022-04-06 20:47:27 -0600

    Add dish arrays to javascript file

--- main.js
+++ main.js
@@ -7,4 +7,57 @@ var entireMealButton = document.querySelector("#entireMeal");
 
 //Query selector other buttons
 
-var cookButton = document.querySelector("#letsCook"); 
+var cookButton = document.querySelector("#letsCook");
+
+
+//Dish arrays
+
+var sidesArray = [
+'Miso Glazed Carrots',
+'Coleslaw',
+'Garden Salad',
+'Crispy Potatoes',
+'Sweet Potato Tots',
+'Coconut Rice',
+'Caeser Salad',
+'Shrimp Summer Rolls',
+'Garlic Butter Mushrooms',
+'Hush Puppies'
+]
+
+var mainsArray = [
+'Spaghetti and Meatballs',
+'Pineapple Chicken',
+'Shakshuka',
+'Thai Yellow Curry',
+'Bibimbap',
+'Chicken Parmesean',
+'Butternut Squash Soup',
+'BBQ Chicken Burgers',
+'Ramen',
+'Empanadas',
+'Chicken Fried Rice',
+'Sheet Pan Fajitas',
+'Margarita Pizza'
+]
+
+var dessertArray = [
+'Apple Pie',
+'Lemon Meringue Pie',
+'Black Forest Cake',
+'Banana Bread',
+'Peach Cobbler',
+'Cheesecake',
+'Funfetti Cake',
+'Baklava',
+'Flan',
+'Macarons',
+'Macaroons',
+'Chocolate Cupcakes',
+'Pavlova',
+'Pumpkin Pie',
+'Key Lime Pie',
+'Tart Tatin',
+'Croissants',
+'Eclairs'
+]

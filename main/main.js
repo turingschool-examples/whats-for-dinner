68de10b1f3456189a0503fdc8789f5ae9dc65e4b
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-04-07 21:58:12 -0600

    Refactor code to apply style changes

--- main/main.js
+++ main/main.js
@@ -46,20 +46,28 @@ let desserts = [
 "Croissants",
 "Eclairs"]
 
-window.addEventListener("load", displayCustomizedFeatures);
+
+//window.addEventListener("load", displayCustomizedFeatures);
+
 
 let favoriteRecipes = [];
 
 
 let userInputForm = document.querySelectorAll(".radioButtons");
 let submitButton = document.getElementById("submit");
-let resultsWindow = document.querySelector(".recipeDisplayContainer");
-let userTitle = document.getElementById("title");
+let resultsWindow = document.getElementById("recipeDisplayContainer");
+let userTitle = document.getElementById("welcome-message");
 let navbar = document.querySelector(".navbar");
-let bodyImage = document.querySelector(".main-container");
-let boxes = document.querySelectorAll(".main");
+let bodyImage = document.querySelector(".display-container");
+let boxes = document.querySelectorAll(".display");
+let themedChanges = document.querySelectorAll(".customizable");
+let userInputTheme = localStorage.getItem("theme")
+let username = localStorage.getItem("username")
+
+
 
 submitButton.addEventListener("click", formHandler);
+displayCustomizedFeatures(userInputTheme);
 
 
 function getRandomIndex(array) {
@@ -84,36 +92,40 @@ function formHandler(){
 }
 
 function returnRecipe(selection) {
+      let recipeMain = getRandomElement(mainrecipes);
+      let recipeSide = getRandomElement(siderecipes);
+      let recipeDessert = getRandomElement(desserts);
   if (selection == "main") {
-    let recipe = getRandomElement(mainrecipes);
-    return recipe;
+    return recipeMain;
   } else if (selection == "side") {
-    let recipe = getRandomElement(siderecipes);
-    return recipe;
+    return recipeSide;
   } else if (selection == "dessert") {
-    let recipe = getRandomElement(desserts);
-    return recipe;
+    return recipeDessert;
+  } else if (selection == "entire-meal") {
+    let mealString = `<div class="dishes-container"><h3 class="dish-title">Main dish:</h3><div class="dishes">${recipeMain}</div>
+    <h3 class="dish-title">Side dish:</h3><div class="dishes">${recipeSide}</div>
+    <h3 class="dish-title">Dessert:</h3><div class="dishes">${recipeDessert}</div>`
+    return mealString
   }
 }
 
 function displayResults(randomRecipe) {
-
   let thing = document.getElementById("results-string-container");
   thing.innerText = "You should have: "
-  resultsWindow.innerText = randomRecipe;
+  resultsWindow.innerHTML = randomRecipe;
 }
 
-function displayCustomizedFeatures() {
-  if (localStorage.length >= 1){
-  let username = localStorage.getItem("username")
-  userTitle.innerText = `What's for dinner, ${username}?`
-
-  let theme = localStorage.getItem("theme")
-  if (theme == "dreamy") {
-    navbar.className = "navbar dreamy"
-    bodyImage.className = "main-container dreamy"
-    boxes[0].className = "main userInputForm dreamy"
-    boxes[1].className = "main contain dreamy"
+function displayCustomizedFeatures(userInputTheme) {
+  displayUsername()
+  for (let i = 0; i < themedChanges.length; i++) {
+    themedChanges[i].classList.add(userInputTheme)
     }
+  }
+
+function displayUsername(){
+  if (username == false){
+    console.log("No username")
+  } else {
+    userTitle.innerText = `What's for dinner, ${username}?`
   }
 }

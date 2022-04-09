cf009578271f195fe58022a3a1ba451bf12d0c54
Cleveland Ticoalu <ayocleveland@gmail.com>
2022-04-09 15:35:27 -0600

    Add arrays

--- main.js
+++ main.js
@@ -1,1 +1,61 @@
+var letsCookButton = document.querySelector('.lets-cook-button');
 
+var radioButtons = document.querySelectAll('.light');
+
+.addEventListener('click')
+
+var randomSides = sidesArray[Math.floor(math.random() * myArray.length)];
+
+var sidesArray = [
+"Miso Glazed Carrots",
+"Coleslaw",
+"Garden Salad",
+"Crispy Potatoes",
+"Sweet Potato Tots",
+"Coconut Rice",
+"Caeser Salad",
+"Shrimp Summer Rolls",
+"Garlic Butter Mushrooms",
+"Hush Puppies"
+];
+
+var mainsArray = [
+"Spaghetti and Meatballs",
+"Pineapple Chicken",
+"Shakshuka",
+"Thai Yellow Curry",
+"Bibimbap",
+"Chicken Parmesean",
+"Butternut Squash Soup",
+"BBQ Chicken Burgers",
+"Ramen",
+"Empanadas",
+"Chicken Fried Rice",
+"Sheet Pan Fajitas",
+"Margarita Pizza"
+];
+
+var dessertsArray = [
+"Apple Pie",
+"Lemon Meringue Pie",
+"Black Forest Cake",
+"Banana Bread",
+"Peach Cobbler",
+"Cheesecake",
+"Funfetti Cake",
+"Baklava",
+"Flan",
+"Macarons",
+"Macaroons",
+"Chocolate Cupcakes",
+"Pavlova",
+"Pumpkin Pie",
+"Key Lime Pie",
+"Tart Tatin",
+"Croissants",
+"Eclairs"
+];
+
+var randomSides = sidesArray[Math.floor(math.random() * myArray.length)];
+var randomMains = mainsArray[Math.floor(math.random() * myArray.length)];
+var randomSides = dessertsArray[Math.floor(math.random() * myArray.length)];

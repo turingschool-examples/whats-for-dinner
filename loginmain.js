48d7d60d9cb4de30c96f74b89838d38831003b66
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-04-09 12:44:37 -0600

    Clean up unused code

--- loginmain.js
+++ loginmain.js
@@ -3,28 +3,8 @@ let username = document.getElementById("input0");
 let theme = document.getElementById("customizeTheme");
 let submitButton = document.getElementById("submit");
 
-//addEventListener('storage', function(e){
-//   console.log(`The value of the ${e.key} changed for the ${e.domain}.`);
-//});
-
 submitButton.addEventListener("click", storeData)
 
-//function render() {
-//  display.innerHTML += `<section class="log-in-form">
-//  <div class="input"><input type="text" name="user-log-in" class="input-field" id="input0" value="">
-//  <label for="name">Username: </label></div>
-//  <div class="input"><input type="text" name="user-log-in" class="input-field" id="input1" value="">
-//  <label for="theme">Choose a Theme: </label></div>
-//  </section>`
-//}
-
-
-
-//render();
-
-
-
-// Set name:value pair to local storage: window.localStorage.setItem('theme','dark');
 
 function storeData() {
   let inputUserName = username.value

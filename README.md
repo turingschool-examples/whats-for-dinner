b9200911705b428e209b3819ac6cf4312c28ef68
Blue Jessen <canealisblue@gmail.com>
2022-04-09 22:52:29 -0700

    Take out initial read me information

--- README.md
+++ README.md
@@ -2,22 +2,11 @@
 
 ## Overview
 
-What's for dinner? The ultimate question. You will be building an app that helps users choose a recipe and put together meals. 
+What's for dinner? The ultimate question.
 
-This Solo Challenge gives students and instructors the opportunity to get a pulse on where you are with the foundational concepts of Module 1 curriculum. Students should use this as an opportunity to challenge themselves and work completely independently. Google can (and probably should!) be used, but any other code base should not be referenced. Instructors will be able to use your work, both completion of functionality and code quality, to determine where you stand and if you are behind for this point in the module, provide supports to intervene.
+The spec for this project can be found [here](https://frontend.turing.io/projects/module-1/dinner.html).
 
-The spec for this project can be found [here](https://frontend.turing.io/projects/module-1/dinner.html). 
+## Lessons Learned
 
-## Learning Goals
-
-- Gain experience building an application that utilizes HTML, CSS and JavaScript
-- Write HTML and CSS to match a provided comp
-- Understand how to listen to and respond to user events
-- Individualize your programming skill set
 
 ## Setup
-
-- Fork this project to your own Github account
-- Clone the repository to your local machine
-- `cd` into the project
-- Read this README thoroughly, then begin working!
--- main.js
+++ main.js
@@ -1,103 +1,107 @@
-//querySelectors
-var recipePickForm = document.querySelector('.recipe-item-form');
-var letsCookButton = document.querySelector('.lets-cook-button');
-var menuItemChoice = document.getElementsByName('options');
-var displayRecipeItem = document.querySelector('.random-recipe');
-var presentRecipeText = document.querySelector('.recipe-intro');
-var cookPot = document.querySelector('.cookpot');
-var clearButton = document.querySelector('.clear-button');
-var required = document.querySelector('.required');
+  //querySelectors
+  var recipePickForm = document.querySelector('.recipe-item-form');
+  var letsCookButton = document.querySelector('.lets-cook-button');
+  var menuItemChoice = document.getElementsByName('options');
+  var displayRecipeItem = document.querySelector('.random-recipe');
+  var presentRecipeText = document.querySelector('.recipe-intro');
+  var cookPot = document.querySelector('.cookpot');
+  var clearButton = document.querySelector('.clear-button');
+  var required = document.querySelector('.required');
 
-//event listeners
-letsCookButton.addEventListener('click', check);
-clearButton.addEventListener('click', clearPanel);
+  //event listeners
+  letsCookButton.addEventListener('click', check);
+  clearButton.addEventListener('click', clearPanel);
 
-//global variables
-var desserts = [
-  "Banana Bread",
-  "Ciambella",
-  "French Silk Pie",
-  "Cream Horns",
-  "Rice Pudding",
-  "Strawberry Champagne Cake"
-];
-var sides = [
-"Miso Glazed Carrots",
-"Coleslaw",
-"Garden Salad",
-"Crispy Potatoes",
-"Sweet Potato Tots",
-"Coconut Rice",
-"Caeser Salad",
-"Shrimp Summer Rolls",
-"Garlic Butter Mushrooms",
-"Hush Puppies"
-];
-var mainDish = [
-  "Ramen",
-  "Nachos",
-  "Burritos",
-  "Tofu Potato Curry",
-  "Lemon Seared Salmon",
-];
+  //global variables
+  var desserts = [
+    "Banana Bread",
+    "Ciambella",
+    "French Silk Pie",
+    "Cream Horns",
+    "Rice Pudding",
+    "Strawberry Champagne Cake"
+  ];
+  var sides = [
+  "Miso Glazed Carrots",
+  "Coleslaw",
+  "Garden Salad",
+  "Crispy Potatoes",
+  "Sweet Potato Tots",
+  "Coconut Rice",
+  "Caeser Salad",
+  "Shrimp Summer Rolls",
+  "Garlic Butter Mushrooms",
+  "Hush Puppies"
+  ];
+  var mainDish = [
+    "Ramen",
+    "Nachos",
+    "Burritos",
+    "Tofu Potato Curry",
+    "Lemon Seared Salmon",
+  ];
 
-var menuOptions = {
-  desserts: desserts,
-  sides: sides,
-  mainDish: mainDish
-};
+  var menuOptions = {
+    desserts: desserts,
+    sides: sides,
+    mainDish: mainDish
+  };
 
-//functions
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
+  //functions
 
-function check(){
-  event.preventDefault();
-    for (var i = 0; i < menuItemChoice.length; i++) {
-         if (menuItemChoice[i].checked) {
-              menuItemChoice[i].checked = false;
-              return letsCookInput(menuItemChoice[i].value);
+  //Data Model
+  function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
+  function check(){
+    event.preventDefault();
+      for (var i = 0; i < menuItemChoice.length; i++) {
+           if (menuItemChoice[i].checked) {
+                menuItemChoice[i].checked = false;
+                return letsCookInput(menuItemChoice[i].value);
+           }
          }
+         required.classList.remove('hidden');
        }
-       required.classList.remove('hidden');
-     }
 
-function letsCookInput(choice){
-      if(choice === "Entire Meal") {
-        displayFullMeal();
-      }
-    var result = menuOptions[choice][getRandomIndex(menuOptions[choice])];
+  function letsCookInput(choice){
+        if (choice === "Entire Meal") {
+          displayFullMeal();
+        }
+
+      var result = menuOptions[choice][getRandomIndex(menuOptions[choice])];
+      displayRecipe(result);
+    }
+
+  //DOM or Display
+
+  function displayRecipe(recipe) {
+    required.classList.add('hidden')
+    displayRecipeItem.classList.remove('hidden');
+    presentRecipeText.classList.remove('hidden');
+    cookPot.classList.add('hidden');
+    clearButton.classList.remove('hidden');
+
+    displayRecipeItem.innerText = recipe + '!';
+  }
+
+  function displayFullMeal() {
+    displayRecipeItem.classList.remove('random-recipe');
+    displayRecipeItem.classList.add('random-recipe-smaller');
+
+    var result = '';
+    result += `${mainDish[getRandomIndex(mainDish)]} with a side `;
+    result += `of ${sides[getRandomIndex(sides)]} and `;
+    result += `${desserts[getRandomIndex(desserts)]} for dessert`;
+
     displayRecipe(result);
   }
 
-
-function displayRecipe(recipe) {
-  required.classList.add('hidden')
-  displayRecipeItem.classList.remove('hidden');
-  presentRecipeText.classList.remove('hidden');
-  cookPot.classList.add('hidden');
-  clearButton.classList.remove('hidden');
-
-  displayRecipeItem.innerText = recipe + '!';
-}
-
-function displayFullMeal() {
-  displayRecipeItem.classList.remove('random-recipe');
-  displayRecipeItem.classList.add('random-recipe-smaller');
-
-  var result = '';
-  result += `${mainDish[getRandomIndex(mainDish)]} with a side `;
-  result += `of ${sides[getRandomIndex(sides)]} and `;
-  result += `${desserts[getRandomIndex(desserts)]} for dessert`;
-
-  displayRecipe(result);
-}
-
-function clearPanel() {
-  displayRecipeItem.classList.add('hidden');
-  presentRecipeText.classList.add('hidden');
-  cookPot.classList.remove('hidden');
-  clearButton.classList.add('hidden');
-  displayRecipeItem.innerText = '';
-}
+  function clearPanel() {
+    displayRecipeItem.classList.add('hidden');
+    presentRecipeText.classList.add('hidden');
+    cookPot.classList.remove('hidden');
+    clearButton.classList.add('hidden');
+    displayRecipeItem.innerText = '';
+  }

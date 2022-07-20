74c2f65c0d6be534e425ee237e4e731726a3b8c4
Victoria Fields <victoriaashleyfields@gmail.com>
2022-07-20 16:17:57 -0600

    Refactor: added CSS rules to alternative suggestions section display

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 // Data Model
-
 var sides = ['Miso Glazed Carrots', 'Coleslaw', 'Garden Salad',
 'Crispy Potatoes', 'Sweet Potato Tots', 'Coconut Rice', 'Caeser Salad',
 'Shrimp Summer Rolls', 'Garlic Butter Mushrooms', 'Hush Puppies']
 
 var mains = ['Spaghetti and Meatballs', 'Pineapple Chicken', 'Shakshuka',
-'Thai Yellow Curry', 'Bibimbap', 'Chicken Parmesean', 'Butternut Squash Soup'
+'Thai Yellow Curry', 'Bibimbap', 'Chicken Parmesean', 'Butternut Squash Soup',
 'BBQ Chicken Burgers', 'Ramen', 'Empanadas', 'Chicken Fried Rice',
 'Sheet Pan Fajitas', 'Margarita Pizza']
 
@@ -15,7 +14,36 @@ var desserts = ['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake',
 'Key Lime Pie', 'Tart Tatin', 'Croissants', 'Eclairs']
 
 // DOM elements
-
+var userSelectedDish = document.querySelector('input[name="dish"]:checked').value;
 var letsCookButton = document.querySelector('.letsCookButton');
+var suggestionsSection = document.querySelector('.suggestionsContainer');
 
-// letsCookButton.addEventListender('click', ...
+// Event listeners
+letsCookButton.addEventListender('click', displaySuggestion)
+
+// Display functions
+function displayCookpot() {
+  suggestionsSection.innerHTML += `
+  <div class="cookpotImgContainer">
+    <img src="assets/cookpot.svg">
+  </div>
+  `;
+}
+
+displayCookpot();
+
+//
+
+function displaySuggestion() {
+  suggestionsSection.innerHTML = '';
+  if (userSelectedDish === 'side') {
+    // grab a random index of the sides array and display it on the page;
+    suggestionsSection.innerHTML += `
+    
+    `
+  }
+}
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}

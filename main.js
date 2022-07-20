73c40817bd44ec3fd8735b93bc8f9340b684a71d
Victoria Fields <victoriaashleyfields@gmail.com>
2022-07-20 15:25:57 -0600

    Refactor: added name attributes to HTML inputs

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Data Model
+
 var sides = ['Miso Glazed Carrots', 'Coleslaw', 'Garden Salad',
 'Crispy Potatoes', 'Sweet Potato Tots', 'Coconut Rice', 'Caeser Salad',
 'Shrimp Summer Rolls', 'Garlic Butter Mushrooms', 'Hush Puppies']
@@ -11,3 +13,9 @@ var desserts = ['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake',
 'Banana Bread', 'Peach Cobbler', 'Cheesecake', 'Funfetti Cake', 'Baklava',
 'Flan', 'Macarons', 'Chocolate Cupcakes', 'Pavlova', 'Pumpkin Pie',
 'Key Lime Pie', 'Tart Tatin', 'Croissants', 'Eclairs']
+
+// DOM elements
+
+var letsCookButton = document.querySelector('.letsCookButton');
+
+// letsCookButton.addEventListender('click', ...

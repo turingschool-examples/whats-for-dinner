62577668d0c79f5e2aa76b9e5271c749acf0fc89
Victoria Fields <victoriaashleyfields@gmail.com>
2022-07-20 17:04:08 -0600

    Refactor: added event listener to window, troubleshooting displaySuggestion function

--- main.js
+++ main.js
@@ -14,35 +14,44 @@ var desserts = ['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake',
 'Key Lime Pie', 'Tart Tatin', 'Croissants', 'Eclairs']
 
 // DOM elements
-var userSelectedDish = document.querySelector('input[name="dish"]:checked').value;
 var letsCookButton = document.querySelector('.letsCookButton');
+var addRecipeButton = document.querySelector('.addRecipeButton');
 var suggestionsSection = document.querySelector('.suggestionsContainer');
 
 // Event listeners
-letsCookButton.addEventListender('click', displaySuggestion)
+window.addEventListener('load', displayCookpot);
+// letsCookButton.addEventListener('click', displaySuggestion);
+
 
 // Display functions
+
 function displayCookpot() {
   suggestionsSection.innerHTML += `
   <div class="cookpotImgContainer">
     <img src="assets/cookpot.svg">
   </div>
-  `;
+  `
 }
-
-displayCookpot();
 
 //
 
-function displaySuggestion() {
-  suggestionsSection.innerHTML = '';
-  if (userSelectedDish === 'side') {
-    // grab a random index of the sides array and display it on the page;
-    suggestionsSection.innerHTML += `
-    
-    `
-  }
-}
+// function displaySuggestion() {
+//   suggestionsSection.innerHTML = '';
+//   var newSide = sides[getRandomIndex(sides)];
+//   var newMain = mains[getRandomIndex(mains)];
+//   var newDessert = desserts[getRandomIndex(desserts)];
+//   var userSelectedDish = document.querySelector('input[name="dish"]:checked').value;
+//   if (userSelectedDish === 'side') {
+//     // grab a random index of the sides array and display it on the page;
+//     suggestionsSection.innerHTML += `
+//     <div class="suggestion">
+//       <p class="youShouldMake">You should make:</p>
+//       <h1 class="foodSuggestion">${newSide}</h1>
+//     </div>
+//     `
+//   }
+//   console.log('this function ran');
+// }
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);

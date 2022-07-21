b7222debde915559a254be12cf9be6c0ecc81d21
Victoria Fields <victoriaashleyfields@gmail.com>
2022-07-20 18:53:53 -0600

    Refactor: added favorites button with introductory functionality

--- main.js
+++ main.js
@@ -18,11 +18,13 @@ var favorites = [];
 // DOM elements
 var letsCookButton = document.querySelector('.letsCookButton');
 var viewFavoritesButton = document.querySelector('#viewFavoritesButton');
+var userInputSection = document.querySelector('.userInputSection');
 var suggestionsSection = document.querySelector('.suggestionsSection');
 
 // Event listeners
 window.addEventListener('load', displayCookpot);
 letsCookButton.addEventListener('click', displaySuggestion);
+viewFavoritesButton.addEventListener('click', displayFavorites);
 
 // Display functions
 function displayCookpot() {
@@ -72,7 +74,22 @@ function displaySuggestion() {
   }
 };
 
+function displayFavorites() {
+  userInputSection.classList.toggle('hidden');
+  suggestionsSection.classList.toggle('hidden');
+}
+
 // Nondisplay functions
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
+/*
+When a recipe appears, it should appear with a “Favorite” button.
+When the “Favorite” button is clicked, that recipe should be added to a new list of favorite recipes.
+
+When the “View Favorites” button is clicked, users should be taken to a new page that displays all of their favorite recipes.
+Users should be able to navigate back to the main page by clicking a button.
+Users should be able to remove a recipe from their list of favorites, by clicking a button.
+As you add these new elements to the page, be sure to match the style of existing elements.
+*/

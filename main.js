efc64d8a5c76f5c4ad1f8a8dc0574b1ff9b91b05
Hannah.Celemen <hcelemen@icloud.com>
2022-07-20 21:17:41 -0600

    Add arrays for each course

---
+++ main.js
@@ -0,0 +1,80 @@
+//query selectors below
+
+
+
+//arrays for each course
+let sides = [
+  "Steamed Edamame",
+  "Garden Salad",
+  "Seaweed Salad",
+  "Ginger Salad",
+  "Coconut Shrimp",
+  "Hush Puppies",
+  "Crispy Potatoes",
+  "Garlic Butter Mushrooms",
+  "Sweet Potato Fries",
+  "Steamed Green Beans",
+  "Fruit Salad",
+  "Mexican Corn on the Cob",
+  "Roasted Veggies",
+  "Sauteed Zucchini and Squash",
+  "Quinoa and Broccoli Slaw",
+  "Ceviche and Taro Chips",
+  "Takoyaki"
+];
+
+let mains = [
+  "Pineapple Chicken",
+  "Hawaiian Pizza",
+  "Thai Yellow Curry",
+  "Bibimbap",
+  "BBQ Wings",
+  "Yakitori",
+  "Creamy Tomato Pasta",
+  "Beef Bulgogi",
+  "Adobo",
+  "Sinigang",
+  "Sweet Crispy Korean Fried Chicken",
+  "Korean BBQ Pork Ribs",
+  "Jajangmyeon",
+  "Dragon Roll Sushi",
+  "Tonkotsu Ramen",
+  "Chicken Fried Rice",
+  "Pad Thai",
+  "Lamb Korma",
+  "Chicken Tikka Masala",
+  "General Tso Chicken",
+  "Sesame Chicken"
+];
+
+let desserts = [
+  "Apple Pie",
+  "Vanila Ice Cream",
+  "Mango Ice Cream",
+  "Ube Ice Cream",
+  "Macaroons",
+  "Peach Cobbler",
+  "Lemon Meringue Pie",
+  "Banana Cream Pie",
+  "Black Forest Cake",
+  "Pavlova",
+  "Pumpkin Pie",
+  "Chocolate-Filled Croissants",
+  "Halo-Halo",
+  "Candied Sweet Potato",
+  "Macha Cake",
+  "Strawberry Mochi"
+];
+
+//event listeners below
+
+
+
+
+
+//functions below
+
+
+
+
+

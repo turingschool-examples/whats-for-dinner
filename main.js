27e5a5b780c7d1e4601913b77288882ef44d6014
Alycia Canavan <alycia.canavan@gmail.com>
2022-07-21 21:41:41 -0700

    Finish generateRandom function

--- main.js
+++ main.js
@@ -56,17 +56,18 @@ var mains = ["Spaghetti and Meatballs",
 var cornerAddBtn = document.querySelector("#corner-add-button")
 var letsCookBtn = document.querySelector("#lets-cook")
 var clearBtn = document.querySelector("#clear-btn")
-var sideChooser = document.querySelector("#side")
-var mainChooser = document.querySelector("#main-dish")
-var dessertChooser = document.querySelector("#dessert")
+var sideChooser = document.getElementById("sides")
+var mainChooser = document.querySelector("#mains")
+var dessertChooser = document.querySelector("#desserts")
 var wholeMealChooser = document.querySelector("#entire-meal")
-var allChoosers = document.querySelectorAll("#radio")
-var choosers = [sideChooser, mainChooser, dessertChooser, wholeMealChooser]
-//event listeners:
-//this will generate the random output:
-for (var i = 0; i < choosers.length; i++) {
-  choosers[i].addEventListener("input", generateRandom)
+var allChoosers = [sideChooser, mainChooser, dessertChooser, wholeMealChooser]
+
+//EVENT LISTENERS:
+//add event listener to each radio button:
+for (var i = 0; i < allChoosers.length; i++) {
+  allChoosers[i].addEventListener("input", generateRandom)
 }
+
 //this will trigger the output to display:
 letsCookBtn.addEventListener("click", displayRandom)
 // clearBtn.addEventListener("click", clearOutput)
@@ -79,26 +80,24 @@ function getRandomIndex(array) {
 
 function generateRandom(event) {
   var dishType = event.target.id
-  console.log(dishType)
   if (dishType === "entire-meal") {
-    var wholeMeal = { main: getRandomIndex(mains),
-      side: getRandomIndex(sides),
-      dessert: getRandomIndex(desserts) }
-      console.log(wholeMeal)
-      console.log(mains[wholeMeal.main], sides[wholeMeal.side], desserts[wholeMeal.dessert])
+    var wholeMeal = { main: mains[getRandomIndex(mains)],
+      side: sides[getRandomIndex(sides)],
+      dessert: desserts[getRandomIndex(desserts)] }
       return wholeMeal
-    } else {
-      var chosenNum = (getRandomIndex(dishType))
-      console.log(dishType[chosenNum])
-    return getRandomIndex(dishType)
+    } else if (dishType === "sides") {
+      return sides[getRandomIndex(sides)]
+    } else if (dishType === "mains") {
+      return mains[getRandomIndex(mains)]
+    } else if (dishType === "desserts") {
+      return desserts[getRandomIndex(desserts)]
+    }
   }
 
-
-    console.log("Hello")
-  }
 function displayRandom(){
   console.log("hi")
 }
+
 //find out which radio btn user clicked on
 //assign that to a variable (userSelection)
 // that selection will determine which random thing is generated

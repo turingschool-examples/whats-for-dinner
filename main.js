5627b64b1aa00da1639e990eee23057c5836ade7
Leah Young <leah.young21@alumni.colostate.edu>
2022-07-22 09:47:11 -0600

    included helper functions/ fixed css

---
+++ main.js
@@ -0,0 +1,8 @@
+// function hide(element) {
+//   element.classList.add('hidden');
+// }
+//
+// function show(element) {
+//   element.classList.remove('hidden');
+// }
+//  helper functions for hiding and showing things

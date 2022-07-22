b19945ae04ea467a2ab5a55fd60b0e83397f7c4c
Andrew Knapick <ajknapick@yahoo.com>
2022-07-22 13:30:24 -0500

    add arrays

---
+++ main.js
@@ -0,0 +1,3 @@
+var side = ["Esquites", "Elote", "Nopales", "Arroz Mexicano", "Frijoles fritos", "Calabaza rostizada", "Chiles en vinagre", "Guacamole", "Papas fritas","Consome"]
+var mainDish = ["Chiles en Nogada", "Tamales", "Chilaquiles", "Mole negro", "Tlayudas", "Tlacoyos", "Pozole", "Menudo", "Mollejas", "Enchiladas", "Torta ahogado", "Toastadas", "Sopes", "Barbacoa", "Tacos al Pastor"]
+var dessert = ["Gelatina", "Arroz con leche", "Paleta", "Dulce de lecha", "Flan", "Churros", "Concha", "Meringue", "Horchata", "Galletas"]

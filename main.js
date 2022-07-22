87fe894712f624674869a00886b16718662cfbce
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2022-07-22 17:50:49 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,54 @@
+var side = [
+    "asaparugus",
+    "baked beans",
+    "french fries",
+    "broccoli",
+    "bread roll",
+    "green beans",
+    "mac and cheese",
+    "mashed potatoes",
+    "salad",
+];
+
+var mainDish = [
+    "chicken",
+    "meatloaf",
+    "burger",
+    "chicken parmesan",
+    "lasagna",
+    "spaghetti",
+    "fajita",
+    "pizza",
+    "bbq ribs"
+];
+
+var dessert = [
+    "cheesecake",
+    "ice-cream",
+    "apple pie",
+    "chocolate cake",
+    "brownie",
+    "key-lime pie",
+    "cookie",
+    "banana pudding",
+    "boston creme pie",
+];
+
+//------------------------query selectors
+var letsCook = document.querySelector('button');
+
+
+
+
+
+
+
+//------------------------event listeners
+button.addEventListener('click', updateFeeling);
+
+
+
+
+
+
+  

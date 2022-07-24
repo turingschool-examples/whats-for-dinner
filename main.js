8b8c6a3c73e914968f1aa9cba787ca911f03ddb3
Andrew Knapick <ajknapick@yahoo.com>
2022-07-23 20:17:39 -0500

    add new div and button

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 var side = ["Esquites", "Elote", "Nopales", "Arroz Mexicano", "Frijoles fritos", "Calabaza rostizada", "Chiles en vinagre", "Guacamole", "Papas fritas", "Consome"]
 var mainDish = ["Chiles en Nogada", "Tamales", "Chilaquiles", "Mole negro", "Tlayudas", "Tlacoyos", "Pozole", "Menudo", "Mollejas", "Enchiladas", "Torta ahogado", "Toastadas", "Sopes", "Barbacoa", "Tacos al Pastor"]
-var dessert = ["Gelatina", "Arroz con leche", "Paleta", "Dulce de lecha", "Flan", "Churros", "Concha", "Meringue", "Horchata", "Galletas"]
+var dessert = ["Gelatina", "Arroz con leche", "Paleta", "Dulce de leche", "Flan", "Churros", "Concha", "Meringue", "Horchata", "Galletas"]
 
 var sides = side[Math.floor(Math.random() * side.length)]
 var mainDishes = mainDish[Math.floor(Math.random() * mainDish.length)]
 var desserts = dessert[Math.floor(Math.random() * dessert.length)]
 
-
-var letsCook = document.querySelector('.lets-cook') //lets cook button
 var cookPotImg = document.querySelector('.cook-pot') //cook pot image
 var clearButton = document.querySelector('.clear') //clear button
 var hiddenH3 = document.querySelector('.you-should') //you should make h3 header
 
+
 var randomFood = document.querySelector('.random-food')
+var letsCook = document.querySelector('.lets-cook') //lets cook button
 letsCook.addEventListener('click', showRandomFood)
 function showRandomFood() {
     var radioButtons = document.getElementsByName('selection')
@@ -28,6 +28,12 @@ function showRandomFood() {
     clearButton.classList.toggle('toggle')
     hiddenH3.classList.toggle('toggle')
     randomFood.classList.toggle('toggle')
+}
+
+var allRecipe = document.querySelector('.all-recipe-btn')
+allRecipe.addEventListener('click', showAllRecipe)
+function showAllRecipe() {
+    
 }
 
 //need to have radio input value recongized

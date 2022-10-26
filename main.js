d0799c8a030989bf84484db99aa234be4b08321c
Faye Rosenshein <rosefaye.a@gmail.com>
2022-10-26 08:39:17 -0600

    Start my work on HTML

---
+++ main.js
@@ -0,0 +1,9 @@
+// Intermediate CSS
+// Do some research and determine how to make your app respond to the user’s screen size.
+// Layout and spacing should adapt to mobile, tablet, desktop and extra large screens.
+// Add a loading animation when a user clicks the “Let’s Cook” button to simulate searching for a recipe. Hint - You will need to use CSS Keyframes, and a Javascript timeout function for this.
+// Make your buttons grow in size or change color when the user hovers over them, enticing them to click the dang thing.
+// When the recipe appears, the text should fade in.
+// Remember your user experience and animate these things in gradually. Timing is everything!
+
+

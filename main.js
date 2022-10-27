b30f2d950d169e501003606590634304e9524639
Matt Rowan <matt.rowan121@gmail.com>
2022-10-27 12:55:24 -0400

    feat: Add arrays for sides mains and dessert

---
+++ main.js
@@ -0,0 +1,47 @@
+var sides = [
+    'Caeser Salad',
+    'Coconut Rice',
+    'Coleslaw',
+    'Crispy Potatoes',
+    'Garden Salad',
+    'Garlic Butter Mushrooms',
+    'Hush Puppies',
+    'Miso Glazed Carrots',
+    'Shrimp Summer Rolls',
+    'Sweet Potato Tots'
+];
+var mains = [
+    'BBQ Chicken Burgers',
+    'Bibimbap',
+    'Butternut Squash Soup',
+    'Chicken Fried Rice',
+    'Chicken Parmesean',
+    'Empanadas',
+    'Margarita Pizza',
+    'Pineapple Chicken',
+    'Ramen',
+    'Shakshuka',
+    'Sheet Pan Fajitas',
+    'Spaghetti and Meatballs',
+    'Thai Yellow Curry'
+]
+var desserts = [
+    'Apple Pie',
+    'Baklava',
+    'Banana Bread',
+    'Black Forest Cake',
+    'Cheesecake',
+    'Chocolate Cupcakes',
+    'Croissants',
+    'Eclairs',
+    'Flan',
+    'Funfetti Cake',
+    'Key Lime Pie',
+    'Lemon Meringue Pie',
+    'Macarons',
+    'Macaroons',
+    'Pavlova',
+    'Peach Cobbler',
+    'Pumpkin Pie',
+    'Tart Tatin'
+]

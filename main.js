d450536c63f31c97ab9927f43bb9d503b8c8110b
Rick Vermeil <rickv85@gmail.com>
2022-10-27 15:24:17 -0600

    Complete iteration 1 MVP

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 // Query selectors here
+var cookButton = document.querySelector('#lets-cook');
+var sideRadio = document.querySelector('#side');
+var mainRadio = document.querySelector('#main-dish');
+var dessertRadio = document.querySelector('#dessert');
+var potIcon = document.querySelector('.cookpot-img');
+var prompt = document.querySelector('.prompt')
+var suggestion = document.querySelector('.suggestion')
 
 
 // Assets go here
@@ -53,6 +60,34 @@ var desserts = [
 ]
 
 // Event Listeners go here
+cookButton.addEventListener('click', selectFood)
+
 
 
 // Functions 
+
+function getRndFood(array) {
+    var arrayIndex = Math.floor(Math.random() * array.length);
+    return array[arrayIndex];
+  }
+
+  function displayCourse() {
+    potIcon.classList.add("hidden");
+    prompt.classList.remove("hidden");
+    suggestion.classList.remove("hidden")
+};
+
+
+function selectFood() {
+   var selectedFood;
+    if (sideRadio.checked == true) {
+        selectedFood = getRndFood(sides);
+} else if (mainRadio.checked == true) {
+        selectedFood = getRndFood(mains);
+} else if (dessertRadio.checked == true) {
+        selectedFood = getRndFood(desserts);
+}
+suggestion.innerText = `${selectedFood}!`;
+displayCourse()
+};
+

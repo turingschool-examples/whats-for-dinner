bd45cea19d549028a7b3a9225de1821413e0c532
Faye Rosenshein <rosefaye.a@gmail.com>
2022-10-27 17:31:18 -0600

    Finish styling as best I can for now on main page

--- main.js
+++ main.js
@@ -6,4 +6,73 @@
 // When the recipe appears, the text should fade in.
 // Remember your user experience and animate these things in gradually. Timing is everything!
 
+var sides = [
+    'Miso Glazed Carrots',
+    'Coleslaw',
+    'Garden Salad',
+    'Crispy Potatoes',
+    'Sweet Potato Tots',
+    'Coconut Rice',
+    'Caeser Salad',
+    'Shrimp Summer Rolls',
+    'Garlic Butter Mushrooms',
+    'Hush Puppies']
 
+var mains = [
+    'Spaghetti and Meatballs',
+    'Pineapple Chicken',
+    'Shakshuka',
+    'Thai Yellow Curry',
+    'Bibimbap',
+    'Chicken Parmesean',
+    'Butternut Squash Soup',
+    'BBQ Chicken Burgers',
+    'Ramen',
+    'Empanadas',
+    'Chicken Fried Rice',
+    'Sheet Pan Fajitas',
+    'Margarita Pizza']
+
+var desserts = [
+    'Apple Pie',
+    'Lemon Meringue Pie',
+    'Black Forest Cake',
+    'Banana Bread',
+    'Peach Cobbler',
+    'Cheesecake',
+    'Funfetti Cake',
+    'Baklava',
+    'Flan',
+    'Macarons',
+    'Macaroons',
+    'Chocolate Cupcakes',
+    'Pavlova',
+    'Pumpkin Pie',
+    'Key Lime Pie',
+    'Tart Tatin',
+    'Croissants',
+    'Eclairs']
+
+
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+}
+
+var sideRadioButton = document.querySelector('#side')
+var mainRadioButton = document.querySelector('#main-dish')
+var dessertRadioButton = document.querySelector('#dessert')
+var letsCookButton = document.querySelector('.lets-cook')
+var cookPotBox = document.querySelector('.box')
+
+letsCookButton.addEventListener('click', showRandomMealType)
+
+function showRandomMealType() {
+
+    var sideRadioButton = getRandomIndex(sides)
+    var mainRadioButton = getRandomIndex(mains)
+    var dessertRadioButton = getRandomIndex(desserts)
+
+    sides = sides[sideRadioButton],
+    mains = mains[mainRadioButton],
+    desserts = desserts[dessertRadioButton],
+}

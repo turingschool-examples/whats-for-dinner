0ab2dcacc4f2128932ab0049c23f15246b9112ec
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-10-28 08:43:14 -0400

    Complete CYOA functionality

--- main.js
+++ main.js
@@ -72,8 +72,7 @@ var dishToMake = document.querySelector('.suggestion');
 
 letsCookBtn.addEventListener('click', function(event) {
     event.preventDefault();
-    checkRadioValues();
-    displayDish();
+    displayDish(checkRadioValues());
 });
 
 //function
@@ -87,17 +86,21 @@ function checkRadioValues() {
     if(sideButton.checked) {
         dishToCook = randomizeSelecton(sidesArray);
         console.log("Side: ", dishToCook);
+        return dishToCook;
     } else if(mainButton.checked) {
         dishToCook = randomizeSelecton(mainsArray);
         console.log("Main: ", dishToCook);
+        return dishToCook;
     }else if(dessertButton.checked) {
         dishToCook = randomizeSelecton(dessertsArray);
         console.log("Dessert: ", dishToCook);
+        return dishToCook;
     }else if(entireMealButton.checked) {
         mealToCook.push(randomizeSelecton(sidesArray));
         mealToCook.push(randomizeSelecton(mainsArray));
         mealToCook.push(randomizeSelecton(dessertsArray));
         console.log("Meal: ", mealToCook);
+        return mealToCook;
     }
     //else statement for errors
 
@@ -106,25 +109,10 @@ function randomizeSelecton(array) {
     return getRandomArrayElement(array);
     }
 
-function displayDish() {
+function displayDish(selection) {
     dishToMake.innerText = null;
     potImage.classList.add('hidden');
     centered.classList.add('recipe-block');
     youShouldMakeContent.classList.remove('hidden');
-    dishToMake.innerText = `${dishToCook}`;
+    dishToMake.innerText = `${selection}`;
 }
-
-    //function that randomizes array index from selected array and display that element in the right hand box
-     //query select lets cook button in HTML
-    //query select radio buttons
-    //differernt function for each radio button?
-    //event listener for lets cook button
-    //read user selection
-    //insert HTML for innerText/innerHTML for array element selected to display it on page
-    //query select crockpot image to hide/reveal
-    //check radio button default functionality to override
-
-    
-    //prevent default on Let's Cook button (because it is on a form and will refresh the page)
-    //radio button default value for if it's checked. you dont need an event listener
-    //querySelect button. radioButtonSelection() If: radiobutton.checked === true

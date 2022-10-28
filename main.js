be0027847806ee16894c3be6a3dc39be5e631698
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-10-28 09:06:21 -0400

    Add sentence structure to entire meal display

--- main.js
+++ main.js
@@ -1,50 +1,50 @@
 //1 - Create Data model to hold all of the dishes in 3 array: side, main, dessert
 var sidesArray = [
-    'Miso Glazed Carrots', 
-    'Coleslaw', 
-    'Garden Salad', 
-    'Crispy Potatoes', 
-    'Sweet Potato Tots', 
-    'Coconut Rice', 
-    'Caeser Salad', 
-    'Shrimp Summer Rolls', 
-    'Garlic Butter Mushrooms', 
-    'Hush Puppies',
+    ' Miso Glazed Carrots', 
+    ' Coleslaw', 
+    ' Garden Salad', 
+    ' Crispy Potatoes', 
+    ' Sweet Potato Tots', 
+    ' Coconut Rice', 
+    ' Caeser Salad', 
+    ' Shrimp Summer Rolls', 
+    ' Garlic Butter Mushrooms', 
+    ' Hush Puppies',
 ];
 var mainsArray = [
-    'Spaghetti and Meatballs', 
-    'Pineapple Chicken', 
-    'Shakshuka', 
-    'Thai Yellow Curry', 
-    'Bibimbap', 
-    'Chicken Parmesean', 
-    'Butternut Squash Soup', 
-    'BBQ Chicken Burgers', 
-    'Ramen', 
-    'Empanadas', 
-    'Chicken Fried Rice', 
-    'Sheet Pan Fajitas', 
-    'Margarita Pizza', 
+    ' Spaghetti and Meatballs', 
+    ' Pineapple Chicken', 
+    ' Shakshuka', 
+    ' Thai Yellow Curry', 
+    ' Bibimbap', 
+    ' Chicken Parmesean', 
+    ' Butternut Squash Soup', 
+    ' BBQ Chicken Burgers', 
+    ' Ramen', 
+    ' Empanadas', 
+    ' Chicken Fried Rice', 
+    ' Sheet Pan Fajitas', 
+    ' Margarita Pizza', 
 ];
 var dessertsArray = [
-    'Apple Pie', 
-    'Lemon Meringue Pie', 
-    'Black Forest Cake', 
-    'Banana Bread', 
-    'Peach Cobbler', 
-    'Cheesecake', 
-    'Funfetti Cake', 
-    'Baklava', 
-    'Flan', 
-    'Macarons', 
-    'Macaroons', 
-    'Chocolate Cupcakes', 
-    'Pavlova', 
-    'Pumpkin Pie', 
-    'Key Lime Pie', 
-    'Tart Tatin', 
-    'Croissants', 
-    'Eclairs', 
+    ' Apple Pie', 
+    ' Lemon Meringue Pie', 
+    ' Black Forest Cake', 
+    ' Banana Bread', 
+    ' Peach Cobbler', 
+    ' Cheesecake', 
+    ' Funfetti Cake', 
+    ' Baklava', 
+    ' Flan', 
+    ' Macarons', 
+    ' Macaroons', 
+    ' Chocolate Cupcakes', 
+    ' Pavlova', 
+    ' Pumpkin Pie', 
+    ' Key Lime Pie', 
+    ' Tart Tatin', 
+    ' Croissants', 
+    ' Eclairs', 
 ];
 var dishToCook;
 var mealToCook = [];
@@ -110,9 +110,17 @@ function randomizeSelecton(array) {
     }
 
 function displayDish(selection) {
+    console.log(selection);
     dishToMake.innerText = null;
     potImage.classList.add('hidden');
     centered.classList.add('recipe-block');
     youShouldMakeContent.classList.remove('hidden');
-    dishToMake.innerText = `${selection}`;
+    if (selection.length === 3) {
+        dishToMake.innerText = `${selection[1]} with a side of ${selection[0]} and ${selection[2]} for dessert`;
+    } else {
+        dishToMake.innerText = `${selection}`;
+    }
 }
+
+
+//dishToMake.innerHTML = 

d1d4214723317d4276a6da8cb162fc1faaf08cab
El Brewster <brewsterel3@gmail.com>
2022-10-29 14:59:48 -0700

    Add JavaScript variables and format fix

---
+++ main.js
@@ -0,0 +1,16 @@
+//**;* ${Data Model} *;**//
+var sides = ["Miso Glazed Carrots", "Coleslaw", "Garden Salad", "Crispy Potatoes", "Sweet Potato Tots", "Coconut Rice", "Caesar Salad", "Shrimp Summer Rolls", "Garlic Butter Mushrooms", "Hush Puppies"];
+var mains = ["Spaghetti and Meatballs", "Pineapple Chicken", "Shakshuka", "Thai Yellow Curry", "Bibimbap", "Chicken Parmesean", "Butternut Squash Soup", "BBQ Chicken Burgers", "Ramen", "Empenadas", "Chicken Fried Rice", "Cheet Pan Fajitas", "Margarita Pizza"];
+var desserts = ["Apple pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler", "Cheesecake", "Funfetti Cake", "Baklava", "Flan", "Macarons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"];
+
+//**;* ${Query Selectors} *;**/
+// var addARecipeButton = document.querySelector(".name");
+// var clearButton = document.querySelector();
+var sideRadioButton = document.querySelector("#Side");
+var mainRadioButton = document.querySelector("#Main Dish");
+var dessertRadioButton = document.querySelector("#Dessert");
+var entireMealRadioButton = document.querySelector("#Entire Meal");
+var letsCookButton = document.querySelector("#cookButton");
+
+//**;* ${Event Listeners} *;**//
+Xbutton.addEventListener('click', Xfunction)

6d6c642d35853e01b6836811b20eb1a9efe78843
Michael Lobato <mike.lobato.wft@gmail.com>
2022-10-30 06:47:34 -0700

    enable name display on main page

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var loginButton = document.querySelector('.login-button')
 var loginPage = document.querySelector('.login-page')
 var mainPage = document.querySelector('.main-page')
+var userNameInput = document.querySelector('.user-name')
+var topOfPage = document.querySelector('.header')
 
 
 var mainPageHeader = document.querySelector('.header')
@@ -13,5 +15,9 @@ function goToMainPage() {
     mainPage.classList.remove('hidden')
     mainPageHeader.classList.remove('hidden')
     addRecipeButton.classList.remove('hidden')
-
+    
+    topOfPage.innerHTML += `
+    <h1 class="title">What dish you looking for, ${userNameInput.value}?</h1>`
 }
+
+

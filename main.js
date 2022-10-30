56aeae1ade98bb64e3b3af27404fb9794f2b9b80
El Brewster <brewsterel3@gmail.com>
2022-10-30 13:45:58 -0700

    Add radio button functionality and right card functionality

--- main.js
+++ main.js
@@ -3,77 +3,81 @@ var sides = ["Miso Glazed Carrots", "Coleslaw", "Garden Salad", "Crispy Potatoes
 var mains = ["Spaghetti and Meatballs", "Pineapple Chicken", "Shakshuka", "Thai Yellow Curry", "Bibimbap", "Chicken Parmesean", "Butternut Squash Soup", "BBQ Chicken Burgers", "Ramen", "Empenadas", "Chicken Fried Rice", "Cheet Pan Fajitas", "Margarita Pizza"];
 var desserts = ["Apple pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler", "Cheesecake", "Funfetti Cake", "Baklava", "Flan", "Macarons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"];
 
+//**;* ${Query Selectors[Methods Actually] & Event Listeners} *;**/
 //**;* ${Query Selectors} *;**/
-// var addARecipeButton = document.querySelector(".name");
-// var clearButton = document.querySelector();
-var sidesRadioButton = document.querySelector("#Side");
-var mainsRadioButton = document.querySelector("#Main");
-var dessertsRadioButton = document.querySelector("#Dessert");
-var entireMealRadioButton = document.querySelector("#Entire Meal");
-var letsCookButton = document.querySelector("#cookButton");
+var addARecipeButton = document.querySelector(".name");
+var clearButton = document.querySelector("#clear");
 
-//**;* ${Event Listeners} *;**//
-sidesRadioButton.addEventListener('click', getRandomSide());
-mainsRadioButton.addEventListener('click', getRandomMain());
-dessertsRadioButton.addEventListener('click', getRandomDessert());
-letsCookButton.addEventListener('click', youShouldMake());
+var sidesRadioButton = document.querySelector("#side");
+var mainsRadioButton = document.querySelector("#main");
+var dessertsRadioButton = document.querySelector("#dessert");
+var entireMealRadioButton = document.querySelector("#entire-meal");
+var letsCookButton = document.querySelector("#cookbutton");
 
-//**;* ${Functions} *;**//
-    //sides//
-function getRandomIndex(food) {
-    return Math.floor(Math.random() * food.length);
-}
-var randomSideNumber = getRandomIndex(sides);
-var randomSide = sides[randomSideNumber];
+var youShouldText = document.querySelector('#message');
 
-function getRandomSide() {
-    return randomSide;
-}
-    //mains//
-var randomMainNumber = getRandomIndex(mains);
-var randomMain = mains[randomMainNumber];
-    
-function getRandomMain() {
-    
-}
-    //desserts//
-var randomDessertNumber = getRandomIndex(desserts);
-var randomDessert = desserts[randomDessertNumber];
-    
-function getRandomDessert() {
-    
-}
-    // //entire meal//
-    // function getRandomIndex(sides) {
-    //     return Math.floor(Math.random() * sides.length);
-    // }
-    // var randomSideNumber = getRandomIndex(sides);
-    // var randomSide = sides[randomSideNumber];
-    
-    // function showRandomSide() {
-    
-    // }
-//**;* card flip *;**//
 var potIcon = document.querySelector('.front');
 var potContents = document.querySelector('.back');
 
+// **;* ${Event Listeners} *;**//
+// sidesRadioButton.addEventListener('click', getRandomSide);
+// mainsRadioButton.addEventListener('click', getRandomMain);
+// dessertsRadioButton.addEventListener('click', getRandomDessert);
+letsCookButton.addEventListener('click', function() {
+    var menuItemString = whichButton();
+    insertMealToDOM(menuItemString);
+    showMealCardRandomText();
+});
+
+// document.getElementById("menuItem").addEventListener("click", whichButton);
+document.getElementById("cookbutton").addEventListener("click", youShouldMake);
+//
+//grab radio button result when 'cookbutton' clicked (don't need to do anything, no eventListener)
+//gather the value with querySelect
+
+//pull random variable for chosen array-use randomIndex to get random food result 
+//push/update data model(kinda)
+//update DOM (.innerText)
+//display (toggle hidden)
+
+function getRandomIndex(food) {
+    return Math.floor(Math.random() * food.length);
+}
+
+function whichButton() {
+    event.preventDefault();
+   if (sidesRadioButton.checked === true) {
+        var sideIndexNum = getRandomIndex(sides);
+        var chosenRandomSide = sides[sideIndexNum];
+        console.log(chosenRandomSide);
+        return chosenRandomSide;
+    }
+   if (mainsRadioButton.checked === true) {
+        var mainIndexNum = getRandomIndex(mains);
+        var chosenRandomMain = mains[mainIndexNum];
+        console.log(chosenRandomMain);
+        return chosenRandomMain;
+    }
+    if (dessertsRadioButton.checked === true) {
+        var dessertIndexNum = getRandomIndex(desserts);
+        var chosenRandomDessert = desserts[dessertIndexNum];
+        console.log(chosenRandomDessert);
+        return chosenRandomDessert;
+    }
+}
+
+function insertMealToDOM(menuItemString){
+    youShouldText.innerText = menuItemString;
+}
+
+//**;* card flip *;**//
 function showCookPotIcon() {
-//add front
     potIcon.classList.remove('hidden');
-//hide back
     potContents.classList.add('hidden');
 }
-
 function showMealCardRandomText() {
-    potIcon.classList.add('.hidden');
-    potContents.classlist.remove('.hidden');
+    potIcon.classList.add('hidden');
+    potContents.classList.remove('hidden');
 }
-//add back
-//hide front
+//**;* right card meal details *;**//
 
-//**;* right card recipe details *;**//
-var youShouldText = document.querySelector('.message');
-function youShouldMake() {
-    showMealCardRandomText();
-    youShouldText.innerText = `You should make: ${randomSide}!`
-}

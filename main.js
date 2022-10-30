00b4aa1300b2031cd4554f690b9387c66f707be2
El Brewster <brewsterel3@gmail.com>
2022-10-30 08:05:02 -0700

    Add right card updates and some js

--- main.js
+++ main.js
@@ -6,19 +6,18 @@ var desserts = ["Apple pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana 
 //**;* ${Query Selectors} *;**/
 // var addARecipeButton = document.querySelector(".name");
 // var clearButton = document.querySelector();
-var sideRadioButton = document.querySelector("#Side");
-document.querySelector("div.")
-var mainRadioButton = document.querySelector("#Main Dish");
-var dessertRadioButton = document.querySelector("#Dessert");
+var sidesRadioButton = document.querySelector("#Side");
+var mainsRadioButton = document.querySelector("#Main Dish");
+var dessertsRadioButton = document.querySelector("#Dessert");
 var entireMealRadioButton = document.querySelector("#Entire Meal");
 var letsCookButton = document.querySelector("#cookButton");
 
 //**;* ${Event Listeners} *;**//
-sideRadioButton.addEventListener('click', randomSide);
-mainRadioButton.addEventListener('click', getRandomMains);
-dessertRadioButton.addEventListener('click', getRandomDesserts);
+sidesRadioButton.addEventListener('click', getRandomSide());
+mainsRadioButton.addEventListener('click', getRandomMain());
+dessertsRadioButton.addEventListener('click', getRandomDessert());
 
-letsCookButton.addEventListener('click', x);
+// letsCookButton.addEventListener('click', showRandomDishText());
 
 //**;* ${Functions} *;**//
     //sides//
@@ -28,5 +27,58 @@ function getRandomIndex(sides) {
 var randomSideNumber = getRandomIndex(sides);
 var randomSide = sides[randomSideNumber];
 
+function getRandomSide() {
 
+}
+    //mains//
+function getRandomIndex(mains) {
+        return Math.floor(Math.random() * mains.length);
+    }
+var randomMainNumber = getRandomIndex(mains);
+var randomMain = mains[randomMainNumber];
+    
+function getRandomMain() {
+    
+}
+    //desserts//
+function getRandomIndex(desserts) {
+        return Math.floor(Math.random() * desserts.length);
+    }
+var randomDessertNumber = getRandomIndex(desserts);
+var randomDessert = desserts[randomDessertNumber];
+    
+function getRandomDessert() {
+    
+}
+    // //entire meal//
+    // function getRandomIndex(sides) {
+    //     return Math.floor(Math.random() * sides.length);
+    // }
+    // var randomSideNumber = getRandomIndex(sides);
+    // var randomSide = sides[randomSideNumber];
+    
+    // function showRandomSide() {
+    
+    // }
+//**;* card flip *;**//
+var potIcon = document.querySelector('.front');
+var potContents = docuemnt.querySelector('.back');
 
+// function showCookPotIcon() {
+// //add front
+//     potIcon.classList.remove('hidden');
+// //hide back
+//     potContents.classList.add('hidden');
+// }
+
+// function showMealCardRandomText() {
+//     potIcon.classList.add('.hidden');
+//     potContents.classlist.remove('.hidden');
+// }
+//add back
+//hide front
+function youShouldMake() {
+    //var thing to be interpolalted
+    //if event happens
+    //show interpolated text `You should make: ${}!` or use .innerText?
+}

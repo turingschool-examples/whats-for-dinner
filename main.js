a2a90483fab3f7fdeb668896ed107a8300c2b70c
Sharie Trachsel <strachsel22@turing.edu>
2023-02-16 10:52:41 -0600

    Add suggestion for dessert

--- main.js
+++ main.js
@@ -1,24 +1,119 @@
 // *** Selectors ***
 // 1. Lets Cook Button
-var letsCookBtn = document.querySelector(".cook-button")
+var letsCookBtn = document.querySelector(".cook-button");
 // 2. Clear Button
-var clearBtn = document.querySelector(".clear-button")
+var clearBtn = document.querySelector(".clear-button");
 // 3. Crockpot Article
-var crockpotArt = document.querySelector(".js-crockpot")
+var crockpotArt = document.querySelector(".js-crockpot");
 // 4. You should make article
-var shouldMake = document.querySelector(".js-make")
+var shouldMake = document.querySelector(".js-make");
 // 5. Radio Group 
-var radioBtns = document.querySelectorAll('input[name ="dishType"]')
+var radioBtns = document.querySelectorAll('input[name ="dishType"]');
+// 6. Crockpot Article
+var crockpotDisplay = document.querySelector('.js-crockpot');
+// 7. You Should Make Article
+var makeDisplay = document.querySelector('.js-make');
+// 8. You should make selection
+var makeText = document.querySelector('h1');
 
 
 // *** Arrays ***
+var sides = [
+    'Miso Glazed Carrots',
+    'Coleslaw',
+    'Garden Salad',
+    'Crispy Potatoes',
+    'Sweet Potato Tots',
+    'Coconut Rice',
+    'Caeser Salad',
+    'Shrimp Summer Rolls',
+    'Garlic Butter Mushrooms',
+    'Hush Puppies'
+]
+
+var mainDishes = [
+    'Spaghetti and Meatballs',
+    'Pineapple Chicken',
+    'Shakshuka',
+    'Thai Yellow Curry',
+    'Bibimbap',
+    'Chicken Parmesean',
+    'Butternut Squash Soup',
+    'BBQ Chicken Burgers',
+    'Ramen',
+    'Empanadas',
+    'Chicken Fried Rice',
+    'Sheet Pan Fajitas',
+    'Margarita Pizza'
+]
+
+var desserts = [
+    'Apple Pie',
+    'Lemon Meringue Pie',
+    'Black Forest Cake',
+    'Banana Bread',
+    'Peach Cobbler',
+    'Cheesecake',
+    'Funfetti Cake',
+    'Baklava',
+    'Flan',
+    'Macarons',
+    'Macaroons',
+    'Chocolate Cupcakes',
+    'Pavlova',
+    'Pumpkin Pie',
+    'Key Lime Pie',
+    'Tart Tatin',
+    'Croissants',
+    'Eclairs'
+]
 
 // *** Event Listeners *** 
 // 1. Lets cook button click
+letsCookBtn.addEventListener('click', giveMakeSuggestion)
+
 // 2. Clear buttton
 
 
 // *** Functions ***
 
 //1. randomize index number param - array
-//2. 
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+//2. radio button selection
+function findMealType() {
+    var mealType;
+    for (var radioButton of radioBtns) {
+        if (radioButton.checked) {
+            mealType = radioButton.value
+        }
+    } console.log(mealType)
+    return mealType
+}
+
+// 3. Display Selection
+function displayShouldMake(){
+    crockpotDisplay.classList.add('hidden')
+    makeDisplay.classList.remove('hidden')
+}
+
+function displayCrockpot(){
+    crockpotDisplay.classList.remove('hidden')
+    makeDisplay.classList.add('hidden')
+} 
+
+// ** Populate you should make
+function giveMakeSuggestion() {
+    var selectedType = findMealType()
+    var randomSelection;
+
+    if (selectedType === 'dessert'){
+        var index = getRandomIndex(desserts)
+        randomSelection = desserts[index]
+    }
+    displayShouldMake()
+    makeText.innerText = `${randomSelection}`
+}
+    
+

12c60f516643cb4b2423982125cc8b449a8cdc27
John Featherstone <johnwfeatherstone.personal@gmail.com>
2023-02-16 12:28:52 -0700

    Add js file with arrays of meal options and reference new js file in HTML.

---
+++ mealData.js
@@ -0,0 +1,48 @@
+var sides = [
+    "Mac and Cheese",
+    "Collard Greens",
+    "Rice and Beans",
+    "Tostones",
+    "Maduros",
+    "Sauteed carrots",
+    "Miso-Glazed Brussel Sprouts",
+    "Mashed Potatoes",
+    "Potatoes Dauphinoise",
+    "Red Lentil Daal",
+    "Ratatouille",
+    "Kale Salad",
+    "Couscous",
+];
+
+var mains = [
+    "Duck Confit",
+    "Aloo Gobi",
+    "Lu Rou Fan",
+    "Beef Wellington",
+    "Roast Chicken",
+    "Tikka Masala",
+    "Lechon Asado",
+    "Steak and Ale Pie",
+    "Lentil Soup",
+    "Jollof Rice",
+    "Gumbo",
+    "Etouffee"
+]
+
+var desserts = [
+    "Apple Pie",
+    "Tres Leches Cake",
+    "Almond Cake",
+    "Tiramisu",
+    "Panna Cotta",
+    "Sticky Toffee Pudding",
+    "Poached Pears",
+    "Flan",
+    "Bread Pudding",
+    "Rum Cake",
+    "Baklava",
+    "Creme Brulee",
+    "Pasteis de Nata",
+    "Ice Cream",
+    "Tarte Tatin"
+]

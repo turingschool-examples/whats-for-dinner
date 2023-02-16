fdf5487891f971cb62f8e551870046a0178ca8e2
Sharie Trachsel <strachsel22@turing.edu>
2023-02-16 10:01:27 -0600

    Add selectors

---
+++ main.js
@@ -0,0 +1,24 @@
+// *** Selectors ***
+// 1. Lets Cook Button
+var letsCookBtn = document.querySelector(".cook-button")
+// 2. Clear Button
+var clearBtn = document.querySelector(".clear-button")
+// 3. Crockpot Article
+var crockpotArt = document.querySelector(".js-crockpot")
+// 4. You should make article
+var shouldMake = document.querySelector(".js-make")
+// 5. Radio Group 
+var radioBtns = document.querySelectorAll('input[name ="dishType"]')
+
+
+// *** Arrays ***
+
+// *** Event Listeners *** 
+// 1. Lets cook button click
+// 2. Clear buttton
+
+
+// *** Functions ***
+
+//1. randomize index number param - array
+//2. 

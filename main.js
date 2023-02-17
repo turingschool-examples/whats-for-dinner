983113ea4667ab17386a14be9c66d191b9d534bc
Houda Jawad <houdajawad@gmail.com>
2023-02-17 14:59:57 -0800

    Add querySelectors and eventListeners

---
+++ main.js
@@ -0,0 +1,17 @@
+// When a user selects a dish option 
+// and then clicks the “Let’s Cook!” button,
+//  the user sees a random dish from the list of possible dishes for that category.
+
+// add query selectors for inputs to pull in the html.
+// needs an event listener for button.
+var selectSide = document.querySelector("#side");
+var letsCookButn = document.querySelector(".lets-cook-button");
+
+letsCookButn.addEventListener("click", );
+selectSide.addEventListener("click", );
+
+
+
+
+
+

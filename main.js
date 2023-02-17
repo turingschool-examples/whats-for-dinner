2b57466b0296ae155bf97f6412ec699256bcaa78
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-02-16 21:01:19 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,61 @@
+// User selects a dish option, clicks button and random dish pulls up
+      //from the elected category
+// Dish appears on right side and cooking pot gets hidden
+
+// research radio input and how to make them work.
+// assign "lets cook" to a variable
+// create a variable for each dish category and hold their arrays
+// create and event listener for the button
+// when button is clicked...
+  // random number generator
+  // grab an index at random
+  // insert that index value into an html element
+  // hide the cooking pot emblem
+// WILL NEED //
+// function for hiding cooking pot
+// function for random number
+// 
+
+var sides = ["Miso Glazed Carrots",
+  "Coleslaw",
+  "Garden Salad",
+  "Crispy Potatoes",
+  "Sweet Potato Tots",
+  "Coconut Rice",
+  "Caeser Salad",
+  "Shrimp Summer Rolls",
+  "Garlic Butter Mushrooms",
+  "Hush Puppies"];
+
+var mains = ["Spaghetti and Meatballs",
+  "Pineapple Chicken",
+  "Shakshuka",
+  "Thai Yellow Curry",
+  "Bibimbap",
+  "Chicken Parmesean",
+  "Butternut Squash Soup",
+  "BBQ Chicken Burgers",
+  "Ramen",
+  "Empanadas",
+  "Chicken Fried Rice",
+  "Sheet Pan Fajitas",
+  "Margarita Pizza"];
+
+var desserts = ["Apple Pie",
+  "Lemon Meringue Pie",
+  "Black Forest Cake",
+  "Banana Bread",
+  "Peach Cobbler",
+  "Cheesecake",
+  "Funfetti Cake",
+  "Baklava",
+  "Flan",
+  "Macarons",
+  "Macaroons",
+  "Chocolate Cupcakes",
+  "Pavlova",
+  "Pumpkin Pie",
+  "Key Lime Pie",
+  "Tart Tatin",
+  "Croissants",
+  "Eclairs"];

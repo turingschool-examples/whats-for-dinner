f41ad5685c3a3a59c910b95d2c6c7160a8caebe7
Houda Jawad <houdajawad@gmail.com>
2023-02-17 18:22:19 -0800

    Add getRandomIndex function and update html text

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
-// When a user selects a dish option 
-// and then clicks the “Let’s Cook!” button,
-//  the user sees a random dish from the list of possible dishes for that category.
-
-// add query selectors for inputs to pull in the html.
-// needs an event listener for button.
+// query selector variables 
 var selectSide = document.querySelector("#side");
 var letsCookButton = document.querySelector(".lets-cook-button");
-var foodSelection = document.querySelector(".food-selection")
+var foodSelectionText = document.querySelector(".selection-text");
+
+// eventListeners
 letsCookButton.addEventListener("click", getDish );
+
 
 function getDish() {
 if (document.getElementById("side").checked === true) {
-    console.log(sides)
+  foodSelectionText.innerText = getRandomIndex(sides)
 } else if (document.getElementById("main-dish").checked === true) {
-    console.log(mains)
+  foodSelectionText.innerHTML = getRandomIndex(mains)
 } else if (document.getElementById("dessert").checked === true) {
-    console.log(desserts)
+  foodSelectionText.innerHTML = getRandomIndex(desserts)
 }
 }
        
+function getRandomIndex(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
 
 
-sides = ["Miso Glazed Carrots","Coleslaw","Garden Salad"]
-mains = ["Spaghetti and Meatballs","Pineapple Chicken","Shakshuka"]
-desserts = ["Apple Pie","Lemon Meringue Pie","Black Forest Cake"]
+// Arrays
+var sides = ["Miso Glazed Carrots","Coleslaw","Garden Salad"]
+var mains = ["Spaghetti and Meatballs","Pineapple Chicken","Shakshuka"]
+var desserts = ["Apple Pie","Lemon Meringue Pie","Black Forest Cake"]
    
   
 

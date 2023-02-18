38696e085b8e5ef56c4d543a10357390a6621782
Devynne Marshall <devynnem@gmail.com>
2023-02-18 15:26:10 -0700

    Finally print my food in the center of the box althoughwithout italics

--- main.js
+++ main.js
@@ -14,3 +14,37 @@ var dessert = ["Apple Pie", "Lemon Meringue Pie", "Black Forest Cake",
     "Baklava", "Flan", "Macarons", "Macaroons", "Chocolate Cupcakes",
     "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin",
     "Croissants", "Eclairs"]
+
+var seeRandomFood = document.querySelector(".food-suggestion")
+var cookPotImage = document.querySelector(".box2");
+var image = document.querySelector(".image");
+var foodSuggestion = document.querySelector(".food-suggestion");
+var currentFood;
+
+// function cookPot() {
+//     document.querySelector(".box2").classList.add("hidden");
+//     document.querySelector(".food-suggestion").classList.remove("hidden");
+
+// }
+
+function letsCook() {
+    var randomSide = getRandomIndex(side);
+    // var randomMainDish = getRandomIndex(mainDish);
+    // var randomDessert = getRandomIndex(dessert);
+    image.classList.add('hidden')
+    foodSuggestion.classList.remove('hidden')
+
+     if (document.getElementById("Side").checked) {
+        currentFood = side[randomSide]
+        seeRandomFood.innerText = `You should make: 
+        ${side[randomSide]}`
+    }
+
+}
+        
+
+
+
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+ };

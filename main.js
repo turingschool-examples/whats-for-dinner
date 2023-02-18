00a8724a17807bfaa5f9a47030563d6f5a192509
Houda Jawad <houdajawad@gmail.com>
2023-02-17 16:46:16 -0800

    Refactor the function to grab the right elements

--- main.js
+++ main.js
@@ -5,10 +5,25 @@
 // add query selectors for inputs to pull in the html.
 // needs an event listener for button.
 var selectSide = document.querySelector("#side");
-var letsCookButn = document.querySelector(".lets-cook-button");
+var letsCookButton = document.querySelector(".lets-cook-button");
+var foodSelection = document.querySelector(".food-selection")
+letsCookButton.addEventListener("click", getDish );
 
-letsCookButn.addEventListener("click", );
-selectSide.addEventListener("click", );
+function getDish() {
+if(document.getElementById("side").checked === true) {
+    console.log(sides)
+} else if(document.getElementById("main-dish").checked === true) {
+    console.log(mains)
+}
+
+}
+       
+
+
+sides = ["Miso Glazed Carrots","Coleslaw","Garden Salad"]
+mains = ["Spaghetti and Meatballs","Pineapple Chicken","Shakshuka"]
+
+
 
 
 

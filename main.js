21e17ec51dac50584b283983abb5ac5fab8b3890
Houda Jawad <houdajawad@gmail.com>
2023-02-18 08:09:15 -0800

    Add function and CSS to hide crockpot when user clicks on button

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
 // query selector variables 
 var selectSide = document.querySelector("#side");
 var letsCookButton = document.querySelector(".lets-cook-button");
-var foodSelectionText = document.querySelector(".selection-text");
+var foodOutput = document.querySelector(".food-selection")
+var foodOutputText = document.querySelector(".selection-text");
+var crockPotImg = document.querySelector(".crockpot-img")
 
 // eventListeners
 letsCookButton.addEventListener("click", getDish );
 
-
+// functions
 function getDish() {
-if (document.getElementById("side").checked === true) {
-  foodSelectionText.innerText = getRandomIndex(sides)
-} else if (document.getElementById("main-dish").checked === true) {
-  foodSelectionText.innerHTML = getRandomIndex(mains)
-} else if (document.getElementById("dessert").checked === true) {
-  foodSelectionText.innerHTML = getRandomIndex(desserts)
+   if (document.getElementById("side").checked === true) {
+  foodOutputText.innerText = getRandomIndex(sides)
+ } else if (document.getElementById("main-dish").checked === true) {
+  foodOutputText.innerHTML = getRandomIndex(mains)
+ } else if (document.getElementById("dessert").checked === true) {
+  foodOutputText.innerHTML = getRandomIndex(desserts)
 }
+hideCrockPot()
 }
        
 function getRandomIndex(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
+  return array[Math.floor(Math.random() * array.length)];
+}
 
+function hideCrockPot() {
+crockPotImg.style.visibility = "hidden"
+foodOutput.style.visibility = "visible"
+}
 
 // Arrays
 var sides = ["Miso Glazed Carrots","Coleslaw","Garden Salad"]

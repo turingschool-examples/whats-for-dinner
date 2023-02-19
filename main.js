96edce8f35d04124b17a86398287c14a70047094
John Featherstone <johnwfeatherstone.personal@gmail.com>
2023-02-19 10:31:42 -0700

    Add in button loading animation with time delay.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var letsCookButton = document.querySelector("#meal-choice-button");
 var outputBox = document.querySelector('#outputBox');
-var cookpotImg = document.querySelector('#cookpot')
+var cookpotImg = document.querySelector('#cookpot');
+var submitButton = document.querySelector('#meal-choice-button');
 
 
 
@@ -9,6 +10,10 @@ var cookpotImg = document.querySelector('#cookpot')
 letsCookButton.addEventListener("click", function(event) {
     event.preventDefault();
     getDish();
+    submitButton.classList.add('loading');
+    setTimeout(function() {
+        submitButton.classList.remove('loading');
+    }, 1000)
 }
 )
 
@@ -25,20 +30,20 @@ function getDish() {
     if (selectedMeal === "side") {
         cookpotImg.classList.add('hidden')
         outputMessage.innerHTML = `
-        <p id="output-lead"> You should make: </p>
-        <h1 id="output-meal"> ${sides[getRandomIndex(sides)]}!</h1>
+        <p id="output-lead" class="fade-in"> You should make: </p>
+        <h1 id="output-meal" class="fade-in"> ${sides[getRandomIndex(sides)]}!</h1>
         `
     } else if (selectedMeal === "main-dish") {
         cookpotImg.classList.add('hidden')
         outputMessage.innerHTML = `
-        <p id="output-lead"> You should make: </p>
-        <h1 id="output-meal"> ${mains[getRandomIndex(mains)]}!</h1>
+        <p id="output-lead" class="fade-in"> You should make: </p>
+        <h1 id="output-meal" class="fade-in"> ${mains[getRandomIndex(mains)]}!</h1>
         `
     } else if (selectedMeal === "dessert") {
         cookpotImg.classList.add('hidden')
         outputMessage.innerHTML = `
-        <p id="output-lead"> You should make: </p>
-        <h1 id="output-meal"> ${desserts[getRandomIndex(desserts)]}!</h1>
+        <p id="output-lead" class="fade-in"> You should make: </p>
+        <h1 id="output-meal" class="fade-in"> ${desserts[getRandomIndex(desserts)]}!</h1>
         `
     }
 }

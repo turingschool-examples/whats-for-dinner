bc427456ca6f0ea183c660759c1e960971aab12f
Denzel Turambi <dturambi26@gmail.com>
2023-02-19 15:04:31 -0700

    Revise showRandomIndex function wip

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
  // query selector vairables: 
 letsCookButton = document.querySelector('#lets-cook-button');
-sideArray = document.querySelector('#Side')
+choice = document.querySelector('input[name="dish_type"]:checked');
+foodResult = document.querySelector('#food-name');
+
+// sideArray = document.querySelector('#Side')
+
  // data/arrays:
 var side = ["Fries", "Rice & beans", "Nachos"];
 var mainDish = ["Cheeseburger", "Fried Chicken", "Burrito Bowl"];
 var dessert = ["Milkshake", "Chocolate Lava Cake", "Cinnamon Rolls"];
 
  // event listeners:
- letsCookButton.addEventListener(`click`, );
+ letsCookButton.addEventListener(`click`, showRandomIndex);
 
  // functions:
 var selectedArray;
 
- function getRandomIndex(array) {
-    return Math.floor(Math.random() * array.length);
-  }
+//  function getRandomIndex(selectedArray) {
+//     return Math.floor(Math.random() * selectedArray.length);
+//   }
+function showRandomIndex() {
+    if(choice === "Side") {
+        selectedArray = side;
+    } else if (choice === "Main Dish") {
+        selectedArray = mainDish;
+    } else if (choice === "Dessert") {
+        selectedArray = dessert;
+    }
+    var randomIndex = Math.floor(Math.random() * selectedArray.length);
+    var randomElement = selectedArray[randomIndex];
+    foodResult.innerHTML = randomElement;
 
+    alert(`hello`)
+}
   

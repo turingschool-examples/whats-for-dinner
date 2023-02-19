dadfcc04486feb2f3683bf613480a757a413ee60
Denzel Turambi <dturambi26@gmail.com>
2023-02-19 13:53:55 -0700

    Add data arrays

---
+++ main.js
@@ -0,0 +1,19 @@
+ // query selector vairables: 
+letsCookButton = document.querySelector('#lets-cook-button');
+sideArray = document.querySelector('#Side')
+ // data/arrays:
+var side = ["Fries", "Rice & beans", "Nachos"];
+var mainDish = ["Cheeseburger", "Fried Chicken", "Burrito Bowl"];
+var dessert = ["Milkshake", "Chocolate Lava Cake", "Cinnamon Rolls"];
+
+ // event listeners:
+ letsCookButton.addEventListener(`click`, );
+
+ // functions:
+var selectedArray;
+
+ function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
+  

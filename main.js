30eda82bb049cd707a2f57049d0e9db74ab6874b
Rachel Brendel <brendelrachel@gmail.com>
2023-02-19 16:53:38 -0600

    Add food arrays

---
+++ main.js
@@ -0,0 +1,51 @@
+var sides = [
+  'Garden Salad',
+  'Caeser Salad',
+  'Hush Puppies',
+  'Garlic Mushrooms',
+  'Mashed Potatoes and Gravy',
+  'Greenbeans',
+  'Coleslaw',
+  'Homestyle Fried Potatoes',
+  'Chili Cheese Tots',
+  'Rice',
+  'Summer Rolls',
+  'Baked Creamcheese Peppers',
+  'Herb Blend Stuffing'
+]
+
+var mains = [
+  'BBQ Bacon Burgers',
+  'Chicken Fried Rice',
+  'Homemade Ramen',
+  'Eggrolls',
+  'Shepards Pie',
+  'Bibimbap',
+  'Chicken Parmesean',
+  'Baked Potatoe Soup',
+  'Empanadas',
+  'Lemon Pepper Chicken',
+  'Fajitas',
+  'Spaghetti and Meatballs',
+  'BYO Pizza'
+];
+
+var desserts = [
+  'Macaroons',
+  'Lemon Meringue Pie',
+  'Pumpkin Pie',
+  'Funfetti Cake',
+  'Peach Cobbler',
+  'Cheesecake',
+  'Baklava',
+  'Flan',
+  'Macarons',
+  'Pinapple Upside Down Cake',
+  'Apple Pie',
+  'Chocolate Cupcakes',
+  'Pavlova',
+  'Banana Bread',
+  'Key Lime Pie',
+  'Orange Dream Bunt Cakes',
+  'Eclairs'
+];

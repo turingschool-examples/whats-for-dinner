31461e208e62c5cd8e517629578a4f906bfeae95
Caitlin Cradick <caitlincradick@gmail.com>
2023-02-19 14:15:49 -0800

    This commit will add the event listeners I tried to add that are not working yet

--- main.js
+++ main.js
@@ -5,19 +5,20 @@ var selectDessert = document.querySelector("#dessert")
 var letsCookButton = document.querySelector(".lets-cook-button")
 var shouldMake = document.querySelector(".should-make")
 var cookpotImg = document.querySelector("img")
+var dontLikeButton = document.querySelector(".dont-like")
 
 //event listeners
-letsCookButton.addEventListener('click', hidePot)
+letsCookButton.addEventListener('click', hidePot, dontLikeAdd)
 letsCookButton.addEventListener('click', showMeal) 
+// window.addEventListener("load", dontLikeRemove)
+// dontLikeButton.addEventListener("click", dontLikeRemove)
+// dontLikeButton.addEventListener("click", deleteAlert)
 
   
 //arrays
 var sides = [ "Miso Glazed Carrots","Coleslaw", "Garden Salad","Crispy Potatoes","Sweet Potato Tots", "Coconut Rice", "Caeser Salad","Summer Rolls", "Garlic Butter Mushrooms", "Hush Puppies"];
 var mains = ["Spaghetti","Pineapple Curry","Shakshuka",  "Thai Yellow Curry","Bibimbap","Vegan Chicken Parmesean", "Butternut Squash Soup","BBQ Veggie Burgers","Ramen","Empanadas","Fried Rice","Sheet Pan Fajitas","Margarita Pizza"]
-var desserts = ["Apple Pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread","Peach Cobbler","Cheesecake",
-"Funfetti Cake","Baklava","Flan","Macarons","Macaroons",
-"Chocolate Cupcakes","Pavlova","Pumpkin Pie","Key Lime Pie",
-"Tart Tatin", "Croissants","Eclairs"]
+var desserts = ["Apple Pie", "Lemon Meringue Pie"]
 
 // VARIABLES
 //these will pull random values from my arrays 
@@ -31,7 +32,18 @@ var dessertDish = getRandomDish(desserts);
 function hidePot () {
     shouldMake.classList.remove('hidden')
     cookpotImg.classList.add('hidden')
+    
 }
+// shows button:
+
+function dontLikeAdd () {
+    dontLikeButton.classList.remove('hidden')
+}
+
+// function dontLikeAdd () {
+//     dontLikeButton.classList.add('hidden')
+//     dontLikeButton.classList.remove('hidden')
+// }
 
 //this will pull random item from array based on index variable for radio buttons 
 function getRandomDish(dishArray) {
@@ -55,4 +67,14 @@ ${dessertDish}`
 }
 }
 
+//deleting recipe 
+function deleteRecipe (event) {
 
+
+
+   
+}
+
+function deleteAlert (){
+    window.alert ("This recipe has been deleted.")
+}

d323cb53d87363e0f9a455e53127ef8b7b1e9af5
Denzel Turambi <dturambi26@gmail.com>
2023-02-19 17:40:20 -0700

    Refactor js file to display dish name wip

--- main.js
+++ main.js
@@ -1,36 +1,73 @@
- // query selector vairables: 
-letsCookButton = document.querySelector('#lets-cook-button');
-choice = document.querySelector('input[name="dish_type"]:checked');
-foodResult = document.querySelector('#food-name');
-
-// sideArray = document.querySelector('#Side')
+//  // query selector vairables: 
+var sideRadio = document.querySelector('#Side');
+var mainRadio = document.querySelector('#Main Dish');
+var dessertRadio = document.querySelector('#Dessert');
+var foodName = document.querySelector('#food-name');
+var cookpotImage = document.querySelector('.cookpot-image');
 
  // data/arrays:
-var side = ["Fries", "Rice & beans", "Nachos"];
-var mainDish = ["Cheeseburger", "Fried Chicken", "Burrito Bowl"];
-var dessert = ["Milkshake", "Chocolate Lava Cake", "Cinnamon Rolls"];
+var sideDishes = ["Garlic Bread", "Fries", "Rice & beans", "Nachos"];
+var mainDishes = ["Spaghetti Bolognese", "Cheeseburger", "Fried Chicken", "Burrito Bowl"];
+var dessertDishes = ["Chocolate Lava Cake", "Milkshake", "Ice Cream Sundae", "Cinnamon Rolls"];
 
  // event listeners:
- letsCookButton.addEventListener(`click`, showRandomIndex);
+var letsCookButton = document.querySelector('#lets-cook-button');
+letsCookButton.addEventListener('click', displayRandomDish);
 
  // functions:
-var selectedArray;
+function selectRandomDish(dishes) {
+    return dishes[Math.floor(Math.random() * dishes.length)];
+  }
+  
+  // Define function to display a random dish for the currently selected category
+  function displayRandomDish() {
+    // Determine the currently selected category
+    var selectedCategory = '';
+    if (sideRadio.checked) {
+      selectedCategory = 'Side';
+    } else if (mainRadio.checked) {
+      selectedCategory = 'Main Dish';
+    } else if (dessertRadio.checked) {
+      selectedCategory = 'Dessert';
+    }
+    
+    // Select a random dish from the corresponding list
+    var randomDish = '';
+    if (selectedCategory === 'Side') {
+      randomDish = selectRandomDish(sideDishes);
+    } else if (selectedCategory === 'Main Dish') {
+      randomDish = selectRandomDish(mainDishes);
+    } else if (selectedCategory === 'Dessert') {
+      randomDish = selectRandomDish(dessertDishes);
+    }
+    
+    // Display the random dish and hide the cookpot image
+    foodName.textContent = randomDish;
+    cookpotImage.style.display = 'none';
+  }
+  
+  // Attach event listener to "LET'S COOK!" button
+  var letsCookButton = document.querySelector('#lets-cook-button');
+  letsCookButton.addEventListener('click', displayRandomDish);
+// var selectedArray;
 
 //  function getRandomIndex(selectedArray) {
 //     return Math.floor(Math.random() * selectedArray.length);
 //   }
-function showRandomIndex() {
-    if(choice === "Side") {
-        selectedArray = side;
-    } else if (choice === "Main Dish") {
-        selectedArray = mainDish;
-    } else if (choice === "Dessert") {
-        selectedArray = dessert;
-    }
-    var randomIndex = Math.floor(Math.random() * selectedArray.length);
-    var randomElement = selectedArray[randomIndex];
-    foodResult.innerHTML = randomElement;
 
-    alert(`hello`)
-}
+// function showRandomIndex() {
+//     alert(`hi`);
+//     if(document.getElementById('Side').checked) {
+//         selectedArray = side;
+//     } else if (document.getElementById('Main Dish').checked) {
+//         selectedArray = mainDish;
+//     } else if (document.getElementById('Dessert').checked) {
+//         selectedArray = dessert;
+//     }
+//     var randomIndex = getRandomIndex();
+//     var randomElement = selectedArray[randomIndex];
+//     return foodResult.innerHTML += `You should make: ${randomElement}`;
+// }
   
+
+

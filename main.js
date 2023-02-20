e585e680fda249ba1961befaba894a0f482a162a
Rachel Brendel <brendelrachel@gmail.com>
2023-02-19 18:09:05 -0600

    creates functions to hide opt and show food text

--- main.js
+++ main.js
@@ -49,3 +49,41 @@ var desserts = [
   'Orange Dream Bunt Cakes',
   'Eclairs'
 ];
+
+//Variables
+
+var letsCookBtn = document.querySelector('.letsCookBtn');
+var sideInput = document.getElementById('side');
+var mainDishInput = document.getElementById('mainDish');
+var dessertInput = document.getElementById('dessert');
+var food = document.getElementById('foodtxt');
+var pot = document.querySelector('cookPot')
+
+//Event Listeners
+
+letsCookBtn.addEventListener('click', showRandomDish);
+
+//Functions
+
+function selectRandomFoodIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};
+
+function showRandomDish() {
+  showFood();
+  if (sideInput.checked == true) {
+    food.innerText = sides[selectRandomFoodIndex(sides)];
+  } else if (mainDishInput.checked == true) {
+    food.innerText = mains[selectRandomFoodIndex(mains)];
+  } else {
+    food.innerText = desserts[selectRandomFoodIndex(desserts)];
+  }
+};
+
+function hideCookPot() {
+  cookPot.classList.add('hidden')
+};
+
+function showFood() {
+  foodtxt.classList.remove('hidden')
+};

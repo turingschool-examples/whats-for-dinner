90bfde5e5116f7fce8b878fbe9979e6eb31a2e21
Elise Jones <02elise.jones02@gmail.com>
2023-02-20 23:27:25 -0700

    Added screenshot of website

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var sides = ['Miso Glazed Carrots',
+var sides = [
+    'Miso Glazed Carrots',
     'Coleslaw',
     'Garden Salad',
     'Crispy Potatoes',
@@ -7,8 +8,11 @@ var sides = ['Miso Glazed Carrots',
     'Caeser Salad',
     'Shrimp Summer Rolls',
     'Garlic Butter Mushrooms',
-    'Hush Puppies']
-var mains = ['Spaghetti and Meatballs',
+    'Hush Puppies'
+]
+
+var mains = [
+    'Spaghetti and Meatballs',
     'Pineapple Chicken',
     'Shakshuka',
     'Thai Yellow Curry',
@@ -20,56 +24,61 @@ var mains = ['Spaghetti and Meatballs',
     'Empanadas',
     'Chicken Fried Rice',
     'Sheet Pan Fajitas',
-    'Margarita Pizza']
-
-var desserts = [
-'Apple Pie',
-'Lemon Meringue Pie',
-'Black Forest Cake',
-'Banana Bread',
-'Peach Cobbler',
-'Cheesecake',
-'Funfetti Cake',
-'Baklava',
-'Flan',
-'Macarons',
-'Macaroons',
-'Chocolate Cupcakes',
-'Pavlova',
-'Pumpkin Pie',
-'Key Lime Pie',
-'Tart Tatin',
-'Croissants',
-'Eclairs',
+    'Margarita Pizza'
 ]
 
-var imageBox = document.querySelector('.image-box')
-var toMake = document.querySelector('#dish-recommendation-box')
-var letsCookButton = document.querySelector('.letscook-button')
-var side = document.querySelector('#side')
-var makeRecommendation = document.querySelector('.to-make-rec')
-letsCookButton.addEventListener('click', showDish)
+var desserts = [
+    'Apple Pie',
+    'Lemon Meringue Pie',
+    'Black Forest Cake',
+    'Banana Bread',
+    'Peach Cobbler',
+    'Cheesecake',
+    'Funfetti Cake',
+    'Baklava',
+    'Flan',
+    'Macarons',
+    'Macaroons',
+    'Chocolate Cupcakes',
+    'Pavlova',
+    'Pumpkin Pie',
+    'Key Lime Pie',
+    'Tart Tatin',
+    'Croissants',
+    'Eclairs',
+]
+
+var imageBox = document.querySelector('.image-box');
+var toMake = document.querySelector('#dish-recommendation-box');
+var letsCookButton = document.querySelector('.letscook-button');
+var side = document.querySelector('#side');
+var makeRecommendation = document.querySelector('.to-make-rec');
+
+letsCookButton.addEventListener('click', showDish);
 
 function hide(element) {
-    element.classList.add("hidden")
+    element.classList.add("hidden");
 }
 
 function show(element) {
-    element.classList.remove('hidden')
+    element.classList.remove('hidden');
 }
 
-function showDish(){
-    hide(imageBox)
-    show(toMake)   
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+}
+
+function showDish() {
+    hide(imageBox);
+    show(toMake);   
     if(document.getElementById('side').checked === true) {
         makeRecommendation.innerText = sides[getRandomIndex(sides)] + `!`
     } else if (document.getElementById('main-dish').checked === true) {
         makeRecommendation.innerText = mains[getRandomIndex(mains)] + `!`
     } else if (document.getElementById('dessert').checked === true){ 
         makeRecommendation.innerText = desserts[getRandomIndex(desserts)] + `!`
+    } else if (document.getElementById('entire-meal').checked){
+        makeRecommendation.innerText = mains[getRandomIndex(mains)] + ' with a side of ' + sides[getRandomIndex(sides)] + ' and ' + desserts[getRandomIndex(desserts)] + ' for dessert!'
     }
 }
 
-function getRandomIndex(array) {
-    return Math.floor(Math.random() * array.length);
-}

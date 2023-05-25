4df329c5bf11a44e97afa09490f5445566824e5b
Josh Bennett <joshbennett793@yahoo.com>
2023-05-24 23:28:30 -0500

    Return a random indexed element from array

--- main.js
+++ main.js
@@ -2,3 +2,9 @@
 import { sides } from './data/sides.js';
 import { mains } from './data/mains.js';
 import { desserts } from './data/desserts.js';
+
+// FUNCTIONS AND HANDLERS
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}

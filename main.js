e3dbe3a9daa27976da0be88c1a069add048fdd4a
Josh Bennett <joshbennett793@yahoo.com>
2023-05-25 15:54:19 -0500

    Return meal based on which checkboxes are checked

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ var letsCookBtn = document.querySelector('#form-submit-button');
 // CHECKBOXES
 var entireMealCB = document.querySelector('input[name=meal]');
 var sideMainDessertCheckBoxes = document.querySelectorAll('.cb');
+var [sideCB, mainCB, dessertCB] = [...sideMainDessertCheckBoxes];
 
 // EVENT LISTENERS
 
@@ -38,12 +39,12 @@ for (var i = 0; i < sideMainDessertCheckBoxes.length; i++) {
   sideMainDessertCheckBoxes[i].addEventListener('change', () => {
     var allCBAreChecked = checkIfAllCBChecked();
     if (entireMealCB.checked) {
-      entireMealCB.checked = !entireMealCB.checked
+      entireMealCB.checked = !entireMealCB.checked;
     }
     if (allCBAreChecked) {
       toggleCheckBoxes();
       entireMealCB.checked = true;
-    } 
+    }
   });
 }
 
@@ -94,4 +95,30 @@ function toggleCheckBoxes() {
 
 function toggleClass(element, className) {
   element.classList.toggle(className);
+}
+
+function returnRequestedMeal(suggestion) {
+  var side = sideCB.checked && !mainCB.checked && !dessertCB.checked;
+  var main = !sideCB.checked && mainCB.checked && !dessertCB.checked;
+  var dessert = !sideCB.checked && !mainCB.checked && dessertCB.checked;
+  var sideAndMain = sideCB.checked && mainCB.checked && !dessertCB.checked;
+  var mainAndDessert = !sideCB.checked && mainCB.checked && dessertCB.checked;
+  var sideAndDessert = sideCB.checked && !mainCB.checked && dessertCB.checked;
+  var entireMeal = entireMealCB.checked;
+
+  if (side) {
+    return `${suggestion.side}!`;
+  } else if (main) {
+    return `${suggestion.main}!`;
+  } else if (dessert) {
+    return `${suggestion.dessert}!`;
+  } else if (sideAndMain) {
+    return `${suggestion.main} with a side of ${suggestion.side}!`;
+  } else if (mainAndDessert) {
+    return `${suggestion.main} and ${suggestion.dessert} for dessert!`;
+  } else if (sideAndDessert) {
+    return `${suggestion.side} as a side and ${suggestion.dessert} for dessert!`;
+  } else if (entireMeal) {
+    return `${suggestion.main} with a side of ${suggestion.side} and ${suggestion.dessert} for dessert!`;
+  }
 }

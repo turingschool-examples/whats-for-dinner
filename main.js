952a7307526d90f5415fb41ae14ebc5b76bf7db1
Renee Pinna <renee.pinna@gmail.com>
2023-05-25 11:24:06 -0600

    Add event listener for the lets cook button

---
+++ main.js
@@ -0,0 +1,21 @@
+// Query Selectors
+
+var letsCookButton = document.querySelector(".lets-cook-btn");
+
+// Variables
+
+
+
+
+// Event Listeners
+
+letsCookButton.addEventListener('click', function(){
+    getRandomRecipe();
+    displayRecipe();
+})
+
+// Functions and Event Handlers
+
+function getRandomRecipe() {
+
+}

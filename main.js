3f0b7fc19de20cb2071881d61f43bffbaa6b934e
Devin Altobello <dl.altobello@gmail.com>
2023-05-26 21:18:19 -0400

    Refactor: update getRandomDish function

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 // When a user selects a dish option and then clicks the “Let’s Cook!” button, the user sees a random dish from the list of possible dishes for that category
 
 // querySelectors:
-var sideRadioButton = document.querySelector("#side")
+var sideRadioButton = document.querySelector("#side-radio")
 var mainRadioButton = document.querySelector("#main")
 var dessertRadioButton = document.querySelector("#dessert")
 var entireMealRadioButton = document.querySelector("#entire-meal")
+var boilingPot = document.querySelector(".boiling-pot")
 
 var letsCookButton = document.querySelector(".lets-cook")
+var addRecipeButton = document.querySelector(".add-recipe-button")
 
 
 
@@ -61,19 +63,56 @@ var desserts = [
     "Eclairs"
 ]
 
+var recipeOptions = []
+
 
 // eventListeners:
-letsCookButton.addEventListener("click", dessertsRandom)
+// sideRadioButton.addEventListener("click", getRandomDish)
+// mainRadioButton.addEventListener("click", getRandomDish)
+// dessertRadioButton.addEventListener("click", getRandomDish)
+// entireMealRadioButton.addEventListener("click", getRandomDish)
+letsCookButton.addEventListener("click", getRandomDish)
+
+
 
 // functions:
-function getRandomDish(array) {
-var randomSide = sides[getRandomIndex(sides)]
-var randomMain = main[getRandomIndex(mains)]
-var randomDessert = desserts[getRandomIndex(desserts)]
-var randomMeal = [randomSide, randomMain, randomDessert]
-}
-
-
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
-  }
+}
+
+function getRandomDish() {
+    boilingPot.classList.add("hidden")
+
+    if (sideRadioButton.checked) {
+        recipeOptions.innerText = sides[getRandomIndex(sides)]
+    } else if (mainRadioButton.checked) {
+        recipeOptions.innerText = mains[getRandomIndex(mains)]
+    } else if (dessertRadioButton.checked) {
+        recipeOptions.innerText = desserts[getRandomIndex(desserts)] 
+    }     
+}
+
+// function sides() {
+//     if (sideRadioButton.checked) {
+//         console.log("hello")
+//     }
+// }
+// check()
+
+// getRandomSide etc. with conditional checking if the button was checked. if it was, put in random side to innerHTML. 
+
+// var randomMeal = [randomSide, randomMain, randomDessert]
+
+
+
+// var randomSide = sides[getRandomIndex(sides)]
+// var randomMain = mains[getRandomIndex(mains)]
+// var randomDessert = desserts[getRandomIndex(desserts)]
+// if (sideRadioButton.checked) {
+//     sides.innerHTML = randomSide
+// } else if (mainRadioButton.checked) {
+//     mains.innerHTML = randomMain
+// } else if (desserts.innerHTML = randomDessert) {
+
+// }
+// }

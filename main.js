b8dbba065ba2106238d80f5886ed00a1daa68f33
Avery Berryman <averyberryman@gmail.com>
2023-05-28 11:10:33 -0400

    Add buttons/dishes and model the DOM

---
+++ main.js
@@ -0,0 +1,15 @@
+// Query Selectors:
+
+var sideRadio = document.querySelector('#side');
+var mainDishRadio = document.querySelector('#main-dish');
+var dessertRadio = document.querySelector('#dessert');
+var wholeMealRadio = document.querySelector('#whole-meal');
+var letsCookButton = document.querySelector('#lets-cook-button');
+var dishName = document.querySelector('#dish-name');
+var potImage = document.querySelector('#pot-image');
+
+// Dishes:
+var sideDishes = ['Sweet Potatoes', 'Caesar salad', 'Garlic Bread', 'Baked potato'];
+var mainDishes = ['Spaghetti', 'Chicken Alfredo', 'Chicken Parmesan'];
+var dessertDishes = ['Tiramisu', 'Gelato', 'Shortbread'];
+

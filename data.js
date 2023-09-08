508c08dedf0ce253f8f30425b96bd3a52bb62b8e
Deanna Stevens <deanna.sofias@gmail.com>
2023-09-08 13:41:01 -0400

    Update formatted arrays

--- data.js
+++ data.js
@@ -1,46 +1,57 @@
-Sides
-Miso Glazed Carrots
-Coleslaw
-Garden Salad
-Crispy Potatoes
-Sweet Potato Tots
-Coconut Rice
-Caeser Salad
-Shrimp Summer Rolls
-Garlic Butter Mushrooms
-Hush Puppies
-
-Mains
-Spaghetti and Meatballs
-Pineapple Chicken
-Shakshuka
-Thai Yellow Curry
-Bibimbap
-Chicken Parmesean
-Butternut Squash Soup
-BBQ Chicken Burgers
-Ramen
-Empanadas
-Chicken Fried Rice
-Sheet Pan Fajitas
-Margarita Pizza
-
-Desserts
-Apple Pie
-Lemon Meringue Pie
-Black Forest Cake
-Banana Bread
-Peach Cobbler
-Cheesecake
-Funfetti Cake
-Baklava
-Flan
-Macarons
-Macaroons
-Chocolate Cupcakes
-Pavlova
-Pumpkin Pie
-Key Lime Pie
-Tart Tatin
-Croissants
-Eclairs
+var sides = [
+    "Miso Glazed Carrots",
+    "Coleslaw",
+    "Garden Salad",
+    "Crispy Potatoes",
+    "Sweet Potato Tots",
+    "Coconut Rice",
+    "Caesar Salad",
+    "Shrimp Summer Rolls",
+    "Garlic Butter Mushrooms",
+    "Hush Puppies",
+    "A nice little salad",
+    "Seaweed Salad",
+    "Manchego Stuffed Dates",
+    "Dolmas"
+  ];
+  
+  var mains = [
+    "Spaghetti and Meatballs",
+    "Pineapple Chicken",
+    "Shakshuka",
+    "Thai Yellow Curry",
+    "Bibimbap",
+    "Chicken Parmesan",
+    "Butternut Squash Soup",
+    "BBQ Chicken Burgers",
+    "Ramen",
+    "Empanadas",
+    "Chicken Fried Rice",
+    "Sheet Pan Fajitas",
+    "Margarita Pizza",
+    "Pumpkin and Shitake Soup",
+    "Jardineira Stew",
+    "Grilled Seabream"
+  ];
+  
+  var desserts = [
+    "Apple Pie",
+    "Lemon Meringue Pie",
+    "Black Forest Cake",
+    "Banana Bread",
+    "Peach Cobbler",
+    "Cheesecake",
+    "Funfetti Cake",
+    "Baklava",
+    "Flan",
+    "Macaroons",
+    "Chocolate Cupcakes",
+    "Pavlova",
+    "Pumpkin Pie",
+    "Key Lime Pie",
+    "Tart Tatin",
+    "Croissants",
+    "Eclairs",
+    "Pecan Pie",
+    "Pistachio Ice Cream"
+  ];

5723fd51c7b86a5b5158c8833c327df57bb0c21d
Deanna Stevens <deanna.sofias@gmail.com>
2023-09-09 11:23:32 -0400

    Add javascript pseudocode

---
+++ main.js
@@ -0,0 +1,40 @@
+// query selectors
+var 
+
+// event listeners 
+
+/*pseudocode map
+    > add fav button
+    > choose dish button
+    > event.target for the cook pot clear button
+
+var current recipe
+
+function to display current one in the dom
+function to favorite the current one in the DM
+
+connect radio button selection to the let's cook button
+> query selectorall for the radio button to transfer the value, treat the values as an array, actually a nodelist, can convert easily
+
+function attached to event listener for the button to toggle the hidden class for the selected dish
+ 
+favorite function:
+id that matches the one on the DOM
+and have a new array to push into for faves
+
+favorite radio button
+will rerender based on T/F of fave get the id property from DM to render on DOM 
+
+OR
+
+have a function working on the current recipe
+the current recipe will have a property of favorited
+automatically update the DOM bc DM is updated 
+
+
+
+
+
+
+
+*/

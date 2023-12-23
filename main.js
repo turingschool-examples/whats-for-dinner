61267214b00fe820bddc60ec94b26f8370c0cb48
tom coney <tom.ce.coney@gmail.com>
2023-12-23 13:34:58 -0700

    Add refactored CSS and Javascript to align with project spec

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ var potIcon = document.querySelector('.stockPot')
 var selectHdr = document.querySelector('.selectHeader')
 var selectTxt = document.querySelector('.selectText')
 var radioButtons = document.getElementsByName('userSelect')
+var sidesRad = document.querySelector('#sides')
 var reset = document.querySelector('.reset')
+var letsCook = document.querySelector('.lets-cook')
 var sides = [
     'Miso Glazed Carrots',
     'Coleslaw',
@@ -60,25 +62,32 @@ function buildArray(sideArr,mainArr,dessertArr) {
     return types
 }
 
+letsCook.addEventListener('click',cookSelection)
 reset.addEventListener('click',resetSelection)
 for(var i=0; i < radioButtons.length; i++) {
     radioButtons[i].addEventListener('click',radioSelect)
 }
 
+function cookSelection() {
+    potIcon.classList.add('hidden')
+    if(selectHdr.classList.contains('hidden')=== true && selectTxt.classList.contains('hidden')=== true) {
+        selectHdr.classList.replace('hidden','runAnimation')
+        selectTxt.classList.replace('hidden','runAnimation')
+    }
+}
+
 function radioSelect(event) {
     selType = event.target.id
-    potIcon.classList.add('hidden')
-    selectHdr.classList.remove('hidden')
-    selectTxt.classList.remove('hidden')
-
+    potIcon.classList.replace('hidden','runAnimation')
+    selectHdr.classList.add('hidden')
+    selectTxt.classList.add('hidden')
     displayRandomSelection(selType)
 }
 
 function displayRandomSelection(arrayName) {
     var array = selectionType[arrayName]
     var selection = array[randomIntGen(array.length)]
-    selectTxt.innerText = selection
-   console.log(array[randomIntGen(array.length)])
+    selectTxt.innerText = `${selection}!`
 }
 
 function randomIntGen(max) {
@@ -90,4 +99,5 @@ function resetSelection() {
     potIcon.classList.remove('hidden')
     selectHdr.classList.add('hidden')
     selectTxt.classList.add('hidden')
+    sidesRad.checked = true
 }

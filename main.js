ab0a5b7cc99dad21832d0ec2baa848e44eccad6f
Cory Sanders <corysanders3@yahoo.com>
2023-12-25 11:00:55 -0600

    Create functionality to give a random entire meal.

--- main.js
+++ main.js
@@ -51,18 +51,19 @@ var desserts = [
 ];
 
 var randomDish = '';
-
+var entireMeal = {
+    side: '',
+    mainDish: '',
+    dessert: ''
+}
 
 //query selectors
 var cookButton = document.querySelector('.lets-cook');
 var showDish = document.querySelector('div');
-var radioButtons = document.getElementsByName('meal')
+var radioButtons = document.getElementsByName('meal');
 
 //event listeners
-cookButton.addEventListener('click', checkRadioButtons)
-
-
-
+cookButton.addEventListener('click', checkRadioButtons);
 
 //function
 function checkRadioButtons() {
@@ -77,15 +78,18 @@ function checkRadioButtons() {
         getRandomDish(mains)
     } else if(selectedMeal === 'Dessert') {
         getRandomDish(desserts)
+    } else  if(selectedMeal === 'Entire Meal') {
+        makeEntireMeal(sides, mains, desserts)
     } else {
-        console.log('NOPE')
+        showDish.innerHTML = `
+        <h4>Oops! Looks like you haven't selected any option. Please choose a dish category and try again.</h4>
+        `
     }
-}
+};
 
 function getRandomDish(dishes) {
     var randomIndex = Math.floor(Math.random() * dishes.length)
     randomDish = dishes[randomIndex]
-    console.log(randomDish)
     showRandomDish()
 };
 
@@ -93,6 +97,23 @@ function showRandomDish() {
     showDish.innerHTML = `
     <p><em>You should make:</em></p>
     <h3>${randomDish}!</h3>
+    `
+};
+
+function makeEntireMeal(sides, mains, desserts) {
+    var randomIndex = Math.floor(Math.random() * sides.length)
+    entireMeal.side = sides[randomIndex]
+    randomIndex = Math.floor(Math.random() * mains.length)
+    entireMeal.mainDish = mains[randomIndex]
+    randomIndex = Math.floor(Math.random() * desserts.length)
+    entireMeal.dessert = desserts[randomIndex]
+    showEntireMeal()
+};
+
+function showEntireMeal() {
+    showDish.innerHTML = `
+    <p><em>You should make:</em></p>
+    <h4>${entireMeal.mainDish} with a side of ${entireMeal.side} and ${entireMeal.dessert} for dessert!</h4>
     `
 };
 
